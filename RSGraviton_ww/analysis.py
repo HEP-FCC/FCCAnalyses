79cf5d256b6f45134f237642362f921801634d64
clementhelsens <clement.helsens@cern.ch>
2018-01-08 18:15:07 +0100

    move to v02

--- RSGraviton_ww/analysis.py
+++ RSGraviton_ww/analysis.py
@@ -13,93 +13,102 @@ comp = cfg.Component(
      files = ["root://eospublic.cern.ch///eos/fcc/hh/generation/DelphesEvents/fcc_v01/pp_RSGraviton_30TeV_ww/events0.root"]
 )
 
-from heppySampleList_fcc_v01 import *
+from heppySampleList_fcc_v02 import *
 
 selectedComponents = [
-			pp_RSGraviton_2TeV_ww,
-			pp_RSGraviton_5TeV_ww,
+#			pp_RSGraviton_2TeV_ww,
+#			pp_RSGraviton_5TeV_ww,
 			pp_RSGraviton_10TeV_ww,
-			pp_RSGraviton_15TeV_ww,
+#			pp_RSGraviton_15TeV_ww,
 			pp_RSGraviton_20TeV_ww,
-			pp_RSGraviton_25TeV_ww,
-			pp_RSGraviton_30TeV_ww,
-			pp_RSGraviton_35TeV_ww,
-			pp_RSGraviton_40TeV_ww,
-			pp_jj012j_5f_HT_0_500,
-			pp_jj012j_5f_HT_500_1000,
-			pp_jj012j_5f_HT_1000_2000,
-			pp_jj012j_5f_HT_2000_4000,
-			pp_jj012j_5f_HT_4000_7200,
-			pp_jj012j_5f_HT_7200_15000,
-			pp_jj012j_5f_HT_15000_25000,
-			pp_jj012j_5f_HT_25000_35000,
-			pp_jj012j_5f_HT_35000_100000,
-			pp_tt012j_5f_HT_0_600,
-			pp_tt012j_5f_HT_600_1200,
-			pp_tt012j_5f_HT_1200_2100,
-			pp_tt012j_5f_HT_2100_3400,
-			pp_tt012j_5f_HT_3400_5300,
-			pp_tt012j_5f_HT_5300_8100,
-			pp_tt012j_5f_HT_8100_15000,
-			pp_tt012j_5f_HT_15000_25000,
-			pp_tt012j_5f_HT_25000_35000,
-			pp_tt012j_5f_HT_35000_100000,
-			pp_vv012j_4f_HT_0_300,
-			pp_vv012j_4f_HT_300_1400,
-			pp_vv012j_4f_HT_1400_2900,
-			pp_vv012j_4f_HT_2900_5300,
-			pp_vv012j_4f_HT_5300_8800,
-			pp_vv012j_4f_HT_8800_15000,
-			pp_vv012j_4f_HT_15000_25000,
-			pp_vv012j_4f_HT_25000_35000,
-			pp_vv012j_4f_HT_35000_100000,
+#			pp_RSGraviton_25TeV_ww,
+#			pp_RSGraviton_30TeV_ww,
+#			pp_RSGraviton_35TeV_ww,
+#			pp_RSGraviton_40TeV_ww,
+
+                        pp_tt_Pt2500toInf,
+                        pp_jj_Pt2500toInf,
+                        pp_vv_Pt2500toInf,
+                        pp_ll_Pt5000toInf,
+
+#			pp_jj012j_5f_HT_0_500,
+#			pp_jj012j_5f_HT_500_1000,
+#			pp_jj012j_5f_HT_1000_2000,
+#			pp_jj012j_5f_HT_2000_4000,
+#			pp_jj012j_5f_HT_4000_7200,
+#			pp_jj012j_5f_HT_7200_15000,
+#			pp_jj012j_5f_HT_15000_25000,
+#			pp_jj012j_5f_HT_25000_35000,
+#			pp_jj012j_5f_HT_35000_100000,
+#			pp_tt012j_5f_HT_0_600,
+#			pp_tt012j_5f_HT_600_1200,
+#			pp_tt012j_5f_HT_1200_2100,
+#			pp_tt012j_5f_HT_2100_3400,
+#			pp_tt012j_5f_HT_3400_5300,
+#			pp_tt012j_5f_HT_5300_8100,
+#			pp_tt012j_5f_HT_8100_15000,
+#			pp_tt012j_5f_HT_15000_25000,
+#			pp_tt012j_5f_HT_25000_35000,
+#			pp_tt012j_5f_HT_35000_100000,
+#			pp_vv012j_4f_HT_0_300,
+#			pp_vv012j_4f_HT_300_1400,
+#			pp_vv012j_4f_HT_1400_2900,
+#			pp_vv012j_4f_HT_2900_5300,
+#			pp_vv012j_4f_HT_5300_8800,
+#			pp_vv012j_4f_HT_8800_15000,
+#			pp_vv012j_4f_HT_15000_25000,
+#			pp_vv012j_4f_HT_25000_35000,
+#			pp_vv012j_4f_HT_35000_100000,
 		     ]
 
 
-pp_RSGraviton_2TeV_ww.splitFactor = 10
-pp_RSGraviton_5TeV_ww.splitFactor = 10
+#pp_RSGraviton_2TeV_ww.splitFactor = 10
+#pp_RSGraviton_5TeV_ww.splitFactor = 10
 pp_RSGraviton_10TeV_ww.splitFactor = 10
-pp_RSGraviton_15TeV_ww.splitFactor = 10
+#pp_RSGraviton_15TeV_ww.splitFactor = 10
 pp_RSGraviton_20TeV_ww.splitFactor = 10
-pp_RSGraviton_25TeV_ww.splitFactor = 10
-pp_RSGraviton_30TeV_ww.splitFactor = 10
-pp_RSGraviton_35TeV_ww.splitFactor = 10
-pp_RSGraviton_40TeV_ww.splitFactor = 10
+#pp_RSGraviton_25TeV_ww.splitFactor = 10
+#pp_RSGraviton_30TeV_ww.splitFactor = 10
+#pp_RSGraviton_35TeV_ww.splitFactor = 10
+#pp_RSGraviton_40TeV_ww.splitFactor = 10
 
-pp_tt012j_5f_HT_0_600.splitFactor = 10
-pp_tt012j_5f_HT_600_1200.splitFactor = 10
-pp_tt012j_5f_HT_1200_2100.splitFactor = 10
-pp_tt012j_5f_HT_2100_3400.splitFactor = 10
-pp_tt012j_5f_HT_3400_5300.splitFactor = 10
-pp_tt012j_5f_HT_5300_8100.splitFactor = 10
-pp_tt012j_5f_HT_8100_15000.splitFactor = 10
-pp_tt012j_5f_HT_15000_25000.splitFactor = 10
-pp_tt012j_5f_HT_25000_35000.splitFactor = 10
-pp_tt012j_5f_HT_35000_100000.splitFactor = 10
+#pp_tt012j_5f_HT_0_600.splitFactor = 10
+#pp_tt012j_5f_HT_600_1200.splitFactor = 10
+#pp_tt012j_5f_HT_1200_2100.splitFactor = 10
+#pp_tt012j_5f_HT_2100_3400.splitFactor = 10
+#pp_tt012j_5f_HT_3400_5300.splitFactor = 10
+#pp_tt012j_5f_HT_5300_8100.splitFactor = 10
+#pp_tt012j_5f_HT_8100_15000.splitFactor = 10
+#pp_tt012j_5f_HT_15000_25000.splitFactor = 10
+#pp_tt012j_5f_HT_25000_35000.splitFactor = 10
+#pp_tt012j_5f_HT_35000_100000.splitFactor = 10
 
-pp_jj012j_5f_HT_0_500.splitFactor = 10
-pp_jj012j_5f_HT_500_1000.splitFactor = 10
-pp_jj012j_5f_HT_1000_2000.splitFactor = 10
-pp_jj012j_5f_HT_2000_4000.splitFactor = 10
-pp_jj012j_5f_HT_4000_7200.splitFactor = 10
-pp_jj012j_5f_HT_7200_15000.splitFactor = 10
-pp_jj012j_5f_HT_15000_25000.splitFactor = 10
-pp_jj012j_5f_HT_25000_35000.splitFactor = 10
-pp_jj012j_5f_HT_35000_100000.splitFactor = 10
+#pp_jj012j_5f_HT_0_500.splitFactor = 10
+#pp_jj012j_5f_HT_500_1000.splitFactor = 10
+#pp_jj012j_5f_HT_1000_2000.splitFactor = 10
+#pp_jj012j_5f_HT_2000_4000.splitFactor = 10
+#pp_jj012j_5f_HT_4000_7200.splitFactor = 10
+#pp_jj012j_5f_HT_7200_15000.splitFactor = 10
+#pp_jj012j_5f_HT_15000_25000.splitFactor = 10
+#pp_jj012j_5f_HT_25000_35000.splitFactor = 10
+#pp_jj012j_5f_HT_35000_100000.splitFactor = 10
 
-pp_vv012j_4f_HT_0_300.splitFactor = 10
-pp_vv012j_4f_HT_300_1400.splitFactor = 10
-pp_vv012j_4f_HT_1400_2900.splitFactor = 10
-pp_vv012j_4f_HT_2900_5300.splitFactor = 10
-pp_vv012j_4f_HT_5300_8800.splitFactor = 10
-pp_vv012j_4f_HT_8800_15000.splitFactor = 10
-pp_vv012j_4f_HT_15000_25000.splitFactor = 10
-pp_vv012j_4f_HT_25000_35000.splitFactor = 10
-pp_vv012j_4f_HT_35000_100000.splitFactor = 10
+#pp_vv012j_4f_HT_0_300.splitFactor = 10
+#pp_vv012j_4f_HT_300_1400.splitFactor = 10
+#pp_vv012j_4f_HT_1400_2900.splitFactor = 10
+#pp_vv012j_4f_HT_2900_5300.splitFactor = 10
+#pp_vv012j_4f_HT_5300_8800.splitFactor = 10
+#pp_vv012j_4f_HT_8800_15000.splitFactor = 10
+#pp_vv012j_4f_HT_15000_25000.splitFactor = 10
+#pp_vv012j_4f_HT_25000_35000.splitFactor = 10
+#pp_vv012j_4f_HT_35000_100000.splitFactor = 10
 
 #selectedComponents = [comp]
 
-from heppy.analyzers.fcc.Reader import Reader
+#from heppy.analyzers.fcc.Reader import Reader
+#for fcc_v02
+from heppy.FCChhAnalyses.Reader import Reader
+
 source = cfg.Analyzer(
     Reader,
 
