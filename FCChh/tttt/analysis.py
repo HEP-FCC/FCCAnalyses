15bd647caa95b0706694512b071ea3bb9ea843e5
David Olivier Jamin <david.jamin@cern.ch>
2019-02-22 16:19:28 +0100

    new objects 08 + updated sel and Tree outputs + updated bkgd list

--- FCChh/tttt/analysis.py
+++ FCChh/tttt/analysis.py
@@ -17,10 +17,51 @@ comp = cfg.Component(
 
 selectedComponents = [
                         sample.mgp8_pp_tttt_5f,
+                        sample.mgp8_pp_bbbbj_QCD,
+                        sample.mgp8_pp_ttz_5f_zbb,
+                        sample.mgp8_pp_ttbb_4f,
+                        sample.mgp8_pp_tth01j_5f_hbb,
+                        sample.mgp8_pp_ttzz_5f,
+                        sample.mgp8_pp_ttww_4f,
+                        sample.mgp8_pp_ttwz_5f,
                      ]
 
-splitFac = 15
-sample.mgp8_pp_tttt_5f.splitFactor = splitFac
+## short list from fcc_v02
+#mgp8_pp_tttt_5f                   1,450,000
+#------------
+#------------
+#mgp8_pp_tv012j_5f                   100,000
+#------------
+#mgp8_pp_ttw_5f                    3,720,000
+#mgp8_pp_ttv01j_5f                 1,107,948
+#------------
+#mgp8_pp_ttz_5f_zbb               38,710,000
+#mgp8_pp_ttz_5f                    3,750,000
+#mgp8_pp_ttz01j_5f_HT_1100_2700    1,802,063
+#mgp8_pp_ttz01j_5f_HT_2700_4900    1,876,797
+#mgp8_pp_ttz01j_5f_HT_4900_8100    1,923,311
+#mgp8_pp_ttz01j_5f_HT_8100_100000    677,540 
+#------------
+#mgp8_pp_bbbbj_QCD                14,010,000 ??
+#------------
+#mgp8_pp_tt012j_5f                15,253,535
+#mgp8_pp_ttbb_4f                  23,239,639
+#mgp8_pp_ttj_4f                   38,500,000
+#------------
+#mgp8_pp_tth01j_5f_hbb            32,486,572
+#------------
+#mgp8_pp_ttzz_5f                   1,430,000
+#mgp8_pp_ttww_4f                     570,000
+#mgp8_pp_ttwz_5f                     500,000
+
+sample.mgp8_pp_tttt_5f.splitFactor       = 15
+sample.mgp8_pp_bbbbj_QCD.splitFactor     = 140
+sample.mgp8_pp_ttz_5f_zbb.splitFactor    = 390
+sample.mgp8_pp_ttbb_4f.splitFactor       = 230
+sample.mgp8_pp_tth01j_5f_hbb.splitFactor = 320
+sample.mgp8_pp_ttzz_5f.splitFactor       = 14
+sample.mgp8_pp_ttww_4f.splitFactor       = 6
+sample.mgp8_pp_ttwz_5f.splitFactor       = 5
 
 selectedComponents = [comp]
 
@@ -38,43 +79,31 @@ source = cfg.Analyzer(
     electrons = 'electrons',
     muons = 'muons',
 
-    #main jets trk02
-    trkjets02  = 'trkjets02',
-    trkbTags02 = 'trkbTags02',
+    #main jets trk08
+    trkjets08  = 'trkjets08',
+    trkbTags08 = 'trkbTags08',
 
-    trkjetsOneSubJettiness02    = 'trkjetsOneSubJettiness02',
-    trkjetsTwoSubJettiness02    = 'trkjetsTwoSubJettiness02',
-    trkjetsThreeSubJettiness02  = 'trkjetsThreeSubJettiness02',
-    trksubjetsSoftDropTagged02  = 'trksubjetsSoftDropTagged02',
-    trksubjetsSoftDrop02        = 'trksubjetsSoftDrop02',
-    #
-    trksubjetsSoftDropTagged04  = 'trksubjetsSoftDropTagged04',
-    trksubjetsSoftDrop04        = 'trksubjetsSoftDrop04',
+    trkjetsOneSubJettiness08    = 'trkjetsOneSubJettiness08',
+    trkjetsTwoSubJettiness08    = 'trkjetsTwoSubJettiness08',
+    trkjetsThreeSubJettiness08  = 'trkjetsThreeSubJettiness08',
     trksubjetsSoftDropTagged08  = 'trksubjetsSoftDropTagged08',
     trksubjetsSoftDrop08        = 'trksubjetsSoftDrop08',
   
-    #pf jets pf02 for correction
-    pfjets02  = 'pfjets02',
-    pfbTags02 = 'pfbTags02',
+    #pf jets pf08 for correction
+    pfjets08  = 'pfjets08',
+    pfbTags08 = 'pfbTags08',
 
-    pfjetsOneSubJettiness02    = 'pfjetsOneSubJettiness02',
-    pfjetsTwoSubJettiness02    = 'pfjetsTwoSubJettiness02',
-    pfjetsThreeSubJettiness02  = 'pfjetsThreeSubJettiness02',
-    pfsubjetsSoftDropTagged02  = 'pfsubjetsSoftDropTagged02',
-    pfsubjetsSoftDrop02        = 'pfsubjetsSoftDrop02',
+    pfjetsOneSubJettiness08    = 'pfjetsOneSubJettiness08',
+    pfjetsTwoSubJettiness08    = 'pfjetsTwoSubJettiness08',
+    pfjetsThreeSubJettiness08  = 'pfjetsThreeSubJettiness08',
+    pfsubjetsSoftDropTagged08  = 'pfsubjetsSoftDropTagged08',
+    pfsubjetsSoftDrop08        = 'pfsubjetsSoftDrop08',
 
 
     # used for b-tagging
     pfjets04  = 'pfjets04',
     pfbTags04 = 'pfbTags04',
     pfjetsFlavor04  = 'pfjetsFlavor04',
-
-    # used for mreco
-    pfjets08  = 'pfjets08',
-    pfbTags08 = 'pfbTags08',
-
-    trkjets04  = 'trkjets04',
-    trkjets08  = 'trkjets08',
 
 )
 
@@ -90,77 +119,43 @@ from EventStore import EventStore as Events
 #uncomment the following to go back to normal
 
 from heppy.analyzers.Selector import Selector
-# select pf02 jets above 2000 GeV
-jets_pf02_1500 = cfg.Analyzer(
+# select pf08 jets above 30 GeV
+jets_pf08_30 = cfg.Analyzer(
     Selector,
-    'jets_pf02_1500',
-    output = 'jets_pf02_1500',
-    input_objects = 'pfjets02',
-    filter_func = lambda fatjet: fatjet.pt()>750.
+    'jets_pf08_30',
+    output = 'jets_pf08_30',
+    input_objects = 'pfjets08',
+    filter_func = lambda fatjet: fatjet.pt()>30.
 )
 
-jets_trk02_1000 = cfg.Analyzer(
+jets_trk08_20 = cfg.Analyzer(
     Selector,
-    'jets_trk02_1000',
-    output = 'jets_trk02_1000',
-    input_objects = 'trkjets02',
-    filter_func = lambda jet: jet.pt()>500
-)
-
-jets_trk04_1000 = cfg.Analyzer(
-    Selector,
-    'jets_trk04_1000',
-    output = 'jets_trk04_1000',
-    input_objects = 'trkjets04',
-    filter_func = lambda jet: jet.pt()>500
-)
-
-jets_trk08_1000 = cfg.Analyzer(
-    Selector,
-    'jets_trk08_1000',
-    output = 'jets_trk08_1000',
+    'jets_trk08_20',
+    output = 'jets_trk08_20',
     input_objects = 'trkjets08',
-    filter_func = lambda jet: jet.pt()>500
+    filter_func = lambda jet: jet.pt()>20
 )
 
-# select pf04 jets above 1000 GeV for b-tagging
-jets_pf04_1000 = cfg.Analyzer(
+# select pf04 jets above 20 GeV for direct b-tagging
+jets_pf04_20 = cfg.Analyzer(
     Selector,
-    'jets_pf04_1000',
-    output = 'jets_pf04_1000',
+    'jets_pf04_20',
+    output = 'jets_pf04_20',
     input_objects = 'pfjets04',
-    filter_func = lambda jet: jet.pt()>500
+    filter_func = lambda jet: jet.pt()>20
 )
 
 # apply jet flavour tagging
 from heppy.FCChhAnalyses.analyzers.FlavourTagger import FlavourTagger
-jets_pf04_1000_pdg = cfg.Analyzer(
+jets_pf04_20_pdg = cfg.Analyzer(
     FlavourTagger,
-    'jets_pf04_1000_pdg',
-    input_jets = 'jets_pf04_1000',
+    'jets_pf04_20_pdg',
+    input_jets = 'jets_pf04_20',
     input_genparticles = 'gen_particles',
-    output_jets = 'jets_pf04_1000_pdg',
+    output_jets = 'jets_pf04_20_pdg',
     dr_match = 0.4,
     pdg_tags = [5, 4, 0],
     ptr_min = 0.1,
-)
-
-# select pf04 jets above 1500 GeV for jet correction
-jets_pf04_1500 = cfg.Analyzer(
-    Selector,
-    'jets_pf04_1500',
-    output = 'jets_pf04_1500',
-    input_objects = 'pfjets04',
-    filter_func = lambda jet: jet.pt()>750
-)
-
-# select pf08 jets above 1500 GeV
-jets_pf08_1500 = cfg.Analyzer(
-    Selector,
-    'jets_pf08_1500',
-    output = 'jets_pf08_1500',
-    input_objects = 'pfjets08',
-    filter_func = lambda jet: jet.pt()>750
 )
 
 # select electrons above 100 GeV
@@ -185,14 +180,9 @@ muons_100 = cfg.Analyzer(
 from heppy.FCChhAnalyses.FCChh.tttt.TreeProducer import TreeProducer
 tree = cfg.Analyzer(
     TreeProducer,
-    jets_trk02_1000 = 'jets_trk02_1000',
-    jets_trk04_1000 = 'jets_trk04_1000',
-    jets_trk08_1000 = 'jets_trk08_1000',
-
-    jets_pf02_1500  = 'jets_pf02_1500',
-    jets_pf04_1000  = 'jets_pf04_1000',
-    jets_pf04_1500  = 'jets_pf04_1500',
-    jets_pf08_1500  = 'jets_pf08_1500',
+    jets_trk08_20 = 'jets_trk08_20',
+    jets_pf04_20  = 'jets_pf04_20',
+    jets_pf08_30  = 'jets_pf08_30',
 
     electrons = 'electrons_100',
     muons = 'muons_100',
@@ -204,15 +194,11 @@ tree = cfg.Analyzer(
 # the analyzers will process each event in this order
 sequence = cfg.Sequence( [
     source,
-    jets_pf02_1500,
-    jets_pf04_1000,
-    jets_pf04_1000_pdg,
-    jets_pf04_1500,
-    jets_pf08_1500,
+    jets_pf08_30,
+    jets_pf04_20,
+    jets_pf04_20_pdg,
 
-    jets_trk02_1000,
-    jets_trk04_1000,
-    jets_trk08_1000,
+    jets_trk08_20,
 
     electrons_100,
     muons_100,
