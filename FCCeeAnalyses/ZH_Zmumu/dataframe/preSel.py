0328c46fcfc92674a691a36462afb282102a93cf
clementhelsens <clement.helsens@cern.ch>
2020-03-05 14:49:27 +0100

    add outDir

--- FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py
@@ -1,11 +1,11 @@
 #python FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py 
 
 basedir="/afs/cern.ch/work/h/helsens/public/FCCDicts/yaml/FCCee/fcc_v01/"
-
+outdir="FCCee/ZH_Zmumu/"
 NUM_CPUS = 10
 process_list=['p8_ee_ZZ_ecm240','p8_ee_WW_ecm240','p8_ee_ZH_ecm240']
 fraction=0.005
 
 import bin.runDataFrame as rdf
 myana=rdf.runDataFrame(basedir,process_list)
-myana.run(ncpu=NUM_CPUS,fraction=fraction)
+myana.run(ncpu=NUM_CPUS,fraction=fraction,outDir=outdir)
