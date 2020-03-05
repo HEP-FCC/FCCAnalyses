6066672a188af09c8871a4fa4d6693a20634ec8b
clementhelsens <clement.helsens@cern.ch>
2020-03-05 14:52:08 +0100

    add plots configuration

---
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/plots.py
@@ -0,0 +1,46 @@
+import ROOT
+
+# global parameters
+intLumi        = 5.0e+06 #in pb-1
+ana_tex        = "e^{+}e^{-} #rightarrow ZH #rightarrow #mu^{+}#mu^{-} + X"
+delphesVersion = "3.4.2"
+energy         = 240.0
+collider       = "FCC-ee"
+inputDir       = "FCCee/ZH_Zmumu/"
+formats        = ['png','pdf']
+yaxis          = ['lin','log']
+stacksig       = ['stack','nostack']
+outdir         = 'FCCee/ZH_Zmumu/plots/'
+
+variables = ['mz','mz_zoom','nbjets']
+
+selections = {}
+selections['ZH']   = ["sel0","sel1","sel2"]
+selections['ZH_2'] = ["sel0","sel2"]
+
+colors = {}
+colors['ZH'] = ROOT.kRed
+colors['WW'] = ROOT.kBlue+1
+colors['ZZ'] = ROOT.kGreen+2
+colors['VV'] = ROOT.kGreen+3
+
+plots = {}
+plots['ZH'] = {'signal':{'ZH':['p8_ee_ZH_ecm240']},
+               'backgrounds':{'WW':['p8_ee_WW_ecm240'],
+                              'ZZ':['p8_ee_ZZ_ecm240']}
+           }
+
+
+plots['ZH_2'] = {'signal':{'ZH':['p8_ee_ZH_ecm240']},
+                 'backgrounds':{'VV':['p8_ee_WW_ecm240','p8_ee_ZZ_ecm240']}
+             }
+
+legend = {}
+legend['ZH'] = 'ZH boson'
+legend['WW'] = 'WW boson'
+legend['ZZ'] = 'ZZ boson'
+legend['VV'] = 'VV boson'
+
+
+
+
