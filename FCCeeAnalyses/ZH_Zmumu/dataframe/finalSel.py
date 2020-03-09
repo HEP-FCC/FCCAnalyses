d25fb5d02e1dc604a3223b54545ffa556b5b11b8
clementhelsens <clement.helsens@cern.ch>
2020-03-09 12:23:23 +0100

    add leptonic recoil

--- FCCeeAnalyses/ZH_Zmumu/dataframe/finalSel.py
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/finalSel.py
@@ -7,9 +7,9 @@ procDict = "/afs/cern.ch/work/h/helsens/public/FCCDicts/FCCee_procDict_fcc_v01.j
 NUM_CPUS = 5
 process_list=['p8_ee_ZZ_ecm240','p8_ee_WW_ecm240','p8_ee_ZH_ecm240']
 
-cut_list = ['zed_leptonic_m.size() > 0',
-            'zed_leptonic_m.size() > 0 && zed_leptonic_m[0] > 80 &&  zed_leptonic_m[0] < 100',
-            'zed_leptonic_m.size() > 0 && zed_leptonic_m[0] > 80 &&  zed_leptonic_m[0] < 100 && nbjets>1']
+cut_list = ['zed_leptonic_m.size() == 1',
+            'zed_leptonic_m.size() == 1 && zed_leptonic_m[0] > 80 &&  zed_leptonic_m[0] < 100',
+            'zed_leptonic_m.size() == 1 && zed_leptonic_m[0] > 80 &&  zed_leptonic_m[0] < 100 && nbjets==2']
 
 
 ### variable list
@@ -17,6 +17,7 @@ variables = {
     "mz":{"name":"zed_leptonic_m","title":"m_{Z} [GeV]","bin":125,"xmin":0,"xmax":250},
     "mz_zoom":{"name":"zed_leptonic_m","title":"m_{Z} [GeV]","bin":40,"xmin":80,"xmax":100},
     "nbjets":{"name":"nbjets","title":"number of bjets","bin":10,"xmin":0,"xmax":10},
+    "leptonic_recoil_m":{"name":"zed_leptonic_recoil_m","title":"Z leptonic recoil [GeV]","bin":100,"xmin":0,"xmax":200},
 }
 
 
