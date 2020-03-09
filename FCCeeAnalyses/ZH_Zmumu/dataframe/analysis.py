7eba2036a90a0f1d06dbc365c02320174b6a9e19
clementhelsens <clement.helsens@cern.ch>
2020-03-09 12:22:52 +0100

    add recoil and selection specific for Z->mumu

--- FCCeeAnalyses/ZH_Zmumu/dataframe/analysis.py
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/analysis.py
@@ -23,21 +23,19 @@ class analysis():
         print " done"
     #__________________________________________________________
     def run(self):
-        df2 = self.df.Define("selected_electrons", "selectParticlesPtIso(10, 0.4)(electrons, electronITags)") \
-                     .Define("selected_muons", "selectParticlesPtIso(10, 0.4)(muons, muonITags)") \
-                     .Define("selected_leptons", "mergeElectronsAndMuons(selected_electrons, selected_muons)") \
-                     .Define("selected_leptons_pt", "get_pt(selected_leptons)") \
-                     .Define("zeds", "LeptonicZBuilder(selected_leptons)") \
-                     .Define("zeds_pt", "get_pt(zeds)") \
-                     .Define("zeds_m", "get_mass(zeds)") \
+        df2 = self.df.Define("selected_muons", "selectParticlesPtIso(10, 0.4)(muons, muonITags)") \
+                     .Define("selected_muons_pt", "get_pt(selected_muons)") \
+                     .Define("selected_muons_y", "get_y(selected_muons)") \
+                     .Define("selected_muons_p", "get_p(selected_muons)") \
+                     .Define("selected_muons_e", "get_e(selected_muons)") \
                      .Define("jets_10_bs", "selectJets(10, true)(efjets, efbTags)") \
                      .Define("jets_10_lights", "selectJets(10, false)(efjets, efbTags)") \
-                     .Define("selected_bs", "noMatchJets(0.2)(jets_10_bs, selected_leptons)") \
-                     .Define("selected_lights", "noMatchJets(0.2)(jets_10_lights, selected_leptons)") \
+                     .Define("selected_bs", "noMatchJets(0.2)(jets_10_bs, selected_muons)") \
+                     .Define("selected_lights", "noMatchJets(0.2)(jets_10_lights, selected_muons)") \
                      .Define("nbjets", "get_njets(selected_bs)") \
                      .Define("njets", "get_njets2(selected_bs, selected_lights)") \
                      .Define("weight"," id_float(mcEventWeights)") \
-                     .Define("zed_leptonic","ResonanceBuilder(23, 91)(selected_leptons)") \
+                     .Define("zed_leptonic","ResonanceBuilder(23, 91)(selected_muons)") \
                      .Define("zed_leptonic_m", "get_mass(zed_leptonic)") \
                      .Define("zed_leptonic_pt","get_pt(zed_leptonic)") \
                      .Define("zed_hadronic_light","JetResonanceBuilder(23, 91)(jets_10_lights)") \
@@ -46,19 +44,25 @@ class analysis():
                      .Define("zed_hadronic_b","JetResonanceBuilder(23, 91)(jets_10_bs)") \
                      .Define("zed_hadronic_b_m", "get_mass(zed_hadronic_b)") \
                      .Define("zed_hadronic_b_pt","get_pt(zed_hadronic_b)") \
+                     .Define("zed_leptonic_recoil","recoil(240)(zed_leptonic)") \
+                     .Define("zed_leptonic_recoil_m","get_mass(zed_leptonic_recoil)") \
+
+        
 
 
         branchList = ROOT.vector('string')()
         for branchName in [
-                "selected_leptons_pt",
-                "zeds_pt",
-                "zeds_m",
+                "selected_muons_pt",
+                "selected_muons_y",
+                "selected_muons_p",
+                "selected_muons_e",
                 "zed_leptonic_pt",
                 "zed_leptonic_m",
                 "zed_hadronic_light_pt",
                 "zed_hadronic_light_m",
                 "zed_hadronic_b_pt",
                 "zed_hadronic_b_m",
+                "zed_leptonic_recoil_m",
                 "nbjets",
                 "njets",
                 "weight",
