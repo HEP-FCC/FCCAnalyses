d821fc142fa1ce60997c33eed726bd936f26f1c6
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2020-07-30 16:57:58 +0200

    Support for controlling the FCCDicts dir with env FCCDICTSDIR
    FCCDICTSDIR should point to a directory containing the json files
    and the rest.
    For example, on lxplus:
    
    $ export FCCDICTSDIR=/afs/cern.ch/work/h/helsens/public/FCCDicts/
    
    (the trailing '/' is optional, it will be checked and added inside)
    
    The default is called 'deffccdicts' read from the new file common_default.py .
    It is currently set to '/afs/cern.ch/work/h/helsens/public/FCCDicts/' , so nothing
    should change for people able to address that directory.

--- FCCeeAnalyses/ZH_Zmumu/dataframe/finalSel.py
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/finalSel.py
@@ -1,12 +1,14 @@
+from common_defaults import deffccdicts
+
 #python FCCeeAnalyses/ZH_Zmumu/dataframe/finalSel.py 
-import sys
+import sys, os
 import ROOT
 
 ###Input directory where the files produced at the pre-selection level are
 baseDir  = "FCCee/ZH_Zmumu/"
 
 ###Link to the dictonary that contains all the cross section informations etc...
-procDict = "/afs/cern.ch/work/h/helsens/public/FCCDicts/FCCee_procDict_fcc_v01.json"
+procDict = os.path.join(os.environ['FCCDICTSDIR'], '') + "FCCee_procDict_fcc_v01.json"
 
 process_list=['p8_ee_ZZ_ecm240','p8_ee_WW_ecm240','p8_ee_ZH_ecm240']
 
--- FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py
@@ -1,6 +1,8 @@
-#python FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py 
+from common_defaults import deffccdicts
+ #python FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py 
+import os
 
-basedir="/afs/cern.ch/work/h/helsens/public/FCCDicts/yaml/FCCee/fcc_v01/"
+basedir=os.path.join(os.getenv('FCCDICTSDIR', deffccdicts), '') + "yaml/FCCee/fcc_v01/"
 outdir="FCCee/ZH_Zmumu/"
 NUM_CPUS = 15
 process_list=['p8_ee_ZZ_ecm240','p8_ee_WW_ecm240','p8_ee_ZH_ecm240']
