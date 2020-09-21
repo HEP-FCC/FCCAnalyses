64f9a5b02fe3acf0cd59a2def7a90079930e95b8
clementhelsens <clement.helsens@cern.ch>
2020-09-21 14:07:50 +0200

    v02 in presel

--- FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py
@@ -1,9 +1,11 @@
+#python FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py 
+
 from common_defaults import deffccdicts
- #python FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py 
 import os
 
-basedir=os.path.join(os.getenv('FCCDICTSDIR', deffccdicts), '') + "yaml/FCCee/fcc_v01/"
+basedir=os.path.join(os.getenv('FCCDICTSDIR', deffccdicts), '') + "yaml/FCCee/fcc_v02/"
 outdir="FCCee/ZH_Zmumu/"
+
 NUM_CPUS = 15
 process_list=['p8_ee_ZZ_ecm240','p8_ee_WW_ecm240','p8_ee_ZH_ecm240']
 fraction=0.01
