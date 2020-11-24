8148fcbab170309e7c5ded481107a8a4e0f0dd7d
clementhelsens <clement.helsens@cern.ch>
2020-11-24 11:35:49 +0100

    some updates

--- FCCeeAnalyses/ZH_Zmumu/preSel.py
+++ FCCeeAnalyses/ZH_Zmumu/preSel.py
@@ -2,11 +2,15 @@ from common_defaults import deffccdicts
  #python FCCeeAnalyses/ZH_Zmumu/dataframe/preSel.py 
 import os
 
-basedir=os.path.join(os.getenv('FCCDICTSDIR', deffccdicts), '') + "yaml/FCCee/fcc_v01/"
+basedir=os.path.join(os.getenv('FCCDICTSDIR', deffccdicts), '') + "yaml/FCCee/fcc_tmp/"
 outdir="FCCee/ZH_Zmumu/"
-NUM_CPUS = 15
+
+import multiprocessing
+NUM_CPUS = int(multiprocessing.cpu_count()-2)
+
 process_list=['p8_ee_ZZ_ecm240','p8_ee_WW_ecm240','p8_ee_ZH_ecm240']
-fraction=0.01
+#process_list=['p8_ee_ZZ_ecm240']
+fraction=1.0
 
 import bin.runDataFrame as rdf
 myana=rdf.runDataFrame(basedir,process_list)
