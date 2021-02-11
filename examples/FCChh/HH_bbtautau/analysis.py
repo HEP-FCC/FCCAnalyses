6afb32727d0df2d6e5e86d5ed31cbe0a738bea9e
clementhelsens <clement.helsens@cern.ch>
2021-02-11 16:21:34 +0100

    add simple path

--- examples/FCChh/HH_bbtautau/analysis.py
+++ examples/FCChh/HH_bbtautau/analysis.py
@@ -33,7 +33,10 @@ class analysis():
                .Define("selected_jets", "selRP_pT(50.)(Jet)")
                .Define("jet_pT",        "getRP_pt(Jet)")
                .Define("seljet_pT",     "getRP_pt(selected_jets)")
-               
+               .Alias("Jet3","Jet#3.index") 
+               .Define("JET_btag", "getJet_btag(Jet3, ParticleIDs, ParticleIDs_0)")
+               .Define("EVT_nbtag", "getJet_ntags(JET_btag)")
+
                )
 
         # select branches for output file
@@ -41,13 +44,16 @@ class analysis():
         for branchName in [
                 "jet_pT",
                 "seljet_pT",
+                "EVT_nbtag"
 
                 ]:
             branchList.push_back(branchName)
         df2.Snapshot("events", self.outname, branchList)
 
+
+
 # example call for standalone file
-# python FCChhAnalyses/FCChh/ttHH/dataframe/analysis.py /eos/experiment/fcc/hh/generation/DelphesEvents/fcc_v04/mgp8_pp_tthh_lambda100_5f/events_152512217.root
+# python examples/FCChh/HH_bbtautau/analysis.py /eos/experiment/fcc/hh/generation/DelphesEvents/fcc_v04/mgp8_pp_tt012j_5f/events_012599692.root 
 
 if __name__ == "__main__":
 
@@ -56,11 +62,11 @@ if __name__ == "__main__":
         print ("python ",sys.argv[0]," file.root")
         sys.exit(3)
     infile = sys.argv[1]
-    outDir = 'FCChh/'+sys.argv[0].split('/')[2]+'/'
+    outDir = sys.argv[0].replace(sys.argv[0].split('/')[0],'outputs/FCChh').replace('analysis.py','')+'/'
     import os
     os.system("mkdir -p {}".format(outDir))
     outfile = outDir+infile.split('/')[-1]
-    ncpus = 0
+    ncpus = 2
     analysis = analysis(infile, outfile, ncpus)
     analysis.run()
 
