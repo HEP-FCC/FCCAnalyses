80243991e6c5c7fe59565699af302579eb9df85d
Birgit Stapf <bstapf@nikhef.nl>
2021-02-24 16:03:26 +0100

    clean up and expand the basic example script, access also met and photons. improve description

--- examples/basics/README.md
+++ examples/basics/README.md
@@ -41,12 +41,17 @@ EDM4HEP event model
 Reading objects from EDM4HEP
 =============
 The example read_EDM4HEP.py shows you how to access the different objects such as jets, electrons, muons, missing ET etc. from the EDM4HEP files. Generally a new variable is calculated with a statement inside the `run()` function of the `analysis` class like
-`dataframe.Define("<your_variable>", "<accessor_fct <name of collection/object>")`
-which creates a column in the RDataFrame named `<your_variable>` and filled with the return value of the `<accessor_fct>` for the given collection/object. 
+`dataframe.Define("<your_variable>", "<accessor_fct (<name_object>)>")`
+which creates a column in the RDataFrame named `<your_variable>` and filled with the return value of the `<accessor_fct>` for the given object. 
 
 Here, accessor functions are the functions found in the C++ analyzers code that return a certain variable, e.g. getRP_pt(object) returns the pT of the object. To find out all functions available for e.g. reconstructed particles have a look at the [respective analyzers code](https://github.com/bistapf/FCCAnalyses/blob/basicexamples/analyzers/dataframe/ReconstructedParticle.h). If you want to add your own function have a look at the [Writing your own function](#writing-your-own-function) section on this page. 
 
-For the name of the object, in principle the names of the EDM4HEP collections are used - muons and electrons are an exception where a few extra steps are required, as shown in the example here. 
+For the name of the object, in principle the names of the EDM4HEP collections are used - photons, muons and electrons are an exception where a few extra steps are required, as shown in the example here. 
+
+This example also shows how to apply object selection cuts, for example selecting only objects with a transverse momentum pT larger than a given threshold by using the `selRP_pT(<threshold>)(<name_object>)` function. 
+
+In the end of the example you can see how the selected variables are written to branches of the output n-tuple, using the `dataframe.Snapshot("<tree_name>", <branch_list> )`, where in all examples here the name of the output-tree is always `events` and the branch_list is defined as a `ROOT.vector('string')` as demonstrated in the example. Note that branches of variables that exist multiple times per event, i.e. anything derived from a collection such as the pT of jets, result in vector branches. This is also true for some quantities that in principle only exist once per event, but are collections in the EDM4HEP format, such as the missing transverse energy.
+
 
 TODO: explain the logics of naming etc and accessor functions for basics like pT, eta, etc. - make a brief overview of the most important ones in the readme or is the .py enough? 
  
--- examples/basics/read_EDM4HEP.py
+++ examples/basics/read_EDM4HEP.py
@@ -42,78 +42,78 @@ class analysis():
 	def run(self):
 
 		df2 = (self.df
-		
-		#JETS:
-		#TODO: count all jets in the event
-		.Define("selected_jets", "selRP_pT(50.)(Jet)") 
-		.Define("jet_pT",        "getRP_pt(Jet)")
-		.Define("seljet_pT",     "getRP_pt(selected_jets)")
-	
-		
-		#ELECTRONS
-		#TODO: clean up 
+
+		#Access the various objects and their properties with the following syntax: .Define("<your_variable>", "<accessor_fct (name_object)>")
+		#This will create a column in the RDataFrame named <your_variable> and filled with the return value of the <accessor_fct> for the given collection/object 
+		#Accessor functions are the functions found in the C++ analyzers code that return a certain variable, e.g. getRP_n(object) returns the number of these objects 
+		#in the event and getRP_pt(object) returns the pT of the object 
+		#For the name of the object, in principle the names of the EDM4HEP collections are used - photons, muons and electrons are an exception, see below
+
+		#OVERVIEW: Accessing different objects and counting them
+		#JETS
+		.Define("n_jets", "getRP_n(Jet)") #count how many jets are in the event in total
+
+		#PHOTONS
+		.Alias("Photon0", "Photon#0.index") 
+		.Define("photons",  "getRP(Photon0, ReconstructedParticles)") 
+		.Define("n_photons",  "getRP_n(photons)") #count how many photons are in the event in total
+
+		#ELECTRONS AND MUONS
+		#TODO: ADD EXPLANATION OF THE EXTRA STEPS
 		.Alias("Electron0", "Electron#0.index")
-		# define the electron collection : TODO ADD EXPLANATION,
 		.Define("electrons",  "getRP(Electron0, ReconstructedParticles)") 
-		.Define("n_electrons",  "getRP_n(electrons)")
-		.Define("pT_electrons",  "getRP_pt(electrons)")
-		#select electrons with pT > 20 GeV
-		.Define("selected_electrons", "selRP_pT(20.)(electrons)")
-		# .Define("n_electrons_sel", "getRP_n(selected_electrons)")
-		.Define("n_electrons_sel", "return selected_electrons.size()") #why does this not work?
-		.Define("pT_electrons_sel",  "getRP_pt(selected_electrons)")
+		.Define("n_electrons",  "getRP_n(electrons)") #count how many electrons are in the event in total
 
-		#MUONS
-		#TODO: clean up 
-		# define an alias for muon index collection #why is this needed?
 		.Alias("Muon0", "Muon#0.index")
-		# define the muon collection
-		.Define("muons",  "getRP(Muon0, ReconstructedParticles)") #dont get it
-		.Define("n_muons",  "getRP_n(muons)")
-		.Define("pT_muons",  "getRP_pt(muons)")
-		#select electrons with pT > 20 GeV
+		.Define("muons",  "getRP(Muon0, ReconstructedParticles)")
+		.Define("n_muons",  "getRP_n(muons)") #count how many muons are in the event in total
+
+		#OBJECT SELECTION: Consider only those objects that have pT > certain threshold
+		.Define("selected_jets", "selRP_pT(50.)(Jet)") #select only jets with a pT > 50 GeV
+		.Define("selected_electrons", "selRP_pT(20.)(electrons)") #select only electrons with a pT > 20 GeV
 		.Define("selected_muons", "selRP_pT(20.)(muons)")
-		.Define("n_muons_sel", "getRP_n(selected_muons)")
-		# .Define("n_electrons_sel", "return selected_electrons.size()") #why does this not work?
-		.Define("pT_muons_sel",  "getRP_pt(selected_muons)")
+		
+		#SIMPLE VARIABLES: Access the basic kinematic variables of the selected jets, works analogously for electrons, muons
+		.Define("seljet_pT",     "getRP_pt(selected_jets)") #transverse momentum pT
+		.Define("seljet_eta",     "getRP_eta(selected_jets)") #pseudorapidity eta
+		.Define("seljet_phi",     "getRP_phi(selected_jets)") #polar angle in the transverse plane phi
 
-		#LEPTONS:
-		.Define("n_leps",  "getRP_n(muons)+getRP_n(electrons)")
-		.Define("n_leps_sel",  "getRP_n(selected_muons)+getRP_n(selected_electrons)")
-
-
-		#MISSING TRANSVERSE ENERGY
-		.Define("MET", "getRP_pt(MissingET)")
+		#EVENTWIDE VARIABLES: Access quantities that exist only once per event, such as the missing transverse energy
+		.Define("MET", "getRP_pt(MissingET)") #absolute value of MET
+		.Define("MET_x", "getRP_px(MissingET)") #x-component of MET
+		.Define("MET_y", "getRP_py(MissingET)") #y-component of MET
+		.Define("MET_phi", "getRP_phi(MissingET)") #angle of MET
+	
 		)
 
 		# select branches for output file
 		branchList = ROOT.vector('string')()
 		for branchName in [
+						"n_jets", 
+						"n_photons",
 						"n_electrons",
-						"n_electrons_sel",
-						"pT_electrons",
-						"pT_electrons_sel",
 						"n_muons",
-						"n_muons_sel",
-						"pT_muons",
-						"pT_muons_sel",
-						"n_leps",
-						"n_leps_sel",
+						"seljet_pT", 
+						"seljet_eta",
+						"seljet_phi", 
 						"MET",
+						"MET_x",
+						"MET_y",
+						"MET_phi",
 						]:
 			branchList.push_back(branchName)
 		df2.Snapshot("events", self.outname, branchList)
 
 if __name__ == "__main__":
 
-	#TODO: UPDATE AND ADD A TESTER FILE TO THE REPO SOMEHOW? 
+	#TODO: UPDATE TO USE A DEDICATED TESTER FILE? 
 	default_input_tester = "/eos/experiment/fcc/hh/generation/DelphesEvents/fcc_v04/pwp8_pp_hh_lambda100_5f_hhbbzz_zleptonic/events_000087952.root"
-	default_out_dir = "./"
+	default_out_dir = "./read_EDM4HEP/"
 
 	#parse input arguments:
 	parser = argparse.ArgumentParser(description="Basic example how to access objects and simple variables with FCCAnalyses.")
 	parser.add_argument('--input', '-i', metavar="INPUTFILE", dest="input_file", default=default_input_tester, help="Path to the input file. If not specified, runs over a default tester file.")
-	parser.add_argument('--output', '-o', metavar="OUTPUTDIR", dest="out_dir", default=default_out_dir, help="Output directory. If not specified, sets to current directory.")
+	parser.add_argument('--output', '-o', metavar="OUTPUTDIR", dest="out_dir", default=default_out_dir, help="Output directory. If not specified, sets to a subdirectory called read_EDM4HEP in the current working directory.")
 	args = parser.parse_args()
 
 	#create the output dir, if it doesnt exist yet:
@@ -129,7 +129,7 @@ if __name__ == "__main__":
 	print("Input file: ", args.input_file)
 	print("Output file: ", output_file)
 
-	ncpus = 1
+	ncpus = 4
 	analysis = analysis(args.input_file, output_file, ncpus)
 	analysis.run()
 
