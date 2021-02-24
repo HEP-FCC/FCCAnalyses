0a7c0182e1373e69ebbc2a1ab59e99bcfd603bd1
clementhelsens <clement.helsens@cern.ch>
2021-02-24 17:56:28 +0100

    add namespace Algorithms to analysers

--- examples/FCCee/top/hadronic/analysis.py
+++ examples/FCCee/top/hadronic/analysis.py
@@ -8,7 +8,7 @@ ROOT.gSystem.Load("libFCCAnalyses")
 ROOT.gErrorIgnoreLevel = ROOT.kFatal
 _edm  = ROOT.edm4hep.ReconstructedParticleData()
 _pod  = ROOT.podio.ObjectID()
-_fcc  = ROOT.dummyloader
+_fcc  = ROOT.dummyLoader
 
 print ('edm4hep  ',_edm)
 print ('podio    ',_pod)
@@ -47,24 +47,24 @@ class analysis():
                .Define("JET_btag",       "ReconstructedParticle::getJet_btag(Jet3, ParticleIDs, ParticleIDs_0)")
                .Define("EVT_nbtag",      "ReconstructedParticle::getJet_ntags(JET_btag)")
                
-               .Define('EVT_thrust',     'minimize_thrust("Minuit2","Migrad")(RP_px, RP_py, RP_pz)')
-               .Define('RP_thrustangle', 'axisCosTheta(EVT_thrust, RP_px, RP_py, RP_pz)')
+               .Define('EVT_thrust',     'Algorithms::minimize_thrust("Minuit2","Migrad")(RP_px, RP_py, RP_pz)')
+               .Define('RP_thrustangle', 'Algorithms::getAxisCosTheta(EVT_thrust, RP_px, RP_py, RP_pz)')
                .Define('EVT_thrust_x',   "EVT_thrust.at(0)")
                .Define('EVT_thrust_y',   "EVT_thrust.at(1)")
                .Define('EVT_thrust_z',   "EVT_thrust.at(2)")
                .Define('EVT_thrust_val', "EVT_thrust.at(3)")
                
-               .Define('EVT_sphericity',     'minimize_sphericity("Minuit2","Migrad")(RP_px, RP_py, RP_pz)')
+               .Define('EVT_sphericity',     'Algorithms::minimize_sphericity("Minuit2","Migrad")(RP_px, RP_py, RP_pz)')
                .Define('EVT_sphericity_x',   "EVT_sphericity.at(0)")
                .Define('EVT_sphericity_y',   "EVT_sphericity.at(1)")
                .Define('EVT_sphericity_z',   "EVT_sphericity.at(2)")
                .Define('EVT_sphericity_val', "EVT_sphericity.at(3)")
-               .Define('RP_sphericityangle', 'axisCosTheta(EVT_sphericity, RP_px, RP_py, RP_pz)')
+               .Define('RP_sphericityangle', 'Algorithms::getAxisCosTheta(EVT_sphericity, RP_px, RP_py, RP_pz)')
 
-               .Define('RP_hemis0_mass',   "getAxisMass(0)(RP_thrustangle, RP_e, RP_px, RP_py, RP_pz)")
-               .Define('RP_hemis1_mass',   "getAxisMass(1)(RP_thrustangle, RP_e, RP_px, RP_py, RP_pz)")
+               .Define('RP_hemis0_mass',   "Algorithms::getAxisMass(0)(RP_thrustangle, RP_e, RP_px, RP_py, RP_pz)")
+               .Define('RP_hemis1_mass',   "Algorithms::getAxisMass(1)(RP_thrustangle, RP_e, RP_px, RP_py, RP_pz)")
 
-               .Define("RP_total_mass",    "getMass(ReconstructedParticles)")
+               .Define("RP_total_mass",    "Algorithms::getMass(ReconstructedParticles)")
 
         )
 
