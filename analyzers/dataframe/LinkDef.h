af5a0bf6e5e74eb5d56d73b9474b1bbb19302bbc
clementhelsens <clement.helsens@cern.ch>
2021-02-24 17:52:53 +0100

    clean up the linkdef

--- analyzers/dataframe/LinkDef.h
+++ analyzers/dataframe/LinkDef.h
@@ -1,10 +1,12 @@
 #ifdef __CINT__
 
+//Globals
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
+//Dictionaries for output objects
 #pragma link C++ class vector<TLorentzVector>+;
 #pragma link C++ class ROOT::VecOps::RVec<TLorentzVector>+;
 #pragma link C++ class std::vector<std::string>+;
@@ -14,31 +16,7 @@
 #pragma link C++ class ROOT::VecOps::RVec<edm4hep::Vector3d>+;
 #pragma link C++ class ROOT::VecOps::RVec<edm4hep::MCParticleData>+;
 
-
-#pragma link C++ function dummyloader;
-
-//Algo
-#pragma link C++ class thrustFit;
-#pragma link C++ class minimize_thrust;
-#pragma link C++ class getRP_combination;
-
-
-//MC Particles
-#pragma link C++ function getMC_p;
-#pragma link C++ function MCParticles::get_px;
-//#pragma link C++ class MCParticle::get_px;
-#pragma link C++ function getMC_py;
-#pragma link C++ function getMC_pz;
-#pragma link C++ function getMC_e;
-
-//Reconstructed Particles to tracks
-#pragma link C++ function getRPTRK_D0;
-#pragma link C++ function getRP2TRK_Z0;
-
-#pragma link C++ function getRP2MC_p;
-#pragma link C++ class getRP2MC_p_func;
-
-//Vertex
-#pragma link C++ function get_nTracks;
+//to load all other functions
+#pragma link C++ function dummyLoader;
 
 #endif
