d9fb376007543eb657ca62f656889a74ebcc306e
clementhelsens <clement.helsens@cern.ch>
2021-03-02 11:13:43 +0100

    remove localFCCana

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
 spack load acts@5.0.0 arch=linux-centos7-x86_64
 
 #spack load acts@5.00.0 build_type=Debug
-export LOCALFCCANALYSES=$PWD/install/include/FCCAnalyses
+#export LOCALFCCANALYSES=$PWD/install/include/FCCAnalyses
 spack load py-pyyaml
