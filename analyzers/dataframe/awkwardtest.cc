a63b60ba2f711bd67e13cbbfe50e7494337aa7f6
clementhelsens <clement.helsens@cern.ch>
2021-03-04 19:01:55 +0100

    add back working example

--- analyzers/dataframe/awkwardtest.cc
+++ analyzers/dataframe/awkwardtest.cc
@@ -14,6 +14,16 @@
 bool awkwardtest(){
 
 
+  //This works
+  int at = std::atoi("-2");
+  
+  std::shared_ptr<awkward::Content> input = awkward::FromJsonString("[[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6]]", awkward::ArrayBuilderOptions(1024, 2.0));
+  std::shared_ptr<awkward::Content> selection = input.get()->getitem_at(at);
+  
+  std::cout << selection.get()->tojson(false, 1) << std::endl;
+
+
+  //This not yet fully clear
   std::vector<std::vector<std::vector<double>>> vector ={{{0.0, 1.1, 2.2}, {}, {3.3, 4.4}}, {{5.5}}, {},{{6.6, 7.7, 8.8, 9.9}}};
 
   awkward::ArrayBuilder builder(awkward::ArrayBuilderOptions(1024, 2.0));
@@ -30,7 +40,7 @@ bool awkwardtest(){
   slice.append(awkward::SliceRange(awkward::Slice::none(), awkward::Slice::none(), 2));// ::2
   slice.append(awkward::SliceRange(1, awkward::Slice::none(), awkward::Slice::none()));
   
-  std::cout << "- ---- ----  " <<array.get()->getitem(slice).get()->tojson(false, 1) << std::endl;
+  std::cout << "-" <<array.get()->getitem(slice).get()->tojson(false, 1) << std::endl;
   
   //std::shared_ptr<awkward::Content> input = awkward::FromJsonString("[[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6]]", awkward::ArrayBuilderOptions(1024, 2.0));
   //std::shared_ptr<awkward::ArrayBuilderOptions> options;
