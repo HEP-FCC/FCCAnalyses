1560d5c4b27f9dd03d753c0ca0d7fdf48c701aa6
clementhelsens <clement.helsens@cern.ch>
2021-03-05 17:45:27 +0100

    push some updates

--- analyzers/dataframe/awkwardtest.cc
+++ analyzers/dataframe/awkwardtest.cc
@@ -2,69 +2,59 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 #include "awkward/Content.h"
 #include "awkward/io/json.h"
-
+#include "awkward/array/NumpyArray.h"
 #include "awkward/builder/ArrayBuilder.h"
 #include "awkward/builder/ArrayBuilderOptions.h"
-#include <vector>
+
+#include "VertexFitterActs.h"
 
 
-bool awkwardtest(){
+bool awkwardtest(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recop,  
+		 ROOT::VecOps::RVec<edm4hep::TrackState> tracks){
 
 
-  //This works
-  int at = std::atoi("-2");
-  
-  std::shared_ptr<awkward::Content> input = awkward::FromJsonString("[[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6]]", awkward::ArrayBuilderOptions(1024, 2.0));
-  std::shared_ptr<awkward::Content> selection = input.get()->getitem_at(at);
-  
-  std::cout << selection.get()->tojson(false, 1) << std::endl;
+  ROOT::VecOps::RVec<int> rp_ind;
+  ROOT::VecOps::RVec<int> tk_ind;
 
-
-  //This not yet fully clear
-  std::vector<std::vector<std::vector<double>>> vector ={{{0.0, 1.1, 2.2}, {}, {3.3, 4.4}}, {{5.5}}, {},{{6.6, 7.7, 8.8, 9.9}}};
-
+  for (size_t i = 0; i < recop.size(); ++i) {
+    auto & p = recop[i];
+    if (p.tracks_begin<tracks.size()) {
+      rp_ind.push_back(i);
+      tk_ind.push_back(p.tracks_begin);
+    }
+  }
+     
   awkward::ArrayBuilder builder(awkward::ArrayBuilderOptions(1024, 2.0));
+  for (size_t i = 0; i < rp_ind.size(); ++i) {
+    builder.beginlist();
+    builder.integer(rp_ind.at(i));
+    builder.integer(tk_ind.at(i));
+    builder.endlist();
+  }
 
-  builder.real(2.);
-  
-  // for(auto x : vector)
-  //  builder.extend(x);
   std::shared_ptr<awkward::Content> array = builder.snapshot();
+  std::shared_ptr<awkward::Content> comb  = array.get()->combinations(3, false, nullptr, awkward::util::Parameters(), 0, 0);
 
-  awkward::Slice slice;
-  slice.append(awkward::SliceRange(awkward::Slice::none(), awkward::Slice::none(), -1));
-  // ::-1
-  slice.append(awkward::SliceRange(awkward::Slice::none(), awkward::Slice::none(), 2));// ::2
-  slice.append(awkward::SliceRange(1, awkward::Slice::none(), awkward::Slice::none()));
-  
-  std::cout << "-" <<array.get()->getitem(slice).get()->tojson(false, 1) << std::endl;
-  
-  //std::shared_ptr<awkward::Content> input = awkward::FromJsonString("[[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6]]", awkward::ArrayBuilderOptions(1024, 2.0));
-  //std::shared_ptr<awkward::ArrayBuilderOptions> options;
-  //options.resize(2);
-  //awkward::ArrayBuilderOptions options2(1024, 2.0);
-  
-  //std::shared_ptr<awkward::Content> input = awkward::ArrayBuilder(options2);
-  
-  //std::shared_ptr<awkward::Content> options = std::shared_ptr<awkward::ArrayBuilderOptions(1024, 2.0)>;
-  //std::shared_ptr<awkward::Content> input = awkward::ArrayBuilder(options);
-  //input.extend([1.1, 2.2, 3.3]);
-  //input.extend([1.1554, 2.232, 3.343, 9.039]);
-  //input.extend([]);
-  //input.extend([8.0]);
+  int64_t length = comb->length();
+  std::cout << "ntracks " << tracks.size()<< "  length 3 comb " << length << std::endl;
 
+  for (int64_t i=0;i<length;i++){
+    std::shared_ptr<awkward::Content> sel = comb->getitem_at(i);
 
-  //std::cout <<  input << std::endl;
+//awkward::NumpyArray test = sel->data();
+    std::cout << "json "<<sel->tojson(false, 1) << std::endl;
+    std::cout << "sel len " << sel->length() << std::endl;
+//sel->data()
+    //const std::string val = sel.get()->tojson(false, 1);
+   
+    //std::cout <<"str " << s << std::endl;
+    //VertexingUtils::FCCAnalysesVertex TheVertex = VertexFitterActs::VertexFitterFullBilloir(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recoparticles,
+    //											    ROOT::VecOps::RVec<edm4hep::TrackState> thetracks ){
 
-  //std::shared_ptr<awkward::Content> selection = input.get()->getitem_at(at);
-
-
-  
-  
-  //std::cout << selection.get()->tojson(false, 1) << std::endl;
-
+  } 
   return 0;
 }
--- analyzers/dataframe/awkwardtest.h
+++ analyzers/dataframe/awkwardtest.h
@@ -1,7 +1,11 @@
 #ifndef AWKWARDTEST_ANALYZERS_H
 #define AWKWARDTEST_ANALYZERS_H
+#include "ROOT/RVec.hxx"
+#include "edm4hep/ReconstructedParticleData.h"
+#include "edm4hep/TrackState.h"
 
 //namespace awkwardtest{
-  bool awkwardtest();
+  bool awkwardtest(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recop,  
+		 ROOT::VecOps::RVec<edm4hep::TrackState> tracks);
 //}
 #endif
