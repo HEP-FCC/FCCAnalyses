a0da247edb4e5ed20bdcef8cf06c5a231b7b244c
Birgit Stapf <bstapf@nikhef.nl>
2021-03-15 18:31:43 +0100

    update to namespace notation: fix library loading header

--- examples/basics/read_EDM4HEP.py
+++ examples/basics/read_EDM4HEP.py
@@ -16,7 +16,7 @@ ROOT.gSystem.Load("libFCCAnalyses")
 ROOT.gErrorIgnoreLevel = ROOT.kFatal
 _edm  = ROOT.edm4hep.ReconstructedParticleData()
 _pod  = ROOT.podio.ObjectID()
-_fcc  = ROOT.getMC_px
+_fcc  = ROOT.dummyLoader
 
 print ('edm4hep  ',_edm)
 print ('podio    ',_pod)
