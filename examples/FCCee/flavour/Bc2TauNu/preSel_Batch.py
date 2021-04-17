6455e18943c13a29d768161fbf09d08941e781c0
clementhelsens <clement.helsens@cern.ch>
2021-04-17 09:37:19 +0200

    adding more macros

---
+++ examples/FCCee/flavour/Bc2TauNu/preSel_Batch.py
@@ -0,0 +1,23 @@
+#python examples/FCCee/flavour/Bc2TauNu/preSel_Batch.py
+
+from config.common_defaults import deffccdicts
+import config.runDataFrameBatch as rdf
+import os
+
+basedir=os.path.join(os.getenv('FCCDICTSDIR', deffccdicts), '') + "yaml/FCCee/fcc_tmp_v03/"
+outdir="/eos/experiment/fcc/ee/analyses/case-studies/flavour/Bc2TauNu/flatNtuples_batch/"
+NUM_CPUS=8
+output_list=[]
+
+fraction=1.
+
+process_list=['p8_ee_Zbb_ecm91',
+              'p8_ee_Zcc_ecm91',
+              'p8_ee_Zuds_ecm91']
+myana=rdf.runDataFrameBatch(basedir,process_list, outlist=output_list)
+myana.run(ncpu=NUM_CPUS,fraction=fraction, chunks=200 ,outDir=outdir)
+
+process_list=['p8_ee_Zbb_ecm91_EvtGen_Bc2TauNuTAUHADNU',    
+              'p8_ee_Zbb_ecm91_EvtGen_Bu2TauNuTAUHADNU']
+myana=rdf.runDataFrameBatch(basedir,process_list, outlist=output_list)
+myana.run(ncpu=NUM_CPUS,fraction=fraction, chunks=2 ,outDir=outdir)
