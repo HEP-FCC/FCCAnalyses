7fbbb1dbaf76b432cf05ed30d31b0e691c661e06
clementhelsens <clement.helsens@cern.ch>
2021-04-24 10:42:00 +0200

    add plot wiht yields

--- config/doPlots.py
+++ config/doPlots.py
@@ -37,7 +37,6 @@ def mapHistos(var, label, sel, param):
                     print ('no scale signal, using 1')
                 print ('scaleSig ',scaleSig)
                 hh.Scale(param.intLumi*scaleSig)
-                print('signal ',s,'  nevents  ',hh.Integral(0,-1)/scaleSig,'  MC events  ',hh.GetEntries())
 
                 if len(hsignal[s])==0:
                     hsignal[s].append(hh)
@@ -58,7 +57,6 @@ def mapHistos(var, label, sel, param):
                 h=tf.Get(var)
                 hh = copy.deepcopy(h)
                 hh.Scale(param.intLumi)
-                print('background ',b,'  nevents  ',hh.Integral(0,-1),'  MC events  ',hh.GetEntries())
                 if len(hbackgrounds[b])==0:
                     hbackgrounds[b].append(hh)
                 else:
@@ -76,7 +74,7 @@ def mapHistos(var, label, sel, param):
     return hsignal,hbackgrounds
 
 #__________________________________________________________
-def runPlots(var,param,hsignal,hbackgrounds,extralab):
+def runPlots(var,sel,param,hsignal,hbackgrounds,extralab):
     legsize = 0.04*(len(hbackgrounds)+len(hsignal))
     leg = ROOT.TLegend(0.58,0.86 - legsize,0.86,0.88)
     leg.SetFillColor(0)
@@ -91,6 +89,12 @@ def runPlots(var,param,hsignal,hbackgrounds,extralab):
     for b in hbackgrounds:
         leg.AddEntry(hbackgrounds[b][0],param.legend[b],"f")
  
+
+    yields={}
+    for s in hsignal:
+        yields[s]=[param.legend[s],hsignal[s][0].Integral(0,-1), hsignal[s][0].GetEntries()]
+    for b in hbackgrounds:
+        yields[b]=[param.legend[b],hbackgrounds[b][0].Integral(0,-1), hbackgrounds[b][0].GetEntries()]
 
     histos=[]
     colors=[]
@@ -114,19 +118,23 @@ def runPlots(var,param,hsignal,hbackgrounds,extralab):
         rt = "#sqrt{{s}} = {:.1f} GeV,   L = {:.0f} ab^{{-1}}".format(param.energy,intLumiab)
 
 
+    if 'AAAyields' in var:
+        drawStack(var, 'events', leg, lt, rt, param.formats, param.outdir+"/"+sel, False , True , histos, colors, param.ana_tex, extralab, param.scaleSig, yields)
+        return
+        
     if 'stack' in param.stacksig:
         if 'lin' in param.yaxis:
-            drawStack(var+"_stack_lin", 'events', leg, lt, rt, param.formats, param.outdir, False , True , histos, colors, param.ana_tex, extralab)
+            drawStack(var+"_stack_lin", 'events', leg, lt, rt, param.formats, param.outdir+"/"+sel, False , True , histos, colors, param.ana_tex, extralab, param.scaleSig)
         if 'log' in param.yaxis:
-            drawStack(var+"_stack_log", 'events', leg, lt, rt, param.formats, param.outdir, True , True , histos, colors, param.ana_tex, extralab)
+            drawStack(var+"_stack_log", 'events', leg, lt, rt, param.formats, param.outdir+"/"+sel, True , True , histos, colors, param.ana_tex, extralab, param.scaleSig)
         if 'lin' not in param.yaxis and 'log' not in param.yaxis:
             print ('unrecognised option in formats, should be [\'lin\',\'log\']'.format(param.formats))
 
     if 'nostack' in param.stacksig:
         if 'lin' in param.yaxis:
-            drawStack(var+"_nostack_lin", 'events', leg, lt, rt, param.formats, param.outdir, False , False , histos, colors, param.ana_tex, extralab)
+            drawStack(var+"_nostack_lin", 'events', leg, lt, rt, param.formats, param.outdir+"/"+sel, False , False , histos, colors, param.ana_tex, extralab, param.scaleSig)
         if 'log' in param.yaxis:
-            drawStack(var+"_nostack_log", 'events', leg, lt, rt, param.formats, param.outdir, True , False , histos, colors, param.ana_tex, extralab)
+            drawStack(var+"_nostack_log", 'events', leg, lt, rt, param.formats, param.outdir+"/"+sel, True , False , histos, colors, param.ana_tex, extralab, param.scaleSig)
         if 'lin' not in param.yaxis and 'log' not in param.yaxis:
             print ('unrecognised option in formats, should be [\'lin\',\'log\']'.format(param.formats))
     if 'stack' not in param.stacksig and 'nostack' not in param.stacksig:
@@ -134,8 +142,10 @@ def runPlots(var,param,hsignal,hbackgrounds,extralab):
 
 
 #_____________________________________________________________________________________________________________
-def drawStack(name, ylabel, legend, leftText, rightText, formats, directory, logY, stacksig, histos, colors, ana_tex, extralab):
+def drawStack(name, ylabel, legend, leftText, rightText, formats, directory, logY, stacksig, histos, colors, ana_tex, extralab, scaleSig, yields=None):
 
+
+    
     canvas = ROOT.TCanvas(name, name, 600, 600) 
     canvas.SetLogy(logY)
     canvas.SetTicks(1,1)
@@ -306,7 +316,7 @@ def drawStack(name, ylabel, legend, leftText, rightText, formats, directory, log
 
 
     if not stacksig:
-        histos[0].Draw("same hist")
+        if 'AAAyields' not in name: histos[0].Draw("same hist")
    
     #legend.SetTextFont(font) 
     legend.Draw() 
@@ -348,6 +358,81 @@ def drawStack(name, ylabel, legend, leftText, rightText, formats, directory, log
     canvas.Modified()
     canvas.Update()
 
+    if 'AAAyields' in name:
+        dummyh=ROOT.TH1F("","",1,0,1)
+        dummyh.SetStats(0)
+        dummyh.GetXaxis().SetLabelOffset(999)
+        dummyh.GetXaxis().SetLabelSize(0)
+        dummyh.GetYaxis().SetLabelOffset(999)
+        dummyh.GetYaxis().SetLabelSize(0)
+        dummyh.Draw("AH")
+        legend.Draw()
+        
+        Text.SetNDC() 
+        Text.SetTextAlign(31);
+        Text.SetTextSize(0.04) 
+
+        text = '#it{' + leftText +'}'
+        Text.DrawLatex(0.90, 0.92, text)
+        
+        text = '#bf{#it{' + rightText[0] +'}}'
+        Text.SetTextAlign(12);
+        Text.SetNDC(ROOT.kTRUE) 
+        Text.SetTextSize(0.04) 
+        Text.DrawLatex(0.18, 0.83, text)
+
+        text = '#bf{#it{' + rightText[1] +'}}'
+        Text.SetTextSize(0.035) 
+        Text.DrawLatex(0.18, 0.78, text)
+
+        text = '#bf{#it{' + ana_tex +'}}'
+        Text.SetTextSize(0.04)
+        Text.DrawLatex(0.18, 0.73, text)
+        
+        text = '#bf{#it{' + extralab +'}}'
+        Text.SetTextSize(0.025)
+        Text.DrawLatex(0.18, 0.68, text)
+
+        text = '#bf{#it{' + 'Signal scale=' + str(scaleSig)+'}}'
+        Text.SetTextSize(0.04)
+        Text.DrawLatex(0.18, 0.55, text)
+
+        dy=0
+        text = '#bf{#it{' + 'Process' +'}}'
+        Text.SetTextSize(0.035)
+        Text.DrawLatex(0.18, 0.45, text)
+        
+        text = '#bf{#it{' + 'Yields' +'}}'
+        Text.SetTextSize(0.035)
+        Text.DrawLatex(0.5, 0.45, text)
+        
+        text = '#bf{#it{' + 'Raw MC' +'}}'
+        Text.SetTextSize(0.035)
+        Text.DrawLatex(0.75, 0.45, text)
+
+        for y in yields:
+            text = '#bf{#it{' + yields[y][0] +'}}'
+            Text.SetTextSize(0.035)
+            Text.DrawLatex(0.18, 0.4-dy*0.05, text)
+
+            stry=str(yields[y][1])
+            stry=stry.split('.')[0]
+            text = '#bf{#it{' + stry +'}}'
+            Text.SetTextSize(0.035)
+            Text.DrawLatex(0.5, 0.4-dy*0.05, text)
+            
+            stry=str(yields[y][2])
+            stry=stry.split('.')[0]
+            text = '#bf{#it{' + stry +'}}'
+            Text.SetTextSize(0.035)
+            Text.DrawLatex(0.75, 0.4-dy*0.05, text)
+
+            
+            dy+=1
+        #canvas.Modified()
+        #canvas.Update()
+       
+        
     printCanvas(canvas, name, formats, directory) 
 
     
@@ -379,9 +464,11 @@ if __name__=="__main__":
     sys.path.insert(0, module_dir)
     param = importlib.import_module(base_name)
         
-
+    counter=0
     for var in param.variables:
         for label, sels in param.selections.items():
             for sel in sels:
                 hsignal,hbackgrounds=mapHistos(var,label,sel, param)
-                runPlots(var+"_"+label+"_"+sel,param,hsignal,hbackgrounds,param.extralabel[sel])
+                runPlots(var+"_"+label,sel,param,hsignal,hbackgrounds,param.extralabel[sel])
+                if counter==0: runPlots("AAAyields_"+label,sel,param,hsignal,hbackgrounds,param.extralabel[sel])
+        counter+=1
