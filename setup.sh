4b38b987e4becfcdb98af6ac1c990286e7159cd2
Valentin Volkl <valentin.volkl@cern.ch>
2021-05-03 13:18:42 +0200

    Simplify setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,4 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
 
-spack load acts@5.0.0 arch=linux-centos7-x86_64
 
-#spack load acts@5.00.0 build_type=Debug
-#export LOCALFCCANALYSES=$PWD/install/include/FCCAnalyses
-spack load py-pyyaml
