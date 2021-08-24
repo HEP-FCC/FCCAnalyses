bb93279ca4cb71d25a9317fe747035c6c9c08a4d
Brieuc Francois <brieucfrancois@hotmail.com>
2021-08-24 14:36:43 +0200

    [AccessDD4hepGeometry] Cleaning

--- analyzers/dataframe/CaloNtupleizer.h
+++ analyzers/dataframe/CaloNtupleizer.h
@@ -13,27 +13,9 @@
 #include "TVector3.h"
 #include "TLorentzVector.h"
 
-#include "DD4hep/Detector.h"
-
 namespace CaloNtupleizer{
 
-//class geometry_toolbox {
-//  public:
-//    dd4hep::DDSegmentation::BitFieldCoder* m_decoder;
-//    geometry_toolbox(std::string xmlGeometryPath) {
-//      dd4hep::Detector* dd4hepgeo = &(dd4hep::Detector::getInstance());
-//      dd4hepgeo->fromCompact(xmlGeometryPath);
-//      dd4hepgeo->volumeManager();
-//      dd4hepgeo->apply("DD4hepVolumeManager", 0, 0);
-//      m_decoder = dd4hepgeo->readout("ECalBarrelPhiEta").idSpec().decoder();
-//    }
-//};
-
 void loadGeometry(std::string xmlGeometryPath);
-
-//dd4hep::DDSegmentation::BitFieldCoder* m_decoder;
-//dd4hep::Detector* dd4hepgeo;
-//IDDescriptors
 
 // calo hits (single cells)
 ROOT::VecOps::RVec<float> getCaloHit_x (ROOT::VecOps::RVec<edm4hep::CalorimeterHitData> in);
