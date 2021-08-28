0329ee22a4ea2e09b7cefc7e114965cd1d360733
Kunal Gautam <kgautam@lxplus724.cern.ch>
2021-08-28 14:02:40 +0200

    Zbb events

--- ZqqAnalysis/analyzer_KG.C
+++ ZqqAnalysis/analyzer_KG.C
@@ -7,12 +7,15 @@
 #include <TH1.h>
 #include <TMath.h>
 #include <TLorentzVector.h>
+#include <TSystem.h>
 #include <TInterpreter.h>
 
 using namespace std;
 
 int main()
 {
+  gInterpreter->GenerateDictionary("vector<vector<int> >","vector");
+
   TFile* file = new TFile("p8_ee_Zbb_ecm91.root","READ");
   TTree* tree = (TTree*) file->Get("events");
   int nEvents = tree->GetEntries();
@@ -21,8 +24,6 @@ int main()
   TString histfname;
   histfname = "histZbb.root";
   TFile *histFile = new TFile(histfname,"RECREATE");
-
-  gInterpreter->GenerateDictionary("vector<vector<int> >","vector");
 
   // hists for the particle loop
   TH1D* h_nb =new TH1D("h_nb","b and #bar{b} in the event",10,0,10);
@@ -34,10 +35,7 @@ int main()
   TH1D* h_theta_bqbar =new TH1D("h_theta_bqbar","#theta of #bar{b}",100,0,3.15);
   TH1D* h_invMb =new TH1D("h_invMb","Invariant Mass - b [GeV]",100,4.79,4.81);
   TH1D* h_invM2b =new TH1D("h_invM2b","Invariant Mass - b and #bar{b} [GeV]",100,86.,91.5);
-  TH1D* h_status =new TH1D("h_status","Gen Status",200,-100,100);
-  TH1D* h_Zpdg =new TH1D("h_Zpdg","PDG code for the mother of b and #bar{b}",50,-25,25);
-  TH1D* h_parent_indx =new TH1D("h_parent_indx","Parent Indices",250,0,250);
-
+  
   // hists for the jet loop
   TH1D* h_njet =new TH1D("h_njet","Jet Multiplicity",25,0,25);
   TH1D* h_pjet =new TH1D("h_pjet","|p| - jet [GeV]",47,0,47);
@@ -48,12 +46,15 @@ int main()
   TH1D* h_Ejet =new TH1D("h_Ejet","Energy - jet [GeV]",47,0,47);
   
   // hists for the jet constituents
-  TH1D* h_pbT_jets =new TH1D("h_pbT_jets","p_{T} of b [GeV] (jet constituents)",47,0,47);
-  TH1D* h_pb_jets =new TH1D("h_pb_jets","|p| of b [GeV] (jet constituents)",60,40,46);
-  TH1D* h_thetab_jets =new TH1D("h_thetab_jets","#theta of b (jet constituents)",100,0,3.15);
+  TH1D* h_deltaAngKs1 =new TH1D("h_deltaAngKs1","Angle between K_{S} and jet axis",100,0,3.15);
+  TH1D* h_deltaAngKs2 =new TH1D("h_deltaAngKs2","Angle between K_{S} and jet axis",100,0,3.15);
+  TH1D* h_deltaThetaKs1 =new TH1D("h_deltaThetaKs1","#Delta #theta between K_{S} and jet axis",100,0,3.15);
+  TH1D* h_deltaPhiKs1 =new TH1D("h_deltaPhiKs1","#Delta #phi between K_{S} and jet axis",100,0,3.15);
+  TH1D* h_deltaThetaKs2 =new TH1D("h_deltaThetaKs2","#Delta #theta between K_{S} and jet axis",100,0,3.15);
+  TH1D* h_deltaPhiKs2 =new TH1D("h_deltaPhiKs2","#Delta #phi between K_{S} and jet axis",100,0,3.15);
   
   vector<float> *MCpx=0, *MCpy=0, *MCpz=0, *MCp=0, *MCe=0, *MCpdg=0, *MCstatus=0;
-  vector<int> *MCparent_indx=0;
+  // Particle branches
   tree->SetBranchAddress("MC_px", &MCpx);
   tree->SetBranchAddress("MC_py", &MCpy);
   tree->SetBranchAddress("MC_pz", &MCpz);
@@ -62,22 +63,31 @@ int main()
   tree->SetBranchAddress("MC_pdg", &MCpdg);
   tree->SetBranchAddress("MC_status", &MCstatus);
 
-  tree->SetBranchAddress("MC_parent", &MCparent_indx);
+  vector<float> *MCpxF=0, *MCpyF=0, *MCpzF=0, *MCeF=0, *MCpdgF=0;
+  // Final particle branches
+  tree->SetBranchAddress("MC_px_f", &MCpxF);
+  tree->SetBranchAddress("MC_py_f", &MCpyF);
+  tree->SetBranchAddress("MC_pz_f", &MCpzF);
+  tree->SetBranchAddress("MC_e_f", &MCeF);
+  tree->SetBranchAddress("MC_pdg_f", &MCpdgF);
   
   vector<float> *jetE=0, *jetPx=0, *jetPy=0, *jetPz=0;
   vector<vector<int>> *jetConst;
+  // Jet branches (ee-genkt)
   //tree->SetBranchAddress("jetconstituents_ee_genkt", &jetConst);
   //tree->SetBranchAddress("jets_ee_genkt_e", &jetE);
   //tree->SetBranchAddress("jets_ee_genkt_px", &jetPx);
   //tree->SetBranchAddress("jets_ee_genkt_py", &jetPy);
   //tree->SetBranchAddress("jets_ee_genkt_pz", &jetPz);
 
+  // Jet branches (kt)
   //tree->SetBranchAddress("jetconstituents_kt", &jetConst);
   //tree->SetBranchAddress("jets_kt_e", &jetE);
   //tree->SetBranchAddress("jets_kt_px", &jetPx);
   //tree->SetBranchAddress("jets_kt_py", &jetPy);
   //tree->SetBranchAddress("jets_kt_pz", &jetPz);
 
+  // Jet branches (eekt)
   tree->SetBranchAddress("jetconstituents_ee_kt", &jetConst);
   tree->SetBranchAddress("jets_ee_kt_e", &jetE);
   tree->SetBranchAddress("jets_ee_kt_px", &jetPx);
@@ -93,8 +103,6 @@ int main()
       double px=0., py=0., pz=0., p=0., e=0.;
       TLorentzVector p4, p_2b, p_b, p_bbar;
 
-      int b_mot_indx=0, bbar_mot_indx=0, b_mot_pdg=0, bbar_mot_pdg=0;
-      
       // particle loop
       for(unsigned int ctr=0; ctr<MCpdg->size(); ctr++)
 	{
@@ -129,19 +137,13 @@ int main()
 		{
 		  p_b = p4;
 		  h_theta_bq->Fill(p4.Theta());
-		  //b_mot_indx = MCparent_indx->at(ctr);
-		  //b_mot_pdg = MCpdg->at(b_mot_indx);
-		  //h_Zpdg->Fill(b_mot_pdg);
-		}
+	        }
 
 	      // b-bar
 	      if(MCpdg->at(ctr)==-5)
 		{
 		  p_bbar = p4;
 		  h_theta_bqbar->Fill(p4.Theta());
-		  //bbar_mot_indx = MCparent_indx->at(ctr);
-		  //bbar_mot_pdg = MCpdg->at(bbar_mot_indx);
-		  //h_Zpdg->Fill(bbar_mot_pdg);
 		}
 	    }
 
@@ -179,7 +181,7 @@ int main()
 	  // jet theta
 	  h_jetTheta->Fill(p_Jet[j].Theta());
 	  
-	  // jet theta
+	  // jet Phi
 	  h_jetPhi->Fill(p_Jet[j].Phi());
 	  
 	  // jet |p|
@@ -205,69 +207,58 @@ int main()
       cout<<"vector in vector : "<<jet2Const.size()<<endl;*/
 
       // JET 1
-      double px_j1=0, py_j1=0, pz_j1=0, p_j1=0, e_j1=0;
+      double px_j1=0, py_j1=0, pz_j1=0, e_j1=0;
       TLorentzVector p4_j1;
+      
+      double delta_ang1=0.;
 
       for(int ele : jet1Const) 
 	{
 	  //cout<<"jet constituent index: "<<ele<<"\t";
-	  px_j1 = MCpx->at(ele);
-	  py_j1 = MCpy->at(ele);
-	  pz_j1 = MCpz->at(ele);
-	  p_j1 = MCp->at(ele);
-	  e_j1 = MCe->at(ele);
+	  px_j1 = MCpxF->at(ele);
+	  py_j1 = MCpyF->at(ele);
+	  pz_j1 = MCpzF->at(ele);
+	  e_j1 = MCeF->at(ele);
 	  
 	  p4_j1.SetPxPyPzE(px_j1, py_j1, pz_j1, e_j1);
-    
-	  if(MCstatus->at(ele)>20 && MCstatus->at(ele)<30)
+
+	  // angle between Ks and jet axis
+	  if(MCpdgF->at(ele)==310 || MCpdgF->at(ele)==-310)
 	    {
-	      if(MCpdg->at(ele)==5 || MCpdg->at(ele)==-5)
-		{
-		  // pT of b or b-bar
-		  h_pbT_jets->Fill(p4_j1.Pt());
-
-		  // |p| of b or b-bar
-		  h_pb_jets->Fill(p_j1);
-
-		  // polar angle
-		  h_thetab_jets->Fill(p4_j1.Theta());
-		}
+	      delta_ang1 = p4_j1.Angle(p_Jet[0].Vect());
+	      h_deltaAngKs1->Fill(delta_ang1);
+	      h_deltaThetaKs1->Fill(p4_j1.Theta() - p_Jet[0].Theta());
+	      h_deltaPhiKs1->Fill(p4_j1.Phi() - p_Jet[0].Phi());
 	    }
 	}
 
       //      cout<<endl<<"jet 1 ends, jet 2 begins"<<endl;
 
       // JET 2
-      double px_j2=0, py_j2=0, pz_j2=0, p_j2=0, e_j2=0;
+      double px_j2=0, py_j2=0, pz_j2=0, e_j2=0;
       TLorentzVector p4_j2;
+
+      double delta_ang2=0.;
 
       for(int ele : jet2Const) 
 	{
 	  //cout<<"jet constituent index: "<<ele<<"\t";
-	  px_j2 = MCpx->at(ele);
-	  py_j2 = MCpy->at(ele);
-	  pz_j2 = MCpz->at(ele);
-	  p_j2 = MCp->at(ele);
-	  e_j2 = MCe->at(ele);
+	  px_j2 = MCpxF->at(ele);
+	  py_j2 = MCpyF->at(ele);
+	  pz_j2 = MCpzF->at(ele);
+	  e_j2 = MCeF->at(ele);
 	  
 	  p4_j2.SetPxPyPzE(px_j2, py_j2, pz_j2, e_j2);
-    
-	  if(MCstatus->at(ele)>20 && MCstatus->at(ele)<30)
+
+	  // angle between Ks and jet axis
+	  if(MCpdgF->at(ele)==310 || MCpdgF->at(ele)==-310)
 	    {
-	      if(MCpdg->at(ele)==5 || MCpdg->at(ele)==-5)
-		{
-		  // pT of b or b-bar
-		  h_pbT_jets->Fill(p4_j2.Pt());
-
-		  // |p| of b or b-bar
-		  h_pb_jets->Fill(p_j2);
-
-		  // polar angle
-		  h_thetab_jets->Fill(p4_j2.Theta());
-		}
+	      delta_ang2 = p4_j2.Angle(p_Jet[1].Vect());
+	      h_deltaAngKs2->Fill(delta_ang2);
+	      h_deltaThetaKs2->Fill(p4_j2.Theta() - p_Jet[1].Theta());
+	      h_deltaPhiKs2->Fill(p4_j2.Phi() - p_Jet[1].Phi());
 	    }
 	}
-
 
       // invariant mass of the event
       h_invMjets->Fill(p_Jets.M());
@@ -277,13 +268,18 @@ int main()
 	  cout<<"In event #"<<evt+1<<", there are ";
 	  cout<<MCpdg->size()<<" entries."<<endl;
 	  cout<<"This event has "<<nb<<" b and bbar; "<<nJet<<" jets; and "<<n_final<<" final particles."<<endl;
-	  cout<<"MCpx = "<<MCpx->size()<<", MCparent_indx = "<<MCparent_indx->size()<<endl;
 	  cout<<endl<<"====================="<<endl;
 	}
+
+      jet1Const.clear();
+      jet2Const.clear();
     }
 
   histFile->Write();
   histFile->Close();
+
+  delete jetConst;
+  jetConst = NULL;
 
   file->Close();
   return -1;
