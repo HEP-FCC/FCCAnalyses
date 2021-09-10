ad3f3136cdd887f5ccfb2b8aa6ea26a183001e49
Kunal Gautam <kgautam@lxplus724.cern.ch>
2021-09-10 16:36:55 +0200

    jet images per event

--- ZqqAnalysis/analyzer_jetimg_test.C
+++ ZqqAnalysis/analyzer_jetimg_test.C
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <sstream>
+#include <string>
 
+#include <TObject.h>
+#include <TList.h>
 #include <TFile.h>
 #include <TTree.h>
 #include <TH1.h>
@@ -22,26 +26,29 @@ int main()
   int nEvents = tree->GetEntries();
   cout<<"Number of Events: "<<nEvents<<endl;
   
-  TString histfname;
-  histfname = "histZbb_jetImages.root";
-  TFile *histFile = new TFile(histfname,"RECREATE");
-
-  // Hists for jet-constituents' properties
-  //TH1D* h_deltaAngKl = new TH1D("h_deltaAngKl","Angle b/n K_{L} and Jet Axis",100,0.,3.15);
-  
   // hists for jet angluar distributions
-  TH2D *h_JetCKaonB[nEvents];
-  TH2D *h_JetNKaonB[nEvents];
-  TH2D *h_JetCPionB[nEvents];
-  TH2D *h_JetElecB[nEvents];
-  TH2D *h_JetMuonB[nEvents];
-  TH2D *h_JetPhotB[nEvents];
-  TH2D *h_JetProtB[nEvents];
-  TH2D *h_JetNeutB[nEvents];
-  
-  for(int nH=0; nH<nEvents; nH++)
+  int one = 1;
+
+  cout<<"Before defining histograms"<<endl;
+
+  vector<TH2D*> h_JetCKaonB;
+  /*
+  vector<TH2D*> h_JetNKaonB;
+  vector<TH2D*> h_JetCPionB;
+  vector<TH2D*> h_JetElecB;
+  vector<TH2D*> h_JetMuonB;
+  vector<TH2D*> h_JetPhotB;
+  vector<TH2D*> h_JetProtB;
+  vector<TH2D*> h_JetNeutB;
+  */
+  for(int nH=0; nH<one; nH++)
     {
-      h_JetCKaonB[nH] = new TH2D("h_JetCKaonB","K^{+/-} in b jets",29,-0.5,0.5,29,-0.5,0.5);
+      stringstream ss;
+      ss<<"hist_jetCKaon_"<<nH;
+      string s = ss.str();
+      
+      h_JetCKaonB.push_back(new TH2D(s.c_str(),"K^{+/-} in b jets",29,-0.5,0.5,29,-0.5,0.5));
+      /*
       h_JetNKaonB[nH] = new TH2D("h_JetNKaonB","K_{L} in b jets",29,-0.5,0.5,29,-0.5,0.5);
       h_JetCPionB[nH] = new TH2D("h_JetCPionB","#pi^{+/-} in b jets",29,-0.5,0.5,29,-0.5,0.5);
       h_JetElecB[nH] = new TH2D("h_JetElecB","e^{+/-} in b jets",29,-0.5,0.5,29,-0.5,0.5);
@@ -49,7 +56,10 @@ int main()
       h_JetPhotB[nH] = new TH2D("h_JetPhotB","#gamma in b jets",29,-0.5,0.5,29,-0.5,0.5);
       h_JetProtB[nH] = new TH2D("h_JetProtB","p in b jets",29,-0.5,0.5,29,-0.5,0.5);
       h_JetNeutB[nH] = new TH2D("h_JetNeutB","n in b jets",29,-0.5,0.5,29,-0.5,0.5);
+      */
     }
+
+  cout<<"After defining histograms"<<endl;
   
   vector<float> *MCpxF=0, *MCpyF=0, *MCpzF=0, *MCeF=0, *MCpdgF=0;
   tree->SetBranchAddress("MC_px_f", &MCpxF);
@@ -58,34 +68,28 @@ int main()
   tree->SetBranchAddress("MC_e_f", &MCeF);
   tree->SetBranchAddress("MC_pdg_f", &MCpdgF);
   
+  cout<<"Before defining vector of vectors"<<endl;
+
   vector<float> *jetE=0, *jetPx=0, *jetPy=0, *jetPz=0;
   vector<vector<int>> *jetConst;
-  //tree->SetBranchAddress("jetconstituents_ee_genkt", &jetConst);
-  //tree->SetBranchAddress("jets_ee_genkt_e", &jetE);
-  //tree->SetBranchAddress("jets_ee_genkt_px", &jetPx);
-  //tree->SetBranchAddress("jets_ee_genkt_py", &jetPy);
-  //tree->SetBranchAddress("jets_ee_genkt_pz", &jetPz);
-
-  //tree->SetBranchAddress("jetconstituents_kt", &jetConst);
-  //tree->SetBranchAddress("jets_kt_e", &jetE);
-  //tree->SetBranchAddress("jets_kt_px", &jetPx);
-  //tree->SetBranchAddress("jets_kt_py", &jetPy);
-  //tree->SetBranchAddress("jets_kt_pz", &jetPz);
-
   tree->SetBranchAddress("jetconstituents_ee_kt", &jetConst);
   tree->SetBranchAddress("jets_ee_kt_e", &jetE);
   tree->SetBranchAddress("jets_ee_kt_px", &jetPx);
   tree->SetBranchAddress("jets_ee_kt_py", &jetPy);
   tree->SetBranchAddress("jets_ee_kt_pz", &jetPz);
   
+  cout<<"After defining vector of vecotrs... Event loop start"<<endl;
+
   // event loop
-  for(unsigned int evt=0; evt<1000; evt++)
+  for(unsigned int evt=0; evt<one; evt++)
     {
       tree->GetEntry(evt);
 
       double jPx=0., jPy=0., jPz=0., jE=0., invMjet=0.;
       int nJet = jetE->size();
       TLorentzVector p_Jet[nJet], p_Jets;
+
+      //cout<<"Jet loop starts"<<endl;
       
       // jet loop
       for(unsigned int j=0; j<nJet; j++)
@@ -101,12 +105,16 @@ int main()
 	  p_Jets += p_Jet[j];
 	}
 
+      //cout<<"Before writing jet constituents"<<endl;
+
       // jet constituents
       vector<int> jet1Const, jet2Const;
       if(jetConst->size()>=1)      jet1Const = jetConst->at(0);
       else cout<<"No jet constituents found"<<endl;
       if(jetConst->size()>=2)      jet2Const = jetConst->at(1);
       else cout<<"Second jet constituents not found"<<endl;
+
+      //cout<<"after writing jet consitituents"<<endl;
 
       // JET 1
       double px_j1=0, py_j1=0, pz_j1=0, e_j1=0;
@@ -128,16 +136,10 @@ int main()
 	  p_norm1 = p4_j1.P()/p_Jet[0].P();
 	  delta_theta1 = p4_j1.Theta() - p_Jet[0].Theta();
 	  delta_phi1 = p4_j1.Phi() - p_Jet[0].Phi();
-	  /*
-	  if(MCpdgF->at(ele)==130 || MCpdgF->at(ele)==-130)
-	    {
-	      delta_ang1 = p4_j1.Angle(p_Jet[0].Vect());
-	      h_deltaAngKl->Fill(delta_ang1);
-	    }
-	  */
+
 	  // K+-
 	  if(MCpdgF->at(ele)==321 || MCpdgF->at(ele)==-321) h_JetCKaonB[evt]->Fill(delta_theta1,delta_phi1,p_norm1);
-	  
+	  /*
 	  // Kl
 	  if(MCpdgF->at(ele)==130 || MCpdgF->at(ele)==-130) h_JetNKaonB[evt]->Fill(delta_theta1,delta_phi1,p_norm1);
 	  
@@ -158,9 +160,11 @@ int main()
 	  
 	  // n
 	  if(MCpdgF->at(ele)==2112 || MCpdgF->at(ele)==-2112) h_JetNeutB[evt]->Fill(delta_theta1,delta_phi1,p_norm1);
-	  
+	  */
 	}
             
+      //cout<<"End of Jet 1"<<endl;
+
       // JET 2
       double px_j2=0, py_j2=0, pz_j2=0, e_j2=0;
       TLorentzVector p4_j2;
@@ -190,7 +194,7 @@ int main()
 	  */
 	  // K+-
 	  if(MCpdgF->at(ele)==321 || MCpdgF->at(ele)==-321) h_JetCKaonB[evt]->Fill(delta_theta2,delta_phi2,p_norm2);
-	  
+	  /*
 	  // Kl
 	  if(MCpdgF->at(ele)==130 || MCpdgF->at(ele)==-130) h_JetNKaonB[evt]->Fill(delta_theta2,delta_phi2,p_norm2);
 	  
@@ -211,19 +215,40 @@ int main()
 	  
 	  // n
 	  if(MCpdgF->at(ele)==2112 || MCpdgF->at(ele)==-2112) h_JetNeutB[evt]->Fill(delta_theta2,delta_phi2,p_norm2);
-	  
+	  */
 	}
+
+      //cout<<"End of Jet 2"<<endl;
 
       jet1Const.clear();
       jet2Const.clear();
     }
-      
-  histFile->Write();
+
+  cout<<"Event loop ends"<<endl;
+
+  file->Close();
+
+  TList *hist_list = new TList();
+
+  for(TH2D* hist : h_JetCKaonB) hist_list->Add(hist);
+
+  cout<<"defining hist file"<<endl;
+
+  TString histfname;
+  histfname = "histZbb_jetImages.root";
+  TFile *histFile = new TFile(histfname,"RECREATE");
+    
+  hist_list->Write("histZbb_jetImages", TObject::kSingleKey);
+  
+  cout<<"Hist file written"<<endl;
+
+  //histFile->Write();
   histFile->Close();
+
+  cout<<"Hist file closed"<<endl;
       
   delete jetConst;
   jetConst = NULL;
   
-  file->Close();
   return -1;
 }
