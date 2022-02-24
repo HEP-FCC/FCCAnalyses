fbec4744bcc4c98f87335e99861a0130d1206b90
clementhelsens <clement.helsens@cern.ch>
2022-02-24 17:17:21 +0100

    optimise and cleanup Algorithms

--- analyzers/dataframe/Algorithms.cc
+++ analyzers/dataframe/Algorithms.cc
@@ -1,11 +1,13 @@
 #include "Algorithms.h"
 
-#include "Math/IFunction.h"
-#include "Math/Factory.h"
-#include "Math/Functor.h"
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+
+#include "Math/IFunction.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+#include "Math/LorentzVector.h"
 
 using namespace Algorithms;
 
@@ -32,7 +34,9 @@ float Algorithms::sphericityFit::operator()(const double *pars){
 };
 
 minimize_sphericity::minimize_sphericity(std::string arg_minname, 
-					 std::string arg_algoname){
+					 std::string arg_algoname,
+					 int arg_maxcalls, 
+					 float arg_tolerance){
   m_minname=arg_minname.c_str(); 
   m_algoname=arg_algoname.c_str();
 
@@ -98,7 +102,10 @@ float Algorithms::thrustFit::operator()(const double *pars){
 };
 
 
-minimize_thrust::minimize_thrust(std::string arg_minname, std::string arg_algoname){
+minimize_thrust::minimize_thrust(std::string arg_minname, 
+				 std::string arg_algoname,
+				 int arg_maxcalls, 
+				 float arg_tolerance){
   m_minname=arg_minname.c_str(); 
   m_algoname=arg_algoname.c_str();
 
@@ -154,18 +161,18 @@ ROOT::VecOps::RVec<float> Algorithms::minimize_thrust::operator()(const ROOT::Ve
 
 getAxisCharge::getAxisCharge(bool arg_pos, 
 			     float arg_power){m_pos = arg_pos; m_power = arg_power;};
-float  Algorithms::getAxisCharge::operator() (ROOT::VecOps::RVec<float> angle, 
-					      ROOT::VecOps::RVec<float> charge, 
-					      ROOT::VecOps::RVec<float> px, 
-					      ROOT::VecOps::RVec<float> py, 
-					      ROOT::VecOps::RVec<float> pz) {
+float  Algorithms::getAxisCharge::operator() (const ROOT::VecOps::RVec<float> & angle, 
+					      const ROOT::VecOps::RVec<float> & charge, 
+					      const ROOT::VecOps::RVec<float> & px, 
+					      const ROOT::VecOps::RVec<float> & py, 
+					      const ROOT::VecOps::RVec<float> & pz) {
   float result=0.;
   float norm = 0.;
   for (size_t i = 0; i < angle.size(); ++i) {
-    norm+=px.at(i)*px.at(i)+py.at(i)*py.at(i)+pz.at(i)*pz.at(i);
+    norm+=px[i]*px[i]+py[i]*py[i]+pz[i]*pz[i];
 
-    if (m_pos==1 && angle.at(i)>0.) result+=charge.at(i)*std::pow(sqrt(px.at(i)*px.at(i)+py.at(i)*py.at(i)+pz.at(i)*pz.at(i)),m_power);
-    if (m_pos==0 && angle.at(i)<0.) result+=charge.at(i)*std::pow(sqrt(px.at(i)*px.at(i)+py.at(i)*py.at(i)+pz.at(i)*pz.at(i)),m_power);
+    if (m_pos==1 && angle[i]>0.) result+=charge[i]*std::pow(sqrt(px[i]*px[i]+py[i]*py[i]+pz[i]*pz[i]),m_power);
+    if (m_pos==0 && angle[i]<0.) result+=charge[i]*std::pow(sqrt(px[i]*px[i]+py[i]*py[i]+pz[i]*pz[i]),m_power);
   }
   return result/std::pow(norm,m_power);
 }
@@ -173,37 +180,37 @@ float  Algorithms::getAxisCharge::operator() (ROOT::VecOps::RVec<float> angle,
 
 
 getAxisMass::getAxisMass(bool arg_pos) : m_pos(arg_pos) {};
-float  Algorithms::getAxisMass::operator() (ROOT::VecOps::RVec<float> angle, 
-					    ROOT::VecOps::RVec<float> energy, 
-					    ROOT::VecOps::RVec<float> px, 
-					    ROOT::VecOps::RVec<float> py, 
-					    ROOT::VecOps::RVec<float> pz) {
+float  Algorithms::getAxisMass::operator() (const ROOT::VecOps::RVec<float> & angle, 
+					    const ROOT::VecOps::RVec<float> & energy, 
+					    const ROOT::VecOps::RVec<float> & px, 
+					    const ROOT::VecOps::RVec<float> & py, 
+					    const ROOT::VecOps::RVec<float> & pz) {
   
-  TLorentzVector result;
+  ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double>> result;
   for (size_t i = 0; i < angle.size(); ++i) {
-    TLorentzVector tmp;
-    tmp.SetPxPyPzE(px.at(i), py.at(i), pz.at(i), energy.at(i));
-    if (m_pos==1 && angle.at(i)>0.) result+=tmp;
-    if (m_pos==0 && angle.at(i)<0.) result+=tmp;
+    ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double>> tmp;
+    tmp.SetPxPyPzE(px[i], py[i], pz[i], energy[i]);
+    if (m_pos==1 && angle[i]>0.) result+=tmp;
+    if (m_pos==0 && angle[i]<0.) result+=tmp;
   }
   return result.M();
 }
 
 getAxisEnergy::getAxisEnergy(bool arg_pos) : m_pos(arg_pos) {};
-ROOT::VecOps::RVec<float>  Algorithms::getAxisEnergy::operator() (ROOT::VecOps::RVec<float> angle, 
-								  ROOT::VecOps::RVec<float> charge, 
-								  ROOT::VecOps::RVec<float> energy) {
+ROOT::VecOps::RVec<float>  Algorithms::getAxisEnergy::operator() (const ROOT::VecOps::RVec<float> & angle, 
+								  const ROOT::VecOps::RVec<float> & charge, 
+								  const ROOT::VecOps::RVec<float> & energy) {
   ROOT::VecOps::RVec<float> result={0.,0.,0.};
   for (size_t i = 0; i < angle.size(); ++i) {
-    if (m_pos==1 && angle.at(i)>0.){
-      result.at(0)+=energy.at(i);
-      if (abs(charge.at(i))>0) result.at(1)+=energy.at(i);
-      else result.at(2)+=energy.at(i);
+    if (m_pos==1 && angle[i]>0.){
+      result[0]+=energy[i];
+      if (abs(charge[i])>0) result[1]+=energy[i];
+      else result[2]+=energy[i];
     }
-    if (m_pos==0 && angle.at(i)<0.){
-      result.at(0)+=energy.at(i);
-      if (abs(charge.at(i))>0) result.at(1)+=energy.at(i);
-      else result.at(2)+=energy.at(i);
+    if (m_pos==0 && angle[i]<0.){
+      result[0]+=energy[i];
+      if (abs(charge[i])>0) result[1]+=energy[i];
+      else result[2]+=energy[i];
     }
   }
   return result;
@@ -211,29 +218,52 @@ ROOT::VecOps::RVec<float>  Algorithms::getAxisEnergy::operator() (ROOT::VecOps::
 
 
 getAxisN::getAxisN(bool arg_pos) : m_pos(arg_pos) {};
-ROOT::VecOps::RVec<int>  Algorithms::getAxisN::operator() (ROOT::VecOps::RVec<float> angle, 
-							   ROOT::VecOps::RVec<float> charge) {
+ROOT::VecOps::RVec<int>  Algorithms::getAxisN::operator() (const ROOT::VecOps::RVec<float> & angle, 
+							   const ROOT::VecOps::RVec<float> & charge) {
   ROOT::VecOps::RVec<int> result={0,0,0};
   for (size_t i = 0; i < angle.size(); ++i) {
-    if (m_pos==1 && angle.at(i)>0.){
-      result.at(0)+=1;
-      if (abs(charge.at(i))>0) result.at(1)+=1;
-      else result.at(2)+=1;
+    if (m_pos==1 && angle[i]>0.){
+      result[0]+=1;
+      if (abs(charge[i])>0) result[1]+=1;
+      else result[2]+=1;
     }
-    if (m_pos==0 && angle.at(i)<0.){
-      result.at(0)+=1;
-      if (abs(charge.at(i))>0) result.at(1)+=1;
-      else result.at(2)+=1;
+    if (m_pos==0 && angle[i]<0.){
+      result[0]+=1;
+      if (abs(charge[i])>0) result[1]+=1;
+      else result[2]+=1;
     }
   }
   return result;
 }
 
+getThrustPointing::getThrustPointing(float arg_dir) : m_dir(arg_dir) {};
+ROOT::VecOps::RVec<float> Algorithms::getThrustPointing::operator()(const ROOT::VecOps::RVec<float> & in,
+								    const ROOT::VecOps::RVec<float> & rp_e,
+								    const ROOT::VecOps::RVec<float> & thrust){
 
-float Algorithms::getMass(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> in){
-  TLorentzVector result;
+  ROOT::VecOps::RVec<float> result;
+  for (unsigned int i =0; i<thrust.size(); i++)result.push_back(thrust[i]);
+  float direction = 1.;
+  float pos_angle=0.;
+  float neg_angle=0.;
+  for (unsigned int i =0; i<rp_e.size(); i++){
+    if (in[i]>0.)pos_angle+=rp_e[i];
+    else neg_angle+=rp_e[i];
+  }
+
+  if (pos_angle>neg_angle)direction=-1.;
+  
+  result[1]=thrust[1]*direction*m_dir;
+  result[3]=thrust[3]*direction*m_dir;
+  result[5]=thrust[5]*direction*m_dir;
+  return thrust;
+}
+
+
+float Algorithms::getMass(const ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> & in){
+  ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double>> result;
   for (auto & p: in) {
-    TLorentzVector tmp;
+    ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double>> tmp;
     tmp.SetPxPyPzE(p.momentum.x, p.momentum.y, p.momentum.z, p.energy);
     result+=tmp;
   }
@@ -241,22 +271,22 @@ float Algorithms::getMass(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>
 }
 
 
-ROOT::VecOps::RVec<float> Algorithms::getAxisCosTheta(ROOT::VecOps::RVec<float> axis, 
-						      ROOT::VecOps::RVec<float> px, 
-						      ROOT::VecOps::RVec<float> py, 
-						      ROOT::VecOps::RVec<float> pz){
+ROOT::VecOps::RVec<float> Algorithms::getAxisCosTheta(const ROOT::VecOps::RVec<float> & axis, 
+						      const ROOT::VecOps::RVec<float> & px, 
+						      const ROOT::VecOps::RVec<float> & py, 
+						      const ROOT::VecOps::RVec<float> & pz){
 
   float thrust_mag = sqrt(axis[1]*axis[1] + axis[3]*axis[3] + axis[5]*axis[5]);
   ROOT::VecOps::RVec<float> result;
   for (unsigned int i =0; i<px.size(); i++){
-    float value = (px.at(i)*axis[1] + py.at(i)*axis[3] + pz.at(i)*axis[5])/(sqrt(px.at(i)*px.at(i)+py.at(i)*py.at(i)+pz.at(i)*pz.at(i))*thrust_mag);
+    float value = (px[i]*axis[1] + py[i]*axis[3] + pz[i]*axis[5])/(sqrt(px[i]*px[i]+py[i]*py[i]+pz[i]*pz[i])*thrust_mag);
     result.push_back(value);
   }
   return result;
 }
 
 
-float Algorithms::getAxisCosTheta(ROOT::VecOps::RVec<float> axis, 
+float Algorithms::getAxisCosTheta(const ROOT::VecOps::RVec<float> & axis, 
 				  float px, 
 				  float py, 
 				  float pz){
@@ -267,24 +297,3 @@ float Algorithms::getAxisCosTheta(ROOT::VecOps::RVec<float> axis,
   return result;
 }
 
-
-ROOT::VecOps::RVec<float> Algorithms::getThrustPointing(ROOT::VecOps::RVec<float> in,
-							ROOT::VecOps::RVec<float> rp_e,
-							ROOT::VecOps::RVec<float> thrust,
-							float dir){
-
-  float direction = 1.;
-  float pos_angle=0.;
-  float neg_angle=0.;
-  for (unsigned int i =0; i<rp_e.size(); i++){
-    if (in.at(i)>0.)pos_angle+=rp_e.at(i);
-    else neg_angle+=rp_e.at(i);
-  }
-
-  if (pos_angle>neg_angle)direction=-1.;
-  
-  thrust.at(1)=thrust.at(1)*direction*dir;
-  thrust.at(3)=thrust.at(3)*direction*dir;
-  thrust.at(5)=thrust.at(5)*direction*dir;
-  return thrust;
-}
