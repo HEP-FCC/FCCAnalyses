2c4d30b3b6c551d3c93da763711b5c9b9d61917f
clementhelsens <clement.helsens@cern.ch>
2022-03-15 14:28:27 +0100

    remove awkward in setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
 export PYTHONPATH=$PWD:$PYTHONPATH
 export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
-export LD_LIBRARY_PATH=`python -m awkward.config --libdir`:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=`python -m awkward.config --libdir`:$LD_LIBRARY_PATH
 
 
