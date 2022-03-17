9344e4357959b2495ff29f4c5fb0779e2d8da304
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-03-17 17:07:07 +0100

    API slightly adapted ; using a singleton (!)

--- analyzers/dataframe/FCCAnalyses/ONNXRuntime.h
+++ analyzers/dataframe/FCCAnalyses/ONNXRuntime.h
@@ -2,18 +2,31 @@
 #define analyzers_ONNXRuntime_h
 
 #include "ROOT/RVec.hxx"
-#include "core/session/onnxruntime_cxx_api.h"
+#include "edm4hep/ReconstructedParticleData.h"
+#include "core/session/experimental_onnxruntime_cxx_api.h"
+
+namespace Ort {
+  class Allocator;
+  class Env;
+  class Session;
+}
 
 class ONNXRuntime {
 public:
-  ONNXRuntime();
-  explicit ONNXRuntime(const std::string& model_path, const std::string& preprocess_json="");
+  static ONNXRuntime& get(const std::string& model_path = "", const std::string& preprocess_json = "");
   ONNXRuntime(const ONNXRuntime&) = delete;
   virtual ~ONNXRuntime();
 
   ONNXRuntime& operator=(const ONNXRuntime&) = delete;
 
-  ROOT::VecOps::RVec<float> run(ROOT::VecOps::RVec<int> in) const;
+  ROOT::VecOps::RVec<float> operator()(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>) const;
+
+private:
+  explicit ONNXRuntime(const std::string& model_path = "", const std::string& preprocess_json = "");
+
+  std::unique_ptr<Ort::Env> env_;
+  std::unique_ptr<Ort::Experimental::Session> session_;
+  std::unique_ptr<Ort::Allocator> alloc_;
 };
 
 
--- analyzers/dataframe/src/ONNXRuntime.cc
+++ analyzers/dataframe/src/ONNXRuntime.cc
@@ -5,10 +5,9 @@
 
 #include <fstream>
 
-ONNXRuntime::ONNXRuntime() {}
-
-ONNXRuntime::ONNXRuntime(const std::string& model_path, const std::string& preprocess_json) {
-  if (!preprocess_json.empty()) {
+ONNXRuntime::ONNXRuntime(const std::string& model_path, const std::string& preprocess_json) :
+    env_(new Ort::Env(OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING, "onnx_runtime")) {
+  /*if (!preprocess_json.empty()) {
     // the preprocessing JSON was found ; extract the variables listing and all useful information
     std::ifstream json_file(preprocess_json);
     std::stringstream json_content;
@@ -16,12 +15,34 @@ ONNXRuntime::ONNXRuntime(const std::string& model_path, const std::string& prepr
     const std::shared_ptr<awkward::Form> json = awkward::Form::fromjson(json_content.str());
     for (const auto& key : json->keys())
       std::cout << ">> " << key << std::endl;
-  }
+  }*/
+  Ort::SessionOptions options;
+  auto path = model_path;
+  session_ = std::make_unique<Ort::Experimental::Session>(*env_, path, options);
+  auto mem_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeDefault);
+  alloc_ = std::make_unique<Ort::Allocator>(*session_, mem_info);
+
+  std::cout << session_->GetInputCount() << std::endl;
+  for (size_t i = 0; i < session_->GetInputCount(); ++i)
+    std::cout << session_->GetInputName(i, *alloc_) << std::endl;
+
+  std::cout << session_->GetOutputCount() << std::endl;
+  for (size_t i = 0; i < session_->GetOutputCount(); ++i)
+    std::cout << session_->GetOutputName(i, *alloc_) << std::endl;
+}
+
+ONNXRuntime& ONNXRuntime::get(const std::string& model_path, const std::string& preprocess_json) {
+  static ONNXRuntime rt(model_path, preprocess_json);
+  return rt;
 }
 
 ONNXRuntime::~ONNXRuntime() {}
 
-ROOT::VecOps::RVec<float> ONNXRuntime::run(ROOT::VecOps::RVec<int> in) const {
+ROOT::VecOps::RVec<float> ONNXRuntime::operator()(ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> in) const {
   ROOT::VecOps::RVec<float> out;
+
+  for (const auto& part : in) {
+    out.push_back(0.);
+  }
   return out;
 }
