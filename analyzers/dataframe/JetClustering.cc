1d31aad6d35b0af849d1957f8233895975439cde
clement helsens <clementhelsens@MacBook-Pro-KIT.local>
2022-03-18 11:53:33 +0100

    first trial withnew fsatjet interface

--- analyzers/dataframe/JetClustering.cc
+++ analyzers/dataframe/JetClustering.cc
@@ -10,6 +10,10 @@
 
 using namespace JetClustering;
 
+
+MyRDFOp::MyRDFOp(int dm){_datamember=dm; std::std::cout << "dm "<< dm << std::endl;}
+double JetClustering::MyRDFOp::operator()(double input1, double input2){ return input1 + input2 + _datamember; }
+
 clustering_kt::clustering_kt(float arg_radius,
                              int arg_exclusive,
                              float arg_cut,
