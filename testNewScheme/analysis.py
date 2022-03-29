72d405d944071fd14189a4dda2aa2df8a1420f8d
clement helsens <clement.helsens@cern.ch>
2022-03-29 10:17:07 +0200

    add new vars

--- testNewScheme/analysis.py
+++ testNewScheme/analysis.py
@@ -3,7 +3,7 @@ processList = ['p8_ee_Zbb_ecm91','p8_ee_Zcc_ecm91','p8_ee_Zuds_ecm91']
 outputList  = []
 prodTag     = "FCCee/spring2021/IDEA/"
 outputDir   = "/eos/experiment/fcc/ee/tmp/testnewscheme/"
-nCPUS       = 1
+nCPUS       = 8
 fraction    = 1.
 runBatch    = False
 
@@ -11,20 +11,16 @@ runBatch    = False
 class RDFanalysis():
 
     #__________________________________________________________
-    def __init__(self, inputlist, outname, ncpu):
-        self.outname = outname
-        if ".root" not in outname:
-            self.outname+=".root"
-
-        ROOT.ROOT.EnableImplicitMT(ncpu)
-        ROOT.EnableThreadSafety()
-        self.df = ROOT.RDataFrame("events", inputlist)
-        print (" init done, about to run")
-
-    #__________________________________________________________
     def analysers(df):
-        #df2 = (self.df.Range(100)
         df2 = (df
+        .Alias("MCRecoAssociations0", "MCRecoAssociations#0.index")
+        .Alias("MCRecoAssociations1", "MCRecoAssociations#1.index")
+        .Alias("Particle0", "Particle#0.index")
+        .Alias("Particle1", "Particle#1.index")
+        .Define("MCVertexObject", "myUtils::get_MCVertexObject(Particle, Particle0)")
+        .Define("MC_Vertex_x",    "myUtils::get_MCVertex_x(MCVertexObject)")
+        .Define("MC_Vertex_y",    "myUtils::get_MCVertex_y(MCVertexObject)")
+        .Define("MC_Vertex_z",    "myUtils::get_MCVertex_z(MCVertexObject)")
         .Define("v", "rdfentry_")
         .Define("w", "return 1./(v+1)")
         )
@@ -32,7 +28,5 @@ class RDFanalysis():
 
     #__________________________________________________________
     def output():
-        branchList = []
-        branchList.append("v")
-        branchList.append("w")
+        branchList = ["v","w","MC_Vertex_x","MC_Vertex_y","MC_Vertex_z"]
         return branchList
