7e0026dd286ea1955db07192539da331b9a25bea
clement helsens <clement.helsens@cern.ch>
2022-04-12 20:33:02 +0200

    starting to modify analysers

--- examples/FCCee/flavour/generic-analysis/analysis_Bu2D0Pi.py
+++ examples/FCCee/flavour/generic-analysis/analysis_Bu2D0Pi.py
@@ -1,40 +1,29 @@
-import sys
-import ROOT
+#Mandatory: List of processes
+processList = {
+    'p8_ee_Zbb_ecm91_EvtGen_Bd2Kstee':{}
+}
 
-print ("Load cxx analyzers ... ",)
-ROOT.gSystem.Load("libedm4hep")
-ROOT.gSystem.Load("libpodio")
-ROOT.gSystem.Load("libawkward")
-ROOT.gSystem.Load("libawkward-cpu-kernels")
-ROOT.gSystem.Load("libFCCAnalyses")
+#Mandatory: Production tag when running over EDM4Hep centrally produced events, this points to the yaml files for getting sample statistics
+prodTag     = "FCCee/spring2021/IDEA/"
 
-ROOT.gErrorIgnoreLevel = ROOT.kFatal
-_edm  = ROOT.edm4hep.ReconstructedParticleData()
-_pod  = ROOT.podio.ObjectID()
-_fcc  = ROOT.dummyLoader
+#Optional: output directory, default is local running directory
+outputDir   = "outputs/FCCee/flavour/B2Kstee/stage1"
 
-print ('edm4hep  ',_edm)
-print ('podio    ',_pod)
-print ('fccana   ',_fcc)
+#Optional: ncpus, default is 4
+nCPUS       = 8
 
+#Optional running on HTCondor, default is False
+runBatch    = False
+#batchQueue = "longlunch"
+#compGroup = "group_u_FCC.local_gen"
 
-class analysis():
+#Mandatory: RDFanalysis class where the use defines the operations on the TTree
+class RDFanalysis():
 
     #__________________________________________________________
-    def __init__(self, inputlist, outname, ncpu):
-        self.outname = outname
-        if ".root" not in outname:
-            self.outname+=".root"
-
-        #ROOT.ROOT.EnableImplicitMT(ncpu)
-
-        self.df = ROOT.RDataFrame("events", inputlist)
-        print (" done")
-    #__________________________________________________________
-    def run(self):
-        
-        df2 = (self.df.Range(1000)
-        #df2 = (self.df
+    #Mandatory: analysers funtion to define the analysers to process, please make sure you return the last dataframe, in this example it is df2
+    def analysers(df):
+        df2 = (df
                .Alias("MCRecoAssociations0", "MCRecoAssociations#0.index")
                .Alias("MCRecoAssociations1", "MCRecoAssociations#1.index")
 
@@ -51,79 +40,24 @@ class analysis():
                .Define("Kaons"       ,"myUtils::sel_PID(321)(RecoPartPID)"  )
                .Define("D0Candidates"          ,"myUtils::build_D0(0.05, 1.5, true)(RecoPartPID, EFlowTrack_1, Pions, Kaons,RecoInd_actsFinder)")
 
-               #does not work given the different index when filtering
-               #.Define("RecoPartPID_PVFiltered", "myUtils::filter_PV(false)(RecoPartPID, RecoInd_actsFinder)")
-               #.Define("Pions_PVFiltered"       ,"myUtils::sel_PID(211)(RecoPartPID_PVFiltered)"  )
-               #.Define("Kaons_PVFiltered"       ,"myUtils::sel_PID(321)(RecoPartPID_PVFiltered)"  )
-               #.Define("D0Candidates"          ,"myUtils::build_D0(0.05, 1.5)(RecoPartPID_PVFiltered, EFlowTrack_1, Pions_PVFiltered, Kaons_PVFiltered)")
                .Define("nD0", "myUtils::getFCCAnalysesComposite_N(D0Candidates)")
                .Filter("nD0>0")
                .Define("massD0", "myUtils::getFCCAnalysesComposite_mass(D0Candidates)")
-               
+
                .Define("Bu2D0PiCandidates","myUtils::build_Bu2D0Pi(RecoPartPID, D0Candidates, Pions)")
                .Define("nBu2D0Pi", "myUtils::getFCCAnalysesComposite_N(Bu2D0PiCandidates)")
                .Define("massBu2D0Pi", "myUtils::getFCCAnalysesComposite_mass(Bu2D0PiCandidates)")
+        )
+        return df2
 
-              
-               
-               )
-
-        # select branches for output file
-        branchList = ROOT.vector('string')()
-        for branchName in [
-                #"RecoInd_actsFinder",
-                #"Vertex_actsFinder",     # on Zuds: both track selections lead to very similar results for the vertex
+    #__________________________________________________________
+    #Mandatory: output function, please make sure you return the branchlist as a python list
+    def output():
+        branchList = [
                "nTrkPV",
                 "nD0",
                 "massD0",
                 "nBu2D0Pi",
-                "massBu2D0Pi",
-                #"awk"
-                ]:
-            branchList.push_back(branchName)
-
-        #opts = ROOT.RDF.RSnapshotOptions()
-        #opts.fCompressionAlgorithm = ROOT.ROOT.kLZ4
-        #opts.fCompressionLevel = 3
-        #opts.fAutoFlush = -1024*1024*branchList.size()
-        #df2.Snapshot("events", self.outname, branchList, opts)
-        df2.Snapshot("events", self.outname, branchList)
-
-# example call for standalone file
-# python examples/FCCee/flavour/generic-analysis/analysis_Bu2D0Pi.py /afs/cern.ch/user/h/helsens/FCCsoft/Key4HEP/k4SimDelphes/ee_Zbb_Bu2D0Pi.root
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv)==1:
-        print ("usage:")
-        print ("python ",sys.argv[0]," file.root")
-        print ("python ",sys.argv[0]," dir/*.root")
-        sys.exit(3)
-
-
-    import glob
-    filelist = glob.glob(sys.argv[1])
-    
-    print ("Create dataframe object from ", )
-    fileListRoot = ROOT.vector('string')()
-    for fileName in filelist:
-        fileListRoot.push_back(fileName)
-        print (fileName, " ",)
-        print (" ...")
-        
-    outDir = sys.argv[0].replace(sys.argv[0].split('/')[0],'outputs/').replace('analysis_Bc2TauNu.py','')+'/'
-    import os
-    os.system("mkdir -p {}".format(outDir))
-    outfile = outDir+sys.argv[1].split('/')[-1]
-    ncpus = 1
-    analysis = analysis(fileListRoot, outfile, ncpus)
-    analysis.run()
-
-    #tf = ROOT.TFile(infile)
-    #entries = tf.events.GetEntries()
-    #p = ROOT.TParameter(int)( "eventsProcessed", entries)
-    #outf=ROOT.TFile(outfile,"UPDATE")
-    #p.Write()
-
-
+                "massBu2D0Pi"
+        ]
+        return branchList
