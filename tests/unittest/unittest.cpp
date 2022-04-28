7769b2ebcdb1d10fc32b414c3ed9e55de7434d57
Clement Helsens <clement.helsens@cern.ch>
2022-04-28 18:34:05 +0200

    fix include

--- tests/unittest/unittest.cpp
+++ tests/unittest/unittest.cpp
@@ -1,10 +1,9 @@
 
 #include "catch2/catch_test_macros.hpp"
-#include "myUtils.h"
+#include "FCCeeAnalyses/myUtils.h"
 
 TEST_CASE("myUtils", "[basics]") {
   ROOT::VecOps::RVec<float> in1 {-1, 2, 4.5, 1e30};
   REQUIRE(myUtils::get_Npos(in1) == 3);
 
 }
-
