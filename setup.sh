28f770308a9986443b3737e4da4b41cf3c9691e1
Juraj Smiesko <juraj.smiesko@cern.ch>
2022-05-02 15:21:42 +0200

    Add protection agains running setup.sh as a script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
-export PYTHONPATH=$PWD:$PYTHONPATH
-export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
-export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
-export LOCAL_DIR=$PWD
-export LD_LIBRARY_PATH=`python -m awkward.config --libdir`:$LD_LIBRARY_PATH
+if [ "$0" != "$BASH_SOURCE" ]; then
+  source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
+  export PYTHONPATH=$PWD:$PYTHONPATH
+  export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
+  export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
+  export LOCAL_DIR=$PWD
+  export LD_LIBRARY_PATH=`python -m awkward.config --libdir`:$LD_LIBRARY_PATH
+else
+  echo "ERROR: This script is meant to be sourced!"
+fi
