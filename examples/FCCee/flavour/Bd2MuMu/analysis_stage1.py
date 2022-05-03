16cf692c76a5f3b38c3b490a3e3446917a6165bf
Clement Helsens <clement.helsens@cern.ch>
2022-05-03 07:56:00 +0200

    add stage1

--- examples/FCCee/flavour/Bd2MuMu/analysis_stage1.py
+++ examples/FCCee/flavour/Bd2MuMu/analysis_stage1.py
@@ -1,6 +1,5 @@
 Mandatory: List of processes
 processList = {
-    'p8_ee_Zbb_ecm91':{},
     'p8_ee_Zbb_ecm91_EvtGen_Bd2MuMu':{}
 }
 
@@ -74,101 +73,10 @@ class RDFanalysis():
                ##    Get the B0 -> MuMu candidate mass    ##
                #############################################
                .Define("Bd2MuMu_mass",    "myUtils::getFCCAnalysesComposite_mass(Bd2MuMuCandidates)")
-
-
            )
-
-        #############################################
-        ##      select branches for output file    ##
-        #############################################
-        branchList = ROOT.vector('string')()
-        for branchName in ["Bd2MuMu_mass" ]:
-            branchList.push_back(branchName)
-
-        df2.Snapshot("events", self.outname, branchList)
-
-
-
-
-
-# example call for standalone file
-# python examples/FCCee/flavour/Bd2MuMu/analysis.py p8_ee_Zbb_Bd2MuMu.root /eos/experiment/fcc/ee/generation/DelphesEvents/spring2021/IDEA/p8_ee_Zbb_ecm91_EvtGen_Bd2MuMu/events_108578506.root
-
-# python examples/FCCee/flavour/Bd2MuMu/analysis.py p8_ee_Zbb.root "/eos/experiment/fcc/ee/generation/DelphesEvents/spring2021/IDEA/p8_ee_Zbb_ecm91/events_0033*.root"
-
-
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv)<3:
-        print ("usage:")
-        print ("python ",sys.argv[0]," output.root input.root")
-        print ("python ",sys.argv[0]," output.root \"inputdir/*.root\"")
-        print ("python ",sys.argv[0]," output.root file1.root file2.root file3.root <nevents>")
-        sys.exit(3)
-
-
-    print ("Create dataframe object from ", )
-    fileListRoot = ROOT.vector('string')()
-    nevents=0
-
-    print("===============================", sys.argv[2])
-
-    if len(sys.argv)==3 and "*" in sys.argv[2]:
-        import glob
-        filelist = glob.glob(sys.argv[2])
-        for fileName in filelist:
-            fileListRoot.push_back(fileName)
-            print (fileName, " ",)
-            print (" ...")
-
-
-    elif len(sys.argv)>2:
-        for i in range(2,len(sys.argv)):
-            try:
-                nevents=int(sys.argv[i])
-                print ("nevents found (will be in the processed events branch in root tree):",nevents)
-            except ValueError:
-                fileListRoot.push_back(sys.argv[i])
-                print (sys.argv[i], " ",)
-                print (" ...")
-
-
-    outfile=sys.argv[1]
-    print("output file:  ",outfile)
-    if len(outfile.split("/"))>1:
-        import os
-        os.system("mkdir -p {}".format(outfile.replace(outfile.split("/")[-1],"")))
-
-    if nevents==0:
-        for f in fileListRoot:
-            tf=ROOT.TFile.Open(str(f),"READ")
-            tt=tf.Get("events")
-            nevents+=tt.GetEntries()
-    print ("nevents ", nevents)
-
-    import time
-    start_time = time.time()
-    ncpus = 8
-    analysis = analysis(fileListRoot, outfile, ncpus)
-    analysis.run()
-
-    elapsed_time = time.time() - start_time
-    print  ("==============================SUMMARY==============================")
-    print  ("Elapsed time (H:M:S)     :  ",time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
-    print  ("Events Processed/Second  :  ",int(nevents/elapsed_time))
-    print  ("Total Events Processed   :  ",int(nevents))
-    print  ("===================================================================")
-
-
-    outf = ROOT.TFile( outfile, "update" )
-    meta = ROOT.TTree( "metadata", "metadata informations" )
-    n = array( "i", [ 0 ] )
-    meta.Branch( "eventsProcessed", n, "eventsProcessed/I" )
-    n[0]=nevents
-    meta.Fill()
-    p = ROOT.TParameter(int)( "eventsProcessed", n[0])
-    p.Write()
-    outf.Write()
-    outf.Close()
+        return df2
+    #__________________________________________________________
+    #Mandatory: output function, please make sure you return the branchlist as a python list
+    def output():
+        branchList = ["Bd2MuMu_mass"]
+        return branchList
