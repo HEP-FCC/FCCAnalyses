4d3b2fc10215a3423d24ed08d116c16dc366bb6b
github-action-benchmark <github@users.noreply.github.com>
2022-05-17 07:19:52 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ade3d5936b032b7591d665a91eb3540bee9b6264

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1652709149690,
+  "lastUpdate": 1652771992179,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -144,6 +144,44 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.83556621940122,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement.helsens@cern.ch",
+            "name": "Clement Helsens",
+            "username": "clementhelsens"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ade3d5936b032b7591d665a91eb3540bee9b6264",
+          "message": "Add FCCAnalyses namespaces for everything (#167)\n\n* add FCCAnalyses namespace\r\n\r\n* add FCCAnalyses namespace in tests\r\n\r\n* fix namespaces for test",
+          "timestamp": "2022-05-17T09:10:46+02:00",
+          "tree_id": "8af7e2b700fb19140be093ae3096c1c146b83b62",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/ade3d5936b032b7591d665a91eb3540bee9b6264"
+        },
+        "date": 1652771990658,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 18.52289319038391,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 19.928242921829224,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
