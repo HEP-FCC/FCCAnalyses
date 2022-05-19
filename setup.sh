c7972cea02a026d9f55904533800d4b99e767a7e
Valentin Volkl <valentin.volkl@cern.ch>
2022-05-19 13:01:42 +0200

    cmake: set CMAKE_PREFIX_PATH in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ if [ "${0}" != "${BASH_SOURCE}" ]; then
   fi
   export PYTHONPATH=$PWD:$PYTHONPATH
   export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
+  export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
   export ROOT_INCLUDE_PATH=$PWD/install/include/FCCAnalyses:$ROOT_INCLUDE_PATH
   export LOCAL_DIR=$PWD
   export LD_LIBRARY_PATH=`python -m awkward.config --libdir`:$LD_LIBRARY_PATH
