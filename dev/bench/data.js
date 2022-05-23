eda393b77bfc2d9a8086d9d92c6a124bbfd42b9f
github-action-benchmark <github@users.noreply.github.com>
2022-05-23 14:44:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 82a87ff68f86d22b30a9ad0e0f7a8df47527e079

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653316870276,
+  "lastUpdate": 1653317096251,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1656,6 +1656,44 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.114381235390577,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "valentin.volkl@cern.ch",
+            "name": "Valentin Volkl",
+            "username": "vvolkl"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82a87ff68f86d22b30a9ad0e0f7a8df47527e079",
+          "message": "cmake: avoid absolute paths to source headers being baked into dictionary (#178)",
+          "timestamp": "2022-05-23T16:31:54+02:00",
+          "tree_id": "7b3ff120f0118bfa6812bc28ebe1467fe21a6755",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/82a87ff68f86d22b30a9ad0e0f7a8df47527e079"
+        },
+        "date": 1653317094857,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 18.14177942276001,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 18.803792476654053,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
