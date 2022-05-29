29e23ee0f4d71876836af7668caca1b43766b9b2
github-action-benchmark <github@users.noreply.github.com>
2022-05-29 22:03:01 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for eb19ed7abd0f9a48376a6189351ca6095b2a57bc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653850684076,
+  "lastUpdate": 1653861781770,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -2304,6 +2304,44 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.2519238785679425,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement.helsens@cern.ch",
+            "name": "Clement Helsens",
+            "username": "clementhelsens"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eb19ed7abd0f9a48376a6189351ca6095b2a57bc",
+          "message": "More parsers (#181)\n\n* start adding more subparsers\r\n\r\n* start adding more subparsers\r\n\r\n* remove early parsing\r\n\r\n* wip: add more parsers\r\n\r\n* add attribute error\r\n\r\n* check before bugfx\r\n\r\n* bugfix\r\n\r\n* add more parsing options to instructions\r\n\r\n* add attribute error for final plots\r\n\r\n* add attribute error for final plots\r\n\r\n* remove run-",
+          "timestamp": "2022-05-29T20:47:48+02:00",
+          "tree_id": "f3505cd41787180abe807ee777fa3afe595983ef",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/eb19ed7abd0f9a48376a6189351ca6095b2a57bc"
+        },
+        "date": 1653861779941,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 12.543112993240356,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.718513011932373,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
