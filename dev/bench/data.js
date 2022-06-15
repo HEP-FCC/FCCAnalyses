d5111725743a4ccd2e1cb04eff5702eb7abf0474
github-action-benchmark <github@users.noreply.github.com>
2022-06-15 08:22:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 64d6b54e80bea62ffd2af0f55412ed05d27f5163

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655280243818,
+  "lastUpdate": 1655281371161,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -2664,6 +2664,44 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.884027297551033,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement.helsens@cern.ch",
+            "name": "Clement Helsens",
+            "username": "clementhelsens"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "64d6b54e80bea62ffd2af0f55412ed05d27f5163",
+          "message": "Merge pull request #183 from vvolkl/covMatrix\n\nadapt to time-extended edm4hep::Track",
+          "timestamp": "2022-06-15T09:55:45+02:00",
+          "tree_id": "188b12bce11792c3ad299e13a3b501997f5e531b",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/64d6b54e80bea62ffd2af0f55412ed05d27f5163"
+        },
+        "date": 1655281369076,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 13.019376277923584,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 16.69191265106201,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
