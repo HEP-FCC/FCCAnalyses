5dc76986eab686c48c54948f1c82a20fbdee6ce1
github-action-benchmark <github@users.noreply.github.com>
2022-06-29 08:01:10 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 23b6a8b467e977a541b90ecfd509b96418c4b022

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656489668299,
+  "lastUpdate": 1656489669983,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -2774,6 +2774,40 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement.helsens@cern.ch",
+            "name": "Clement Helsens",
+            "username": "clementhelsens"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "23b6a8b467e977a541b90ecfd509b96418c4b022",
+          "message": "Merge pull request #184 from clementhelsens/fix-tree\n\nbugfix dotree",
+          "timestamp": "2022-06-29T09:54:19+02:00",
+          "tree_id": "da21c01792bf0367bbae507cf2eff0a86988a522",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/23b6a8b467e977a541b90ecfd509b96418c4b022"
+        },
+        "date": 1656489669394,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.760486054673528,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 6.093696889231943,
+            "unit": "Evt/s"
           }
         ]
       }
