cdf5d1905311d23ca774c6b3d9ffa579a4a5e605
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-07-15 02:28:06 +0200

    Fixed input format for constituents

--- analyzers/dataframe/src/JetFlavourUtils.cc
+++ analyzers/dataframe/src/JetFlavourUtils.cc
@@ -10,27 +10,26 @@ namespace FCCAnalyses {
   namespace JetFlavourUtils {
     void setup_weaver(const std::string& onnx_filename, const std::string& json_filename) {
       gWeaver = std::make_unique<WeaverInterface>(onnx_filename, json_filename);
-      std::cout << "Weaver initialised!!\n";
     }
 
-    rv::RVec<rv::RVec<float> > compute_weights(const rv::RVec<Variables>& jets) {
+    rv::RVec<rv::RVec<float> > compute_weights(const rv::RVec<Variables>& vars) {
       if (!gWeaver)
         throw std::runtime_error("Weaver interface is not initialised!");
       rv::RVec<rv::RVec<float> > out;
-      if (jets.empty())
+      if (vars.empty())  // no variables registered
         return out;
-      for (const auto& jet : jets) {
-        if (jet.empty())  // no variables registered
-          return out;
+      size_t num_jets = vars.at(0).size();
+      if (num_jets == 0)  // no jets to categorise
+        return out;
+      // transform a collection of {var1 -> {jet1 -> {constit1, constit2, ...}, jet2 -> {...}, ...}, var2 -> {...}}
+      //      into a collection of {jet -> {constit1 -> {var1, var2, ...}, constit2 -> {...}, ...}}
+      for (size_t i = 0; i < num_jets; ++i) {
         Variables jet_sc_vars;
-        size_t num_constits = jet.at(0).size();
-        for (size_t i = 0; i < num_constits; ++i) {
+        size_t num_constits = vars.at(0).at(i).size();
+        for (size_t j = 0; j < num_constits; ++j) {
           FCCAnalysesJetConstituentsData constit_vars;
-          for (const auto& var : jet) {
-            if (var.size() != num_constits)
-              throw std::runtime_error("Invalid array of variables specified for each jet constituent!");
-            constit_vars.push_back((float)var.at(i));
-          }
+          for (size_t k = 0; k < vars.size(); ++k)
+            constit_vars.push_back((float)vars.at(k).at(i).at(j));
           jet_sc_vars.push_back(constit_vars);
         }
         out.emplace_back(gWeaver->run(jet_sc_vars));
--- tests/unittest/onnxtest.cpp
+++ tests/unittest/onnxtest.cpp
@@ -9,7 +9,7 @@ TEST_CASE("flavtagging", "") {
       "/afs/cern.ch/work/s/selvaggi/public/4Laurent/ONNX/preprocess.json");
 
   const auto out = FCCAnalyses::JetFlavourUtils::compute_weights(
-      {{{1.2707, 19.1198}, {1.38285, 19.3685}, {1.97631, 1.7312}, {-1.50803, -1.36646}, {0., 0.}, {1., -1.}}});
+      {{{1.2707, 19.1198}}, {{1.38285, 19.3685}}, {{1.97631, 1.7312}}, {{-1.50803, -1.36646}}, {{0., 0.}}, {{1., -1.}}});
 
   std::cout << "out: " << out.size() << " := " << out << std::endl;
 }
