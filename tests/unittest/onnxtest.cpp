10fb8539083f7bb5c98212473855ee379c4dda9d
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-07-21 11:31:04 +0200

    Added some tests in flavtagging unit tests

--- tests/unittest/onnxtest.cpp
+++ tests/unittest/onnxtest.cpp
@@ -1,9 +1,9 @@
-#include "catch2/catch_test_macros.hpp"
 #include "FCCAnalyses/JetFlavourUtils.h"
 
-#include <iostream>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
 
-TEST_CASE("flavtagging", "") {
+TEST_CASE("flavtagging", "[onnx]") {
   FCCAnalyses::JetFlavourUtils::setup_weaver(
       "/afs/cern.ch/work/s/selvaggi/public/4Laurent/ONNX/fccee_flavtagging_dummy.onnx",
       "/afs/cern.ch/work/s/selvaggi/public/4Laurent/ONNX/preprocess.json",
@@ -12,5 +12,7 @@ TEST_CASE("flavtagging", "") {
   const auto out = FCCAnalyses::JetFlavourUtils::compute_weights(
       {{{1.38285, 19.3685}}, {{1.97631, 1.7312}}, {{-1.50803, -1.36646}}, {{0, 0}}, {{1, -1}}});
 
-  std::cout << "out: " << out.size() << " := " << out << std::endl;
+  REQUIRE(out.size() == 1);                                    // single jet -> single collection of weights
+  REQUIRE(out.at(0).size() == 5);                              // weaver model-specific feature
+  REQUIRE(ROOT::VecOps::Sum(out.at(0)) == Catch::Approx(1.));  // all weights should add up to unity
 }
