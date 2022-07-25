cdea51e385f56c1c3dc4978835de60353dfe34b9
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-07-25 22:21:14 +0200

    Documenting jet constituents main functions

--- analyzers/dataframe/FCCAnalyses/JetConstituentsUtils.h
+++ analyzers/dataframe/FCCAnalyses/JetConstituentsUtils.h
@@ -10,10 +10,13 @@ namespace FCCAnalyses {
     using FCCAnalysesJetConstituents = rv::RVec<edm4hep::ReconstructedParticleData>;
     using FCCAnalysesJetConstituentsData = rv::RVec<float>;
 
+    /// Build the collection of constituents (mapping jet -> reconstructed particles) for all jets in event
     rv::RVec<FCCAnalysesJetConstituents> build_constituents(rv::RVec<edm4hep::ReconstructedParticleData>,
                                                             rv::RVec<edm4hep::ReconstructedParticleData>);
 
+    /// Retrieve the constituents of an indexed jet in event
     FCCAnalysesJetConstituents get_jet_constituents(rv::RVec<FCCAnalysesJetConstituents>, int);
+    /// Retrieve the constituents of a collection of indexed jets in event
     rv::RVec<FCCAnalysesJetConstituents> get_constituents(rv::RVec<FCCAnalysesJetConstituents>, rv::RVec<int>);
 
     rv::RVec<FCCAnalysesJetConstituentsData> get_pt(rv::RVec<FCCAnalysesJetConstituents>);
