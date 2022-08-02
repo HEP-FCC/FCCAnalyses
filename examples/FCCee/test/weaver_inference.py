013e6e0ed27c00854b804434e72abec6dd8b8ced
Valentin Volkl <valentin.volkl@cern.ch>
2022-08-02 17:15:00 +0200

    weaver: run example singlethreaded (#190)

--- examples/FCCee/test/weaver_inference.py
+++ examples/FCCee/test/weaver_inference.py
@@ -12,7 +12,7 @@ prodTag     = "FCCee/spring2021/IDEA/"
 outputDir   = "."
 
 #Optional
-nCPUS       = 8
+nCPUS       = 1
 runBatch    = False
 #batchQueue = "longlunch"
 #compGroup = "group_u_FCC.local_gen"
