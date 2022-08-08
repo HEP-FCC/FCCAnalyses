d6435e4e4a62a017116ece1a47adef8fe0129de3
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-08-09 00:48:23 +0200

    First attempt to build a dummy analysis

---
+++ analyses/analysis_example/src/DummyAnalysis.cc
@@ -0,0 +1,15 @@
+#include "analysis_example/include/DummyAnalysis.h"
+#include <iostream>
+
+using namespace std;
+
+namespace analysis_example {
+  void dummy_analysis() { cout << "Dummy analysis initialised." << endl; }
+
+  rv::RVec<float> dummy_collection(const rv::RVec<edm4hep::ReconstructedParticleData>& parts) {
+    rv::RVec<float> output;
+    for (size_t i = 0; i < parts.size(); ++i)
+      output.emplace_back(parts.at(i).momentum.x);
+    return output;
+  }
+}  // namespace analysis_example
