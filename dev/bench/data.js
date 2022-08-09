b2e21366154c3132568fe03c68496c3b40bd9bae
github-action-benchmark <github@users.noreply.github.com>
2022-08-09 07:15:18 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 3845bab0eb7e6b7266c2b7b5ed6c14088d56ed82

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660029316714,
+  "lastUpdate": 1660029318210,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -5157,6 +5157,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lforthomme@protonmail.com",
+            "name": "Laurent Forthomme",
+            "username": "forthommel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3845bab0eb7e6b7266c2b7b5ed6c14088d56ed82",
+          "message": "Fix CI crashes in ONNXRuntime/Weaver integration test (#196)\n\n* Exception thrown if ONNX model path is left empty\r\n\r\n* Explicitely setting model path as string instead of auto\r\n\r\n* Using string copies wherever possibly necessary\r\n\r\n* Dropped FCCAnalyses namespace from WeaverInterface object",
+          "timestamp": "2022-08-09T08:59:38+02:00",
+          "tree_id": "4731b6c1afea7fe218edaba87f24c029204936bd",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/3845bab0eb7e6b7266c2b7b5ed6c14088d56ed82"
+        },
+        "date": 1660029317784,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.510023561214933,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.57601385995007,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 7.295114665881528,
+            "unit": "Evt/s"
           }
         ]
       }
