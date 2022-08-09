6981e29e3252433e1c4087416f52be4d771a281d
github-action-benchmark <github@users.noreply.github.com>
2022-08-09 09:50:47 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for bf64b9ad10e9b0cb07840ae68be9b6bc40a151cc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660038646041,
+  "lastUpdate": 1660038647418,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -5577,6 +5577,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lforthomme@protonmail.com",
+            "name": "Laurent Forthomme",
+            "username": "forthommel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf64b9ad10e9b0cb07840ae68be9b6bc40a151cc",
+          "message": "Revert \"weaver: run example singlethreaded (#190)\" (#197)\n\nThis reverts commit 013e6e0ed27c00854b804434e72abec6dd8b8ced.",
+          "timestamp": "2022-08-09T09:31:20Z",
+          "tree_id": "e0fe4044a70d7afcbcf77efca7bade6028a9d8d8",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/bf64b9ad10e9b0cb07840ae68be9b6bc40a151cc"
+        },
+        "date": 1660038646986,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 9.44694510326267,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 7.067272559082177,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test",
+            "value": 9.147364306265233,
+            "unit": "Evt/s"
           }
         ]
       }
