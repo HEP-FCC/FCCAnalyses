ced10b57dcf7b348a99c754edf00204dc6729c4f
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-08-14 14:36:27 +0200

    Replaced 'setup' command by 'init'

--- config/FCCAnalysisSetup.py
+++ config/FCCAnalysisSetup.py
@@ -3,7 +3,7 @@ def setup(mainparser):
     from config.analysis_builder import setup_analysis
 
     args, _ = mainparser.parse_known_args()
-    if args.command == 'setup':
+    if args.command == 'init':
         setup_analysis(name=args.name,
                        script=args.script,
                        author=args.author,
--- config/Parsers.py
+++ config/Parsers.py
@@ -1,8 +1,8 @@
-def setup_setup_parser(parser):
+def setup_init_parser(parser):
     publicOptions = parser.add_argument_group('User options')
     publicOptions.add_argument('name', help='name of the analysis to be built')
     publicOptions.add_argument('--script', help='name of the main analysis utilitary script', default='DummyAnalysis')
-    publicOptions.add_argument('--author', help="author's \"name <email@address>\" (will use git-config if not specified")
+    publicOptions.add_argument('--author', help="author's \"name <email@address>\" (will use git-config if not specified)")
     publicOptions.add_argument('--standalone', action='store_true', help="also add CMake directive to build standalone package", default=False)
     publicOptions.add_argument('--output-dir', help='output directory where the analysis package will be written')
 
