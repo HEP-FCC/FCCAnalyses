3cc11a987f7282521cf9c1b3afaa0d3a06c7deba
clementhelsens <clementhelsens@users.noreply.github.com>
2022-09-30 15:18:22 +0000

    deploy: ef0469027dda9ec683e2eccc4011b4d1a85f5a2a

--- doc/latest/search/classes_5.js
+++ doc/latest/search/classes_5.js
@@ -9,6 +9,7 @@ var searchData=
   ['getaxisenergy_6',['getAxisEnergy',['../struct_f_c_c_analyses_1_1_algorithms_1_1get_axis_energy.html',1,'FCCAnalyses::Algorithms']]],
   ['getaxismass_7',['getAxisMass',['../struct_f_c_c_analyses_1_1_algorithms_1_1get_axis_mass.html',1,'FCCAnalyses::Algorithms']]],
   ['getaxisn_8',['getAxisN',['../struct_f_c_c_analyses_1_1_algorithms_1_1get_axis_n.html',1,'FCCAnalyses::Algorithms']]],
-  ['getrp2mc_5fp_5ffunc_9',['getRP2MC_p_func',['../struct_f_c_c_analyses_1_1_reconstructed_particle2_m_c_1_1get_r_p2_m_c__p__func.html',1,'FCCAnalyses::ReconstructedParticle2MC']]],
-  ['getthrustpointing_10',['getThrustPointing',['../struct_f_c_c_analyses_1_1_algorithms_1_1get_thrust_pointing.html',1,'FCCAnalyses::Algorithms']]]
+  ['getfloatat_9',['getFloatAt',['../struct_f_c_c_analyses_1_1_calo_ntupleizer_1_1get_float_at.html',1,'FCCAnalyses::CaloNtupleizer']]],
+  ['getrp2mc_5fp_5ffunc_10',['getRP2MC_p_func',['../struct_f_c_c_analyses_1_1_reconstructed_particle2_m_c_1_1get_r_p2_m_c__p__func.html',1,'FCCAnalyses::ReconstructedParticle2MC']]],
+  ['getthrustpointing_11',['getThrustPointing',['../struct_f_c_c_analyses_1_1_algorithms_1_1get_thrust_pointing.html',1,'FCCAnalyses::Algorithms']]]
 ];
