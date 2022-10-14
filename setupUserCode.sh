e7110f133342cecadb1cf23cf4292366237f68d9
Clement Helsens <clement.helsens@cern.ch>
2022-10-14 11:02:13 +0200

    add setupUserCode

---
+++ setupUserCode.sh
@@ -0,0 +1,13 @@
+if [ "${0}" != "${BASH_SOURCE}" ]; then
+  if ! [[ $# -eq 1 ]] ; then
+    echo "Usage: source ${0} <ANALYSISNAME>"
+    echo "Where <ANALYSISNAME> is the local path where the extra analyses will be installed."
+    echo "Example local path (with absolute path): /afs/cern.ch/user/x/xyz/FCCsoft/FCCeePhysicsPerformance/case-studies/flavour/tools/localPythonTools"
+    return 1
+  fi
+  export OUTPUT_DIR=${LOCAL_DIR}/${1}
+  mkdir -p ${OUTPUT_DIR}/build
+  fccanalysis init ${1} --output-dir ${OUTPUT_DIR} --name ${1} --standalone
+else
+  echo "ERROR: This script is meant to be sourced!"
+fi
