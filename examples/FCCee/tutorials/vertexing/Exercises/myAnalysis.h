71ac3f9aabc5ae9e97834195dadd3d74f56ac3bc
Perez <Emmanuel.Perez@cern.ch>
2022-10-18 19:02:41 +0200

    Updates of tutorial files for vertexing and tau3mu

--- examples/FCCee/tutorials/vertexing/Exercises/myAnalysis.h
+++ examples/FCCee/tutorials/vertexing/Exercises/myAnalysis.h
@@ -1,62 +1,68 @@
+// -*- C++ -*-
+//
+/** FCCAnalysis module: myAnalysis
+ *
+ * \file myAnalysis.h
+ * \author Perez <Emmanuel.Perez@cern.ch>
+ *
+ * Description:
+ *   [...]
+ */
 
-#ifndef  MYANALYSIS_ANALYZERS_H
-#define  MYANALYSIS_ANALYZERS_H
+#ifndef myAnalysis_myAnalysis_h
+#define myAnalysis_myAnalysis_h
 
-#include <cmath>
-#include <vector>
+#include "ROOT/RVec.hxx"
+#include "edm4hep/ReconstructedParticle.h"
+
+#include "FCCAnalyses/VertexingUtils.h"
 
 #include "TLorentzVector.h"
-#include "ROOT/RVec.hxx"
 #include "edm4hep/ReconstructedParticleData.h"
-#include "edm4hep/ParticleIDData.h"
-#include "edm4hep/TrackState.h"
 
-#include "VertexingUtils.h"
-#include "ReconstructedParticle2Track.h"
-#include "VertexFitterSimple.h"
+#include "FCCAnalyses/ReconstructedParticle2Track.h"
+#include "FCCAnalyses/VertexFitterSimple.h"
 
 #include <random>
 #include <chrono>
 
-
-namespace FCCAnalyses{
+using namespace FCCAnalyses;
 
 namespace myAnalysis {
+  namespace rv = ROOT::VecOps;
 
- double sum_momentum_tracks( const VertexingUtils::FCCAnalysesVertex&  vertex );
+  void dummy_analysis();
+  rv::RVec<float> dummy_collection(const rv::RVec<edm4hep::ReconstructedParticleData>&);
 
- double tau3mu_vertex_mass( const VertexingUtils::FCCAnalysesVertex&  vertex );
- double tau3mu_raw_mass(  const ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>&  legs ) ;
+double sum_momentum_tracks(const VertexingUtils::FCCAnalysesVertex&  vertex);
+
+double tau3mu_vertex_mass(const VertexingUtils::FCCAnalysesVertex& vertex );
+double tau3mu_raw_mass(const ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>&  legs);
+
+ROOT::VecOps::RVec< ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> > build_triplets(const   ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>&  in , float total_charge);
 
 
-  ROOT::VecOps::RVec< ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> > build_triplets(
-			const 	ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>&  in , float total_charge) ;
+ ROOT::VecOps::RVec< VertexingUtils::FCCAnalysesVertex > build_AllTauVertexObject(const ROOT::VecOps::RVec<ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> >&  triplets, const ROOT::VecOps::RVec<edm4hep::TrackState>& allTracks) ;
 
-  ROOT::VecOps::RVec< VertexingUtils::FCCAnalysesVertex > build_AllTauVertexObject(
-                        const ROOT::VecOps::RVec< ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> >&  triplets,
-                        const ROOT::VecOps::RVec<edm4hep::TrackState>& allTracks ) ;
-
-  ROOT::VecOps::RVec<  double > build_AllTauMasses( const ROOT::VecOps::RVec< VertexingUtils::FCCAnalysesVertex>&  vertices ) ;
+ROOT::VecOps::RVec<  double > build_AllTauMasses(const ROOT::VecOps::RVec< VertexingUtils::FCCAnalysesVertex>&  vertices) ;
 
 
 struct selRP_Fakes {
-  selRP_Fakes( float arg_fakeRate, float arg_mass  );
-  float m_fakeRate = 1e-3;
+  selRP_Fakes( float arg_fakeRate, float arg_mass );
+  float m_fakeRate = 1e-3; //fake rate
   float m_mass = 0.106;  // muon mass
   std::default_random_engine m_generator;
   std::uniform_real_distribution<float> m_flat;
-  std::vector<edm4hep::ReconstructedParticleData>  operator() (ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> in);
+  std::vector<edm4hep::ReconstructedParticleData>  operator() (const ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData>& in);
 };
 
 
-
-
+/*
 float get_p(const edm4hep::MCParticleData& p) ;
 float get_e(const edm4hep::MCParticleData& p) ;
 float get_theta(const edm4hep::MCParticleData& in) ;
+*/
 
-}//end NS myAnalysis
-
-}//end NS FCCAnalyses
+}  // namespace myAnalysis
 
 #endif
