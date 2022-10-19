a0ca3c875e2bb8754ec6331e0d69402482fbd952
Clement Helsens <clement.helsens@cern.ch>
2022-10-19 14:47:36 +0200

    template vec

--- analyzers/dataframe/FCCAnalyses/Utils.h
+++ analyzers/dataframe/FCCAnalyses/Utils.h
@@ -7,7 +7,7 @@ namespace FCCAnalyses {
   namespace Utils {
 
     template<typename T> inline auto getsize( T& vec){ return vec.size();};
-
+    template<typename T> inline ROOT::VecOps::RVec<ROOT::VecOps::RVec<T> >  as_vector(const ROOT::VecOps::RVec<T>& in){return ROOT::VecOps::RVec<ROOT::VecOps::RVec<T> >(1, in);};                                                                                                                                                                              
   }
 }
 
