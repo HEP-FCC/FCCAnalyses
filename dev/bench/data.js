55ed8b994412a472fd812f80e44c8efbbb6ee3c9
github-action-benchmark <github@users.noreply.github.com>
2022-10-20 13:52:44 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1af64f09d8006a1c2f0bf67b7da94efbe7854393

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666273937616,
+  "lastUpdate": 1666273964275,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -10884,6 +10884,58 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/test",
             "value": 6.994265501518274,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement.helsens@cern.ch",
+            "name": "Clement Helsens",
+            "username": "clementhelsens"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1af64f09d8006a1c2f0bf67b7da94efbe7854393",
+          "message": "Merge pull request #220 from clementhelsens/fccswtuto2022\n\nadd mva tuto",
+          "timestamp": "2022-10-20T15:41:37+02:00",
+          "tree_id": "16150d1a2b6c883fcb22fe2007ae01194b2cd54b",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/1af64f09d8006a1c2f0bf67b7da94efbe7854393"
+        },
+        "date": 1666273961303,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 10.641982078552246,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.554599285125732,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.890701055526733,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 11.350926399230957,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
