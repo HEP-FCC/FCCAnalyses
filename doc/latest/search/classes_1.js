9c7968ebaade89e256d512ea1f9dd1918f55f96d
vvolkl <vvolkl@users.noreply.github.com>
2023-01-25 13:49:18 +0000

    deploy: 10ca6e9f94a448d992d8541de1e73079ef1b9d68

--- doc/latest/search/classes_1.js
+++ doc/latest/search/classes_1.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['build_5fcomposite_5fvertex_0',['build_composite_vertex',['../struct_f_c_c_analyses_1_1my_utils_1_1build__composite__vertex.html',1,'FCCAnalyses::myUtils']]],
-  ['build_5fd0_1',['build_D0',['../struct_f_c_c_analyses_1_1my_utils_1_1build___d0.html',1,'FCCAnalyses::myUtils']]],
-  ['build_5ftau23pi_2',['build_tau23pi',['../struct_f_c_c_analyses_1_1my_utils_1_1build__tau23pi.html',1,'FCCAnalyses::myUtils']]],
-  ['build_5ftau23pi_5fvertexing_3',['build_tau23pi_vertexing',['../struct_f_c_c_analyses_1_1my_utils_1_1build__tau23pi__vertexing.html',1,'FCCAnalyses::myUtils']]]
+  ['build_5fd0_0',['build_D0',['../struct_f_c_c_analyses_1_1my_utils_1_1build___d0.html',1,'FCCAnalyses::myUtils']]],
+  ['build_5ftau23pi_1',['build_tau23pi',['../struct_f_c_c_analyses_1_1my_utils_1_1build__tau23pi.html',1,'FCCAnalyses::myUtils']]]
 ];
