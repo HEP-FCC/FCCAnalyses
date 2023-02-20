da0de8d4b5ffb40e53c8b086891e2a03696ec0ce
github-action-benchmark <github@users.noreply.github.com>
2023-02-20 11:01:23 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5e523270663b8760155dba6a3373fcb282c4a719

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676281607267,
+  "lastUpdate": 1676890882886,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -13764,6 +13764,58 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/test",
             "value": 8.298969502567646,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e523270663b8760155dba6a3373fcb282c4a719",
+          "message": "Merge pull request #206 from kunal2796/lcfiplus\n\nImplementation of the Lcfiplus vertexing module",
+          "timestamp": "2023-02-20T11:50:54+01:00",
+          "tree_id": "0b443f24a75dcd9d6196062eef504e02822deb01",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/5e523270663b8760155dba6a3373fcb282c4a719"
+        },
+        "date": 1676890879676,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 9.214627981185913,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 10.878762006759644,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 14.970731258392334,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/test",
+            "value": 11.262612104415894,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
