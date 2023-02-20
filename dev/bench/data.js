4f3f270746482a03fef4f0f04dd0918f72bb1286
github-action-benchmark <github@users.noreply.github.com>
2023-02-20 16:28:05 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a1a1a8693c207d686329cbe594389710c0ac1c7e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676910483344,
+  "lastUpdate": 1676910484941,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -14123,6 +14123,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test/jet_constituents.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "brieuc.francois@cern.ch",
+            "name": "Brieuc Francois",
+            "username": "BrieucF"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a1a1a8693c207d686329cbe594389710c0ac1c7e",
+          "message": "Merge pull request #242 from kjvbrt/bench-clean\n\nUsing full path in the name of the interation benchmark",
+          "timestamp": "2023-02-20T17:19:21+01:00",
+          "tree_id": "a190d4275099ed2065fa5cec58ab615332fdd55b",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/a1a1a8693c207d686329cbe594389710c0ac1c7e"
+        },
+        "date": 1676910484481,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/weaver_inference.py",
+            "value": 3.612621474313526,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 10.801635466435616,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 9.201203853186632,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.9673091270499725,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 9.297410129665309,
+            "unit": "Evt/s"
           }
         ]
       }
