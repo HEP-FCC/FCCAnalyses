071e9b9d2a64ee160ccbbce495dac3bcd8be0363
github-action-benchmark <github@users.noreply.github.com>
2023-02-23 09:11:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for da3f53198ca408c0949d6924031dba40bbc0658e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677143471792,
+  "lastUpdate": 1677143473929,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -14910,6 +14910,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "da3f53198ca408c0949d6924031dba40bbc0658e",
+          "message": "Merge pull request #241 from EmanuelPerez/jets_hemisphere\n\nAdded jets_TwoHemispheres to Algorithms.",
+          "timestamp": "2023-02-23T10:00:08+01:00",
+          "tree_id": "f9b99779cae6ac35edf09120148199e21be49529",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/da3f53198ca408c0949d6924031dba40bbc0658e"
+        },
+        "date": 1677143473169,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/weaver_inference.py",
+            "value": 2.9349436763272494,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.288809777568618,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.5211493233357,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.823820833265875,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.687669369863918,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.166299633729936,
+            "unit": "Evt/s"
           }
         ]
       }
