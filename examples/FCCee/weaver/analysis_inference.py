a56e5abd3d0339fa637d855b198cec9fe79bf84f
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2023-03-01 15:15:49 +0100

    adapted examples to new helpers

--- examples/FCCee/weaver/analysis_inference.py
+++ examples/FCCee/weaver/analysis_inference.py
@@ -1,10 +1,3 @@
-import sys
-import ROOT
-
-# Optional
-nCPUS = 8
-
-
 ## latest particle transformer model, trainied on 9M jets in winter2023 samples
 model_dir = "/eos/experiment/fcc/ee/jet_flavour_tagging/winter2023/wc_pt_13_01_2022"
 model_name = "fccee_flavtagging_edm4hep_wc_v1"
@@ -12,45 +5,55 @@ model_name = "fccee_flavtagging_edm4hep_wc_v1"
 weaver_preproc = "{}/{}.json".format(model_dir, model_name)
 weaver_model = "{}/{}.onnx".format(model_dir, model_name)
 
-import examples.FCCee.weaver.config as weaverConfig # configuration dictionary of variables
 from addons.ONNXRuntime.python.jetFlavourHelper import JetFlavourHelper
+from addons.FastJet.python.jetClusteringHelper import ExclusiveJetClusteringHelper
+
 jetFlavourHelper = None
-
-
+jetClusteringHelper = None
 
 # Mandatory: RDFanalysis class where the use defines the operations on the TTree
 class RDFanalysis:
     # __________________________________________________________
     # Mandatory: analysers funtion to define the analysers to process, please make sure you return the last dataframe, in this example it is df2
     def analysers(df):
-    
+        global jetClusteringHelper
         global jetFlavourHelper
-        jetFlavourHelper = JetFlavourHelper(weaver_preproc, weaver_model, weaverConfig)
-        df = jetFlavourHelper.inference(df)
-    
-        df2 = (
-            df
-            ##### COMPUTE OBSERVABLES FOR ANALYSIS
-            # if not changing training etc... but only interested in the analysis using a trained model (fixed classes), you should only operate in this section.
-            # if you're interested in saving variables used for training don't need to compute them again, just
-            # add them to the list in at the end of the code
-            # EXAMPLE
-            # EVENT LEVEL
-            # extra jet kinematics (not defined earlier)
-            .Define("recojet_pt", "JetClusteringUtils::get_pt(jets_ee_genkt)")
-            .Define("recojet_e", "JetClusteringUtils::get_e(jets_ee_genkt)")
-            .Define("recojet_mass", "JetClusteringUtils::get_m(jets_ee_genkt)")
-            .Define("recojet_phi", "JetClusteringUtils::get_phi(jets_ee_genkt)")
-            .Define("recojet_theta", "JetClusteringUtils::get_theta(jets_ee_genkt)")
-            # counting types of particles composing the jet
+
+        from examples.FCCee.weaver.config import collections, njets
+
+        tag = "test"
+
+        ## define jet clustering parameters
+        jetClusteringHelper = ExclusiveJetClusteringHelper(collections["PFParticles"], njets, tag)
+
+        ## run jet clustering
+        df = jetClusteringHelper.define(df)
+
+        ## define jet flavour tagging parameters
+
+        jetFlavourHelper = JetFlavourHelper(
+            collections,
+            jetClusteringHelper.jets,
+            jetClusteringHelper.constituents,
+            tag,
         )
 
-        return df2
+        ## define observables for tagger
+        df = jetFlavourHelper.define(df)
+
+        ## tagger inference
+        df = jetFlavourHelper.inference(weaver_preproc, weaver_model, df)
+
+        return df
 
     # __________________________________________________________
-    # SAVE PREDICTIONS & OBSERVABLES FOR ANALYSIS
     # Mandatory: output function, please make sure you return the branchlist as a python list
     def output():
-        branchList = ["recojet_mass", "recojet_e", "recojet_pt", "event_invariant_mass", "event_njet"]
+
+        ##  outputs jet properties
+        branchList = jetClusteringHelper.outputBranches()
+
+        ## outputs jet scores and constituent breakdown
         branchList += jetFlavourHelper.outputBranches()
+
         return branchList
