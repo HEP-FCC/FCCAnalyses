b6a86051698497835a36b25c3752b2f8fffb728f
github-action-benchmark <github@users.noreply.github.com>
2023-03-03 09:07:17 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 173e08b6a711caedd9209eb5246f1ec177886435

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677834434866,
+  "lastUpdate": 1677834437231,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -15630,6 +15630,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "173e08b6a711caedd9209eb5246f1ec177886435",
+          "message": "Merge pull request #246 from BrieucF/pi0GammaWeaverInference\n\n[caloNtupleizer] Add weaver GNN inference for pi0/gamma separation wi…",
+          "timestamp": "2023-03-03T09:54:39+01:00",
+          "tree_id": "3c9c727c20549c6a815a8aa3e8dc57a7961dd574",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/173e08b6a711caedd9209eb5246f1ec177886435"
+        },
+        "date": 1677834436372,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.2349029950293158,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.773805432771624,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.813030191354326,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.246573122798071,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.475116565829385,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.485264402518021,
+            "unit": "Evt/s"
           }
         ]
       }
