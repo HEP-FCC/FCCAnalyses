5c285e44bcf39b22172522e10953b215199cf3c5
github-action-benchmark <github@users.noreply.github.com>
2023-03-03 08:53:38 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 0fef057b75aeb2d9025e8670591466b0dc6f5cbc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677833616286,
+  "lastUpdate": 1677833618149,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -15270,6 +15270,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0fef057b75aeb2d9025e8670591466b0dc6f5cbc",
+          "message": "Merge pull request #244 from jeyserma/master\n\nMake ONNX multithread safe",
+          "timestamp": "2023-03-03T09:41:55+01:00",
+          "tree_id": "7402a28489a52fe1ef75f60921ff52c58dd9c9d5",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/0fef057b75aeb2d9025e8670591466b0dc6f5cbc"
+        },
+        "date": 1677833617424,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.5100133086292677,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.8603186035967,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.681836907997934,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.902665652693918,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.824439321804221,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.182005391265152,
+            "unit": "Evt/s"
           }
         ]
       }
