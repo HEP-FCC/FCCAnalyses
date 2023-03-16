2e0df39f24bffd4aac2b993cb1e3c22fa1c88a46
github-action-benchmark <github@users.noreply.github.com>
2023-03-16 09:43:59 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5011e6f821001b788f1173aa4799d74bc91daf24

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678959838144,
+  "lastUpdate": 1678959839767,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -16950,6 +16950,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5011e6f821001b788f1173aa4799d74bc91daf24",
+          "message": "Merge pull request #254 from selvaggi/master\n\nMinor fixes for tagging",
+          "timestamp": "2023-03-16T10:33:37+01:00",
+          "tree_id": "1c0108fc8caa39cf92ff61023d0997a1816c3223",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/5011e6f821001b788f1173aa4799d74bc91daf24"
+        },
+        "date": 1678959839273,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.868146547179981,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.732418496612754,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.37741443392996,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.266854116790902,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.589793680901357,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.558138439040501,
+            "unit": "Evt/s"
           }
         ]
       }
