33d82077f15605bf3a4434be22afce92dd5995ae
github-action-benchmark <github@users.noreply.github.com>
2023-03-23 17:36:49 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a16b9c4abe079bf56a63dc2a6eeff4cca62b6f1a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679593007268,
+  "lastUpdate": 1679593009267,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -17430,6 +17430,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a16b9c4abe079bf56a63dc2a6eeff4cca62b6f1a",
+          "message": "Merge pull request #257 from BNL-FCCee/PathParse\n\nFile path parse fix",
+          "timestamp": "2023-03-23T18:25:50+01:00",
+          "tree_id": "d577019484b81aeedaecf5f37f29b3c263c8a563",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/a16b9c4abe079bf56a63dc2a6eeff4cca62b6f1a"
+        },
+        "date": 1679593008746,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.540945898937646,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.886272682611532,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.472868362902652,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.531364294542764,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.985683624739522,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.7648006349539402,
+            "unit": "Evt/s"
           }
         ]
       }
