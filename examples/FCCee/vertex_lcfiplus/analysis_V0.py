d5c71e6eba2e09f98ef97c9874b2883ecbf2fc9f
Perez <Emmanuel.Perez@cern.ch>
2023-03-23 10:24:17 +0100

    Added a testFile to the two LCFI+ examples

--- examples/FCCee/vertex_lcfiplus/analysis_V0.py
+++ examples/FCCee/vertex_lcfiplus/analysis_V0.py
@@ -1,3 +1,5 @@
+testFile="/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/p8_ee_Zbb_ecm91/events_066726720.root"
+
 #Mandatory: List of processes
 processList = {
     'p8_ee_Zuds_ecm91':{'fraction':0.0001}, #Run 0.01% statistics in one output file named <outputDir>/p8_ee_Zuds_ecm91.root
