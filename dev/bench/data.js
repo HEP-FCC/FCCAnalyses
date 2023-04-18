d35d292a48658f1592d9f280031769a40a6f227f
github-action-benchmark <github@users.noreply.github.com>
2023-04-18 09:28:59 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 3cb2a70f9eaeb9651b515083a4faaba571d6e1df

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681810137476,
+  "lastUpdate": 1681810139335,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -21378,6 +21378,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3cb2a70f9eaeb9651b515083a4faaba571d6e1df",
+          "message": "Merge pull request #269 from jeyserma/master\n\nAdd stat uncertainty and rebin options in plotter",
+          "timestamp": "2023-04-18T11:19:17+02:00",
+          "tree_id": "2699d5f3b7191b02dcfea1bc3f91ea874a84a464",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/3cb2a70f9eaeb9651b515083a4faaba571d6e1df"
+        },
+        "date": 1681810138746,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.9732121587907714,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 10.530560426486412,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.411296968895778,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.861135721602163,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 9.204216647693764,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.1910706892809584,
+            "unit": "Evt/s"
           }
         ]
       }
