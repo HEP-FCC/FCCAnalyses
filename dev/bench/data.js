af7022b0d236869b3795a7ff709b2cb646364762
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 12:24:27 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 499b35769e02e05abc7643f6a5772d9b9bf25b79

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682079864757,
+  "lastUpdate": 1682079867207,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -21978,6 +21978,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "499b35769e02e05abc7643f6a5772d9b9bf25b79",
+          "message": "Merge pull request #277 from jeyserma/histmaker\n\nImplementation of Histmaker",
+          "timestamp": "2023-04-21T14:09:09+02:00",
+          "tree_id": "a8e912435b8d5f49542e7ba52909f5050b5fe03b",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/499b35769e02e05abc7643f6a5772d9b9bf25b79"
+        },
+        "date": 1682079866297,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 1.813853943125858,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 6.404802429067449,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 6.030083160953841,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 4.679749064915778,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 5.5948753589067834,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.45991133653542,
+            "unit": "Evt/s"
           }
         ]
       }
