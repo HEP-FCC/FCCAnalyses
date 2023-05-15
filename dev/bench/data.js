2b81c9917300390d9a1115be04c1f9798e07555a
github-action-benchmark <github@users.noreply.github.com>
2023-05-15 17:45:38 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 51f95567742a73c6b08f3b470ec8d5dd515ab222

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684172735268,
+  "lastUpdate": 1684172737841,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -23898,6 +23898,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "51f95567742a73c6b08f3b470ec8d5dd515ab222",
+          "message": "Merge pull request #287 from kjvbrt/mtinfo\n\nChecking if MT is enabled",
+          "timestamp": "2023-05-15T19:31:52+02:00",
+          "tree_id": "1331a9aa1d8f684249124944c79e43e45cefe5c5",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/51f95567742a73c6b08f3b470ec8d5dd515ab222"
+        },
+        "date": 1684172736775,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 1.9653094052529962,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 6.859017248311363,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 5.660936326586384,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 4.417838918034436,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 6.0716854847046235,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.504819576900587,
+            "unit": "Evt/s"
           }
         ]
       }
