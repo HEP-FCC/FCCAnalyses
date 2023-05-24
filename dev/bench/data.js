8d5f761ed211f7951eb379d523540ffe991448f9
github-action-benchmark <github@users.noreply.github.com>
2023-05-24 16:39:30 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 01cfc5377572af714701c192866c64a50c2d7a1f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684946368122,
+  "lastUpdate": 1684946370371,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -24978,6 +24978,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "brieuc.francois@cern.ch",
+            "name": "Brieuc Francois",
+            "username": "BrieucF"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01cfc5377572af714701c192866c64a50c2d7a1f",
+          "message": "Merge pull request #290 from kjvbrt/chunksfractions\n\nRevising handling of the chunks and fractions in processList",
+          "timestamp": "2023-05-24T18:24:52+02:00",
+          "tree_id": "0cf15c3c97b63804ffefe2fbeba43631cb5de5dd",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/01cfc5377572af714701c192866c64a50c2d7a1f"
+        },
+        "date": 1684946369458,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.0358314240383812,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 7.6447205757322285,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 6.973721152310427,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.862447076695223,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 6.926929346494558,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.9963600792833422,
+            "unit": "Evt/s"
           }
         ]
       }
