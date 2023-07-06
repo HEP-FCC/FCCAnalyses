be40bd9c35c7655130a5e28a13d2ab0b2e1debc1
github-action-benchmark <github@users.noreply.github.com>
2023-07-06 09:50:22 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 2422e7e4b8cd8f99646f5a53e138b2c712dcfef7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688637020768,
+  "lastUpdate": 1688637022464,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -25458,6 +25458,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2422e7e4b8cd8f99646f5a53e138b2c712dcfef7",
+          "message": "Merge pull request #297 from kjvbrt/chunk_path\n\nClearing initial slash in chunk output path stem",
+          "timestamp": "2023-07-06T11:40:30+02:00",
+          "tree_id": "9cb3d6f2d2783e3e848b504c2ab8e56bd5627f24",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/2422e7e4b8cd8f99646f5a53e138b2c712dcfef7"
+        },
+        "date": 1688637021969,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.4984215188876537,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.42166962216932,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.589562607064292,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.471110806747736,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.444029446119516,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.6623608016771225,
+            "unit": "Evt/s"
           }
         ]
       }
