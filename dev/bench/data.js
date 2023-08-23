493903658413473d6f83265563cfe31765cf5199
github-action-benchmark <github@users.noreply.github.com>
2023-08-23 10:18:35 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for ba08f1857d65f897609974a651920a32b01b17e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692785912948,
+  "lastUpdate": 1692785915121,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -28098,6 +28098,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ba08f1857d65f897609974a651920a32b01b17e3",
+          "message": "Merge pull request #310 from kjvbrt/split-fix\n\nExiting when FCCDICTSDIR is empty",
+          "timestamp": "2023-08-23T12:00:47+02:00",
+          "tree_id": "7497d8ae6c2e7cf8ff13cbfd71b3aaa4f9ada945",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/ba08f1857d65f897609974a651920a32b01b17e3"
+        },
+        "date": 1692785914297,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 1.5679142671292423,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 6.130120634631669,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 1.0211211165395997,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 3.862725659406463,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 5.435884264389141,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.1738351884035865,
+            "unit": "Evt/s"
           }
         ]
       }
