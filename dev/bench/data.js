2616d5798424c6bb5ca2bd0a91ec27cf55fbcb75
github-action-benchmark <github@users.noreply.github.com>
2023-09-19 09:52:06 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for b9696683b3f157696bf545a257326aef6b5627fa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695117122834,
+  "lastUpdate": 1695117126478,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -28578,6 +28578,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9696683b3f157696bf545a257326aef6b5627fa",
+          "message": "Merge pull request #312 from kjvbrt/sel_type\n\nImplementing selection by type for Reconstructed particles",
+          "timestamp": "2023-09-19T11:33:31+02:00",
+          "tree_id": "2f54af26874d50bc6b193651091bc533cacf9a62",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/b9696683b3f157696bf545a257326aef6b5627fa"
+        },
+        "date": 1695117125488,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 1.5542448192759584,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 6.37025684156093,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 5.31075691692413,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 4.190609893566623,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 5.537358745371396,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.366918280134926,
+            "unit": "Evt/s"
           }
         ]
       }
