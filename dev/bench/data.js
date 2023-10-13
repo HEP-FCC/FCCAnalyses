a20a80e06ded096ab902f6d6389f0eb488804b1d
github-action-benchmark <github@users.noreply.github.com>
2023-10-13 14:19:30 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5344cad99ad342d5504cd76246d25d2516ccd6cb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697206768303,
+  "lastUpdate": 1697206770369,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -28818,6 +28818,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5344cad99ad342d5504cd76246d25d2516ccd6cb",
+          "message": "Merge pull request #313 from EmanuelPerez/bugfix\n\nBugfix in MCParticle::get_indices_MotherByIndex.",
+          "timestamp": "2023-10-13T16:08:25+02:00",
+          "tree_id": "d8c7fc84f794e34000d162acd5572ed4d5891f21",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/5344cad99ad342d5504cd76246d25d2516ccd6cb"
+        },
+        "date": 1697206769748,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.2860672853553257,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 6.9805207989797,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 6.578931352807566,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.0146043052601925,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 6.6554433621802245,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.5932168379968976,
+            "unit": "Evt/s"
           }
         ]
       }
