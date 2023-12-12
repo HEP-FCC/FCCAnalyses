79f4f16fa5d06529df25f7ef5cd3d249f85ea5e5
github-action-benchmark <github@users.noreply.github.com>
2023-12-12 10:11:27 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for ec3414183fbe8840329ece50a54dba59c6888282

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702375885444,
+  "lastUpdate": 1702375886915,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -30618,6 +30618,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ec3414183fbe8840329ece50a54dba59c6888282",
+          "message": "Merge pull request #323 from flaviacetorelli/master\n\nSCEPCal ntuplizer and plotter",
+          "timestamp": "2023-12-12T11:04:00+01:00",
+          "tree_id": "44a7748229fb0e83bd6303e663365d89d217de22",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/ec3414183fbe8840329ece50a54dba59c6888282"
+        },
+        "date": 1702375886479,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 3.0077425758603256,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 10.019127167476515,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.494145033920045,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.384361645651021,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.838340411465518,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.146009237495786,
+            "unit": "Evt/s"
           }
         ]
       }
