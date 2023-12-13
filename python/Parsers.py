1d9183e7da232f3c69d4c1edb5dcb1b93caea4d8
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2023-12-13 15:53:21 +0100

    Remove old way (--eloglevel) to set verbosity level (#326)

--- python/Parsers.py
+++ python/Parsers.py
@@ -93,9 +93,6 @@ def setup_run_parser(parser):
                         help='rerun failed jobs')
     parser.add_argument('--jobdir', type=str, default='output.root',
                         help='specify the batch job directory')
-    parser.add_argument('--eloglevel', type=str, default='kUnset',
-                        choices=['kUnset', 'kFatal', 'kError', 'kWarning', 'kInfo', 'kDebug'],
-                        help='specify the RDataFrame ELogLevel')
 
     # Internal argument, not to be used by the users
     parser.add_argument('--batch', action='store_true', default=False,
@@ -108,9 +105,6 @@ def setup_run_parser_final(parser):
     '''
     parser.add_argument('anafile_path',
                         help='path to analysis_final script')
-    parser.add_argument('--eloglevel', type=str, default='kUnset',
-                        choices=['kUnset', 'kFatal', 'kError', 'kWarning', 'kInfo', 'kDebug'],
-                        help='Specify the RDataFrame ELogLevel')
 
 
 def setup_run_parser_plots(parser):
