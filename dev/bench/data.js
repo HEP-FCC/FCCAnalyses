aa69a44dd9ef5566dfd4fae84bb5faa6593d0f98
github-action-benchmark <github@users.noreply.github.com>
2023-12-15 12:35:38 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 045eff902c83567ca566ace92fdb8676c38b642e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702643736586,
+  "lastUpdate": 1702643738068,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -31698,6 +31698,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zuoxunwu@gmail.com",
+            "name": "Xunwu Zuo",
+            "username": "zuoxunwu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "045eff902c83567ca566ace92fdb8676c38b642e",
+          "message": "Initial analysis example for top electroweak coupling measurement (#328)\n\n* Add first script for top EWK analysis\r\n\r\n* Add initial README for top electroweak example",
+          "timestamp": "2023-12-15T13:25:42+01:00",
+          "tree_id": "054a6cc9f4df4d294f5f62c70f57ec88480e16b4",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/045eff902c83567ca566ace92fdb8676c38b642e"
+        },
+        "date": 1702643737635,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 1.988807382251743,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.676019412646827,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.801197581168061,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.67138624284898,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.723304805576953,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.2067891292008857,
+            "unit": "Evt/s"
           }
         ]
       }
