2b4137ca98967795896775e24e4338354d8b4e00
Abraham Tishelman-Charny <abraham.tishelman.charny@cern.ch>
2024-02-01 03:33:10 -0500

    Adding tau score to ZH config

--- ZH_Hadronic_stage1.py
+++ ZH_Hadronic_stage1.py
@@ -13,25 +13,20 @@ import sys
 sys.path.append("/usatlas/u/atishelma/FCC/FCCAnalyses/")
 
 from examples.FCCee.weaver.config import collections
+from CustomDefinitions import CustomDefinitions
 
-#configFile = "/afs/cern.ch/work/a/atishelm/private/FCC/BNL-Analyses/RunConfig.yaml" # for the moment, need to specify full path so that HTCondor node can find this file (since afs is mounted). Need to check how to pass this as an input file to HTCondor job.
-#with open(configFile, 'r') as cfg:
-#    values = yaml.safe_load(cfg)
-    
-#    batch = values["batch"]
-#    EOSoutput = values["EOSoutput"]
-#    JobName = values["JobName"]
-#    njets = values["njets"]
+# originally had YAML config here. Not strictly necessary. Check previous commits if you want an example.
 
-# batch with EOS output
-batch = 1
-EOSoutput = 0
-#JobName = "Test_SDCC_Condor"
-JobName = "ZHadronic_4JetReco"
-njets = 4
+batch = 1 # use HTCondor
+EOSoutput = 0 # output to EOS
+JobName = "ZHadronic_4JetReco" # job named used for output directory
+njets = 4 # number of jets in exclusive reclustering
+#exclusive = 1 # to be implemented: type of reclustering to e.g. inclusive vs. exclusive
 
-#exclusive = 1 
-
+# originally was using the flag definitions below. Should follow the path of the `exclusive` flag to see what it actually means.
+# these files may be relevant:
+#addons/FastJet/python/jetClusteringHelper.py
+#addons/FastJet/src/JetClustering.cc
   # flag for exclusive jet clustering. Possible choices are: 
   # 0 = inclusive clustering, for duram kt 
   # 1 = exclusive clustering that would be obtained when running the algorithm with the given dcut, 
@@ -39,12 +34,12 @@ njets = 4
   # 3 = exclusive clustering when the event is clustered (in the exclusive sense) up to exactly njets,
   # 4 = exclusive jets obtained at the given ycutei 
 
-# local
-#batch = 1
-#EOSoutput = 1
+# example for running locally (will also happen by default if you specify an input file at the command line)
+#batch = 0
+#EOSoutput = 0
 #JobName = "Inclusive_R0p4"
 #njets = 4
-#exclusive = 0 # flag for exclusive jet clustering. Possible choices are: 
+#exclusive = 0 
 
 print("batch:",batch)
 print("EOSoutput:",EOSoutput)
@@ -108,99 +103,25 @@ prodTag     = "FCCee/winter2023/IDEA/"
 procDict = "FCCee_procDict_winter2023_IDEA.json" 
 
 if(EOSoutput):
-    #outputDir = f"{JobName}/stage1/"
+    # example output directory on EOS - note that by default this includes copying the output file from one location to another
     #outputDirEos = f"/eos/user/a/atishelm/ntuples/FCC/{JobName}/stage1/" # if you define outputDirEos, this process creates the file locally and copies it to eos.
     #outputDir = f"/eos/user/a/atishelm/ntuples/FCC/ZH_Hadronic_4JetReco/"
-    #outputDir = f"/eos/user/a/atishelm/ntuples/FCC/ZH_Hadronic_InclusiveReco/"
-    
-    eosType = "eosuser"
+
+    eosType = "eosuser" # specify as necessary
 else:
     outputDir   = f"/usatlas/atlas01/atlasdisk/users/atishelma/{JobName}/stage1/"
-    #outputDir   = f"{JobName}/stage1/"
 
 runBatch    = batch
 batchQueue = "testmatch" 
-#eosType = eosType
 
 # Define any functionality which is not implemented in FCCAnalyses
 
 import ROOT
-ROOT.gInterpreter.Declare("""
-ROOT::VecOps::RVec<double> SumFlavorScores(ROOT::VecOps::RVec<double> recojet_isFlavor) {
-
-    double score_1, score_2, pair_score; 
-    ROOT::VecOps::RVec<double> recojetpair_isFlavor;
-
-    // cannot compute any mass pair flavour score values, return a single non-physical value
-    if(recojet_isFlavor.size() < 2){
-        recojetpair_isFlavor.push_back(-99);
-        return recojetpair_isFlavor; 
-    }
-
-
-    // For each jet, take its flavor score sum with the remaining jets. Stop at last jet.
-    for(int i = 0; i < recojet_isFlavor.size()-1; ++i) {
-
-    score_1 = recojet_isFlavor.at(i); 
-
-        for(int j=i+1; j < recojet_isFlavor.size(); ++j){ // go until end
-            score_2 = recojet_isFlavor.at(j);
-            pair_score = score_1 + score_2; 
-            recojetpair_isFlavor.push_back(pair_score);
-
-        }
-    }
-
-    return recojetpair_isFlavor;
-}
-
-
-
-ROOT::VecOps::RVec<double> all_recoil_masses(ROOT::VecOps::RVec<TLorentzVector> all_jet_4vectors){
-  double m_sqrts = 240;
-  auto recoil_p4 = TLorentzVector(0, 0, 0, m_sqrts);
-  TLorentzVector tv1, tv2, tvpair; 
-  double E, px, py, pz, recoil_mass;
-  ROOT::VecOps::RVec<double> recoil_masses;
-
-  // cannot compute any mass pair values, return a single non-physical value
-  if(all_jet_4vectors.size() < 2){
-    recoil_masses.push_back(-99);
-    return recoil_masses;  
-  }
-
-    // For each jet, take its recoil mass using the remaining jets. Stop at last jet.
-    for(int i = 0; i < all_jet_4vectors.size()-1; ++i) {
-
-        tv1 = all_jet_4vectors.at(i);
-
-        for(int j=i+1; j < all_jet_4vectors.size(); ++j){ // go until end
-
-            tv2 = all_jet_4vectors.at(j); 
-            E = tv1.E() + tv2.E();
-            px = tv1.Px() + tv2.Px();
-            py = tv1.Py() + tv2.Py();
-            pz = tv1.Pz() + tv2.Pz();
-
-            tvpair.SetPxPyPzE(px, py, pz, E);
-
-            recoil_p4 = TLorentzVector(0, 0, 0, m_sqrts);
-            recoil_p4 -= tvpair; 
-
-            recoil_mass = recoil_p4.M();
-            recoil_masses.push_back(recoil_mass);
-
-        }
-    }
-
-  return recoil_masses;
-
-}
-
-""") 
+ROOT.gInterpreter.Declare(CustomDefinitions)
 
 # ____________________________________________________________
 def get_file_path(url, filename):
+    print("Looking for file:",filename)
     if os.path.exists(filename):
         return os.path.abspath(filename)
     else:
@@ -213,7 +134,7 @@ def get_file_path(url, filename):
 testFile = "https://fccsw.web.cern.ch/fccsw/testsamples/wzp6_ee_nunuH_Hss_ecm240.root"
 
 ## latest particle transformer model, trainied on 9M jets in winter2023 samples - need to separate train/test samples?
-model_name = "fccee_flavtagging_edm4hep_wc_v1"
+model_name = "fccee_flavtagging_edm4hep_wc"
 
 ## model files needed for unit testing in CI
 url_model_dir = "https://fccsw.web.cern.ch/fccsw/testsamples/jet_flavour_tagging/winter2023/wc_pt_13_01_2022/"
@@ -221,9 +142,11 @@ url_preproc = "{}/{}.json".format(url_model_dir, model_name)
 url_model = "{}/{}.onnx".format(url_model_dir, model_name)
 
 ## model files locally stored on /eos
-model_dir = "/eos/experiment/fcc/ee/jet_flavour_tagging/winter2023/wc_pt_13_01_2022/"
+#model_dir = "/eos/experiment/fcc/ee/jet_flavour_tagging/winter2023/wc_pt_7classes_12_04_2023/"
+model_dir = "./" # copied it locally
 local_preproc = "{}/{}.json".format(model_dir, model_name)
 local_model = "{}/{}.onnx".format(model_dir, model_name)
+
 
 ## get local file, else download from url
 weaver_preproc = get_file_path(url_preproc, local_preproc)
@@ -232,7 +155,6 @@ weaver_model = get_file_path(url_model, local_model)
 from examples.FCCee.weaver.config import (
     variables_pfcand,
     variables_jet,
-    #variables_event, # assumes at least 2 jets for event_invariant_mass variable
 )
 
 from addons.ONNXRuntime.python.jetFlavourHelper import JetFlavourHelper
@@ -242,30 +164,28 @@ jetFlavourHelper = None
 jetClusteringHelper = None
 
 def analysis_sequence(df):
-    anatag = ""
-
     collections["Electrons"] = "Electron"
     collections["Muons"] = "Muon"
     collections["Photons"] = "Photons"
 
     df = (
         # electrons
-        df.Alias("Electron0{}".format(anatag), "{}#0.index".format(collections["Electrons"]))
+        df.Alias("Electron0", "{}#0.index".format(collections["Electrons"]))
         .Define(
-            "electrons{}".format(anatag),
-            "ReconstructedParticle::get(Electron0{}, {})".format(anatag, collections["PFParticles"]),
+            "electrons",
+            "ReconstructedParticle::get(Electron0, {})".format(collections["PFParticles"]),
         )
-        .Define("event_nel{}".format(anatag), "electrons{}.size()".format(anatag))  # are these isolated?
-        .Define("electrons_p{}".format(anatag), "ReconstructedParticle::get_p(electrons{})[0]".format(anatag))
+        .Define("event_nel", "electrons.size()")
+        .Define("electrons_p", "ReconstructedParticle::get_p(electrons)[0]")
         
         # muons
-        .Alias("Muon0{}".format(anatag), "{}#0.index".format(collections["Muons"]))
+        .Alias("Muon0", "{}#0.index".format(collections["Muons"]))
         .Define(
-            "muons{}".format(anatag),
-            "ReconstructedParticle::get(Muon0{}, {})".format(anatag, collections["PFParticles"]),
+            "muons",
+            "ReconstructedParticle::get(Muon0, {})".format(collections["PFParticles"]),
         )
-        .Define("event_nmu{}".format(anatag), "muons{}.size()".format(anatag))
-        .Define("muons_p{}".format(anatag), "ReconstructedParticle::get_p(muons{})[0]".format(anatag))
+        .Define("event_nmu", "muons.size()")
+        .Define("muons_p", "ReconstructedParticle::get_p(muons)[0]")
 
         #EVENTWIDE VARIABLES: Access quantities that exist only once per event, such as the missing energy (despite the name, the MissingET collection contains the total missing energy)
         .Define("RecoMissingEnergy_e", "ReconstructedParticle::get_e(MissingET)")
@@ -280,7 +200,7 @@ def analysis_sequence(df):
     )
 
     for x in range(1, 9):
-        df = (df.Define("d_{}{}{}".format(x,x+1,anatag), "JetClusteringUtils::get_exclusive_dmerge(_jet{}, {})".format(anatag, x))) #dmerge from x+1 to x
+        df = (df.Define("d_{}{}".format(x,x+1), "JetClusteringUtils::get_exclusive_dmerge(_jet, {})".format(x))) #dmerge from x+1 to x
 
     return df
 
@@ -294,7 +214,7 @@ def jet_sequence(df, njets):
     tag = ""
 
     ## define jet clustering parameters
-    #jetClusteringHelper = ExclusiveJetClusteringHelper(collections["PFParticles"], njets, exclusive, tag)
+    # This is where you can try passing the "exclusive" parameter, and you will have to follow it to the ExclusiveJetClusteringHelper definition, which then goes to something else...
     #jetClusteringHelper = ExclusiveJetClusteringHelper(collections["PFParticles"], njets, exclusive, tag)
     jetClusteringHelper = ExclusiveJetClusteringHelper(collections["PFParticles"], njets, tag)
 
@@ -312,15 +232,38 @@ def jet_sequence(df, njets):
     ## define observables for tagger
     df = jetFlavourHelper.define(df)
     df = df.Define("jet_p4", "JetConstituentsUtils::compute_tlv_jets({})".format(jetClusteringHelper.jets))
+   
+    # apply energy correction
+    jet_reco_vars = ["e", "p", "px", "py", "pz", "m", "theta"]
+    for jet_reco_var in jet_reco_vars:
+        df=(df.Define("recojet_{}".format(jet_reco_var), "JetClusteringUtils::get_{}(jet)".format(jet_reco_var)))
+    
+    # phi has slightly different naming
+    df=(df.Define("recojet_phi", "JetClusteringUtils::get_phi_std(jet)"))
+ 
+
+    df = df.Define("jets_tlv_corr", "FCCAnalyses::energyReconstructFourJet(recojet_px, recojet_py, recojet_pz, recojet_e)")
+
+    jet_corr_vars = ["e", "px", "py", "pz"]
+    for jet_corr_var in jet_corr_vars: df = df.Define("jet_%s_corr"%(jet_corr_var), "FCCAnalyses::TLVHelpers::get_%s(jets_tlv_corr)"%(jet_corr_var))
+    
+    #df = df.Define("jet_e_corr", "FCCAnalyses::TLVHelpers::get_e(jets_tlv_corr)")
+    #df = df.Define("jet_px_corr", "FCCAnalyses::TLVHelpers::get_px(jets_tlv_corr)")
+    #df = df.Define("jet_py_corr", "FCCAnalyses::TLVHelpers::get_py(jets_tlv_corr)")
+    #df = df.Define("jet_pz_corr", "FCCAnalyses::TLVHelpers::get_pz(jets_tlv_corr)")
+
     df = df.Define("all_invariant_masses", "JetConstituentsUtils::all_invariant_masses(jet_p4)")
     df = df.Define("recoil_masses", "all_recoil_masses(jet_p4)")
-
+    
     ## tagger inference
     df = jetFlavourHelper.inference(weaver_preproc, weaver_model, df) 
 
     ## define variables using tagger inference outputs
     df = df.Define("recojetpair_isC", "SumFlavorScores(recojet_isC)") 
     df = df.Define("recojetpair_isB", "SumFlavorScores(recojet_isB)") 
+
+    df = df.Define("jetconstituents", "FCCAnalyses::JetClusteringUtils::get_constituents(_jet)")
+    df = df.Define("jets_truth", "FCCAnalyses::jetTruthFinder(jetconstituents, ReconstructedParticles, Particle)")
 
     return df
 
@@ -330,7 +273,7 @@ class RDFanalysis:
     # Mandatory: analysers funtion to define the analysers to process, please make sure you return the last dataframe, in this example it is df2
     def analysers(df):
         df = jet_sequence(df, njets)
-        #df = jet_sequence(df, njets, exclusive)
+        #df = jet_sequence(df, njets, exclusive) # again, was playing with exclusive parameter here. Don't remember if you need to pass it here.
         df = analysis_sequence(df)
 
         return df
@@ -338,18 +281,16 @@ class RDFanalysis:
     # __________________________________________________________
     # Mandatory: output function, please make sure you return the branchlist as a python list
     def output():
-
-        anatag = ""
-
         branchList = []
 
         # jets
         branches_jet = list(variables_jet.keys())
-        #branches_event = list(variables_event.keys())
-
-        #branchList = branches_event  + branches_jet 
         branchList = branches_jet 
         branchList += jetFlavourHelper.outputBranches()
+
+        print("jet branches:",list(variables_jet.keys()))
+        print("jet flavor helper branches:",jetFlavourHelper.outputBranches())
+ 
         branchList += ["event_njet"]
         
         branchList += ["all_invariant_masses"]
@@ -357,8 +298,18 @@ class RDFanalysis:
         branchList += ["recojetpair_isB"]
         branchList += ["recoil_masses"]
 
+        branchList += ["jet_e_corr"]
+        branchList += ["jet_px_corr"]
+        branchList += ["jet_py_corr"]
+        branchList += ["jet_pz_corr"]
+
+        #branchList += ["recojet_isTAU"]
+
+        # truth info
+        branchList += ["jets_truth"]
+
         for x in range(1, 9):
-            branchList.append("d_{}{}{}".format(x,x+1,anatag))
+            branchList.append("d_{}{}".format(x,x+1))
         
         # leptons
         branchList += ["event_nel"]
@@ -369,7 +320,6 @@ class RDFanalysis:
         for MET_var in MET_vars:
             branchList += [f"RecoMissingEnergy_{MET_var}"]
 
-        # remove duplicates 
-        branchList = list(set(branchList))
+        branchList = list(set(branchList)) # remove duplicates
 
         return branchList
