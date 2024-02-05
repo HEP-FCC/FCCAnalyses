9bdfdf9e9f52f5b52599261b3493b9bf04e2a3cf
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2024-02-05 19:07:35 +0100

    Getting ONNX test files from fccsw site (#346)

--- tests/get_test_inputs.sh
+++ tests/get_test_inputs.sh
@@ -7,8 +7,8 @@ export TEST_INPUT_DATA_DIR
 cd $TEST_INPUT_DATA_DIR
 
 # retrieve Weaver tests inputs
-wget https://key4hep.web.cern.ch/key4hep/testFiles/weaverInference/preprocess.json > /dev/null 2>&1
-wget https://key4hep.web.cern.ch/key4hep/testFiles/weaverInference/fccee_flavtagging_dummy.onnx > /dev/null 2>&1
+curl -O -L https://fccsw.web.cern.ch/fccsw/testsamples/fccanalyses/weaver-inference/preprocess.json > /dev/null 2>&1
+curl -O -L https://fccsw.web.cern.ch/fccsw/testsamples/fccanalyses/weaver-inference/fccee_flavtagging_dummy.onnx > /dev/null 2>&1
 
 # announce where we store variables to the outside
 echo -n $TEST_INPUT_DATA_DIR
