d4b31de66af7cab8033cef4d7a96c5879017f53b
github-action-benchmark <github@users.noreply.github.com>
2024-02-05 18:15:16 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 9bdfdf9e9f52f5b52599261b3493b9bf04e2a3cf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707156915172,
+  "lastUpdate": 1707156916470,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -33618,6 +33618,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9bdfdf9e9f52f5b52599261b3493b9bf04e2a3cf",
+          "message": "Getting ONNX test files from fccsw site (#346)",
+          "timestamp": "2024-02-05T19:07:35+01:00",
+          "tree_id": "3b518314db42458c0177ed35a0409fb8cc0d44b4",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/9bdfdf9e9f52f5b52599261b3493b9bf04e2a3cf"
+        },
+        "date": 1707156916025,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.9809509152568836,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.660307494275289,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.682088282944854,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.620637994141965,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.626423662938514,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.1287817125833577,
+            "unit": "Evt/s"
           }
         ]
       }
