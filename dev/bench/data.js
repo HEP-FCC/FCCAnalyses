58c297585cfd8222051d5239874efd51b3bf9bb5
github-action-benchmark <github@users.noreply.github.com>
2024-02-28 14:23:54 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f0ed1a6b2df6f75a5fe8da2dd6d24fef2f781160

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709130232890,
+  "lastUpdate": 1709130234403,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -36011,6 +36011,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f0ed1a6b2df6f75a5fe8da2dd6d24fef2f781160",
+          "message": "Disabling weaver test until we have EDM4hep 1.0 compatible files (#356)",
+          "timestamp": "2024-02-28T15:15:42+01:00",
+          "tree_id": "06fc021e2867faa89a0cc457c367e868fb02e9e3",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/f0ed1a6b2df6f75a5fe8da2dd6d24fef2f781160"
+        },
+        "date": 1709130233903,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 7.845932909116244,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.996597059688592,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.799237321573483,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.731819043863409,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.1060706987908158,
+            "unit": "Evt/s"
           }
         ]
       }
