40aa5f02bfd2421e60f27f1fd77e7995baa7cf71
github-action-benchmark <github@users.noreply.github.com>
2024-04-24 08:45:56 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 2040b3b8409f3d01feb3421e7e8ceeeac970d89f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713948353838,
+  "lastUpdate": 1713948356007,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -37115,6 +37115,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smieško",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "juraj.smiesko@cern.ch",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "distinct": true,
+          "id": "2040b3b8409f3d01feb3421e7e8ceeeac970d89f",
+          "message": "Using TFile.Open in histmaker (#367)",
+          "timestamp": "2024-04-24T10:33:50+02:00",
+          "tree_id": "bbffd344a0de989255756025438a4927fd3b1005",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/2040b3b8409f3d01feb3421e7e8ceeeac970d89f"
+        },
+        "date": 1713948355166,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 5.574857317788415,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 4.888820155603806,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.747609488409937,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 5.296808662879811,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.6030385187582405,
+            "unit": "Evt/s"
           }
         ]
       }
