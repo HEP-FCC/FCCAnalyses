df35b11ab78b26795f59552528724ce22b112fba
github-action-benchmark <github@users.noreply.github.com>
2024-07-03 16:08:43 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for ff50f2541693c369e49790ae3249bbb4e5d13997

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720022921484,
+  "lastUpdate": 1720022923047,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -40055,6 +40055,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.madlener@desy.de",
+            "name": "Thomas Madlener",
+            "username": "tmadlener"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff50f2541693c369e49790ae3249bbb4e5d13997",
+          "message": "Make ONNXRuntime work with older and newer versions of onnxruntime (#381)\n\n* Rig a similar target as is available for newer onnxruntime versions\r\n\r\n* Do not indiscriminantly look for older versions\r\n\r\n* Consistently use onnxruntime::onnxruntime targets\r\n\r\n* Cleanup cmake log messages\r\n\r\n* Make things work with old and new versions of OnnxRuntime",
+          "timestamp": "2024-07-03T18:00:44+02:00",
+          "tree_id": "8100a94e4310b4e447e6e151a3ecf1efb106ec34",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/ff50f2541693c369e49790ae3249bbb4e5d13997"
+        },
+        "date": 1720022922700,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.452113291298705,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.435874033492176,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 7.181130985532799,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.545873610025975,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.185393273573439,
+            "unit": "Evt/s"
           }
         ]
       }
