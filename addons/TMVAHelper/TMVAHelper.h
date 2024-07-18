ccc302a50d8473fe85d6259b2d9c5472ce12039c
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2024-07-18 11:41:58 +0200

    TMVAHelper: Fixing nightlies build (#388)

--- addons/TMVAHelper/TMVAHelper.h
+++ addons/TMVAHelper/TMVAHelper.h
@@ -1,22 +1,28 @@
 #ifndef TMVAHelper_TMVAHelper_h
 #define TMVAHelper_TMVAHelper_h
 
-#include <tbb/task_arena.h>
+// ROOT
 #include "ROOT/RVec.hxx"
 #include "TMVA/RBDT.hxx"
 
+// TBB
+#include <tbb/task_arena.h>
+
+// std
+#include <string>
+#include <vector>
 
 class tmva_helper_xgb {
-    public:
-        explicit tmva_helper_xgb(const std::string &filename, const std::string &name, const unsigned &nvars, const unsigned int nslots = 1);
-        virtual ~tmva_helper_xgb();
-        ROOT::VecOps::RVec<float> operator()(const ROOT::VecOps::RVec<float> vars);
+public:
+  tmva_helper_xgb(const std::string &filename, const std::string &name,
+                  const unsigned int nslots = 1);
+  ~tmva_helper_xgb() {};
+  ROOT::VecOps::RVec<float> operator()(const ROOT::VecOps::RVec<float> vars);
 
-    private:
-        unsigned int nvars_;
-        TMVA::Experimental::RBDT<> model_;
-        std::vector<TMVA::Experimental::RBDT<>> interpreters_;
+private:
+  // Default backend (template parameter) is:
+  // TMVA::Experimental::BranchlessJittedForest<float>
+  std::vector<TMVA::Experimental::RBDT<>> m_interpreters;
 };
-
 
 #endif
--- addons/TMVAHelper/python/TMVAHelper.py
+++ addons/TMVAHelper/python/TMVAHelper.py
@@ -1,39 +1,38 @@
-
 import ROOT
-import pathlib
 
 ROOT.gInterpreter.ProcessLine('#include "TMVAHelper/TMVAHelper.h"')
 ROOT.gSystem.Load("libTMVAHelper")
 
+
 class TMVAHelperXGB():
-
     def __init__(self, model_input, model_name, variables=[]):
-
-        if len(variables) == 0: # try to get the variables from the model file (saved as a TList)
+        # try to get the variables from the model file (saved as a TList)
+        if len(variables) == 0:
             fIn = ROOT.TFile(model_input)
             variables_ = fIn.Get("variables")
             self.variables = [str(var.GetString()) for var in variables_]
             fIn.Close()
-        else: 
+        else:
             self.variables = variables
-        self.nvars = len(self.variables)
         self.model_input = model_input
         self.model_name = model_name
-        self.nthreads = ROOT.GetThreadPoolSize() 
+        self.nthreads = ROOT.GetThreadPoolSize()
 
-        self.tmva_helper = ROOT.tmva_helper_xgb(self.model_input, self.model_name, self.nvars, self.nthreads)
+        self.tmva_helper = ROOT.tmva_helper_xgb(self.model_input,
+                                                self.model_name,
+                                                self.nthreads)
         self.var_col = f"tmva_vars_{self.model_name}"
 
-    def run_inference(self, df, col_name = "mva_score"):
+    def run_inference(self, df, col_name="mva_score"):
 
         # check if columns exist in the dataframe
         cols = df.GetColumnNames()
         for var in self.variables:
-            if not var in cols:
+            if var not in cols:
                 raise Exception(f"Variable {var} not defined in dataframe.")
 
         vars_str = ', (float)'.join(self.variables)
-        df = df.Define(self.var_col, f"ROOT::VecOps::RVec<float>{{{vars_str}}}")
+        df = df.Define(self.var_col,
+                       f"ROOT::VecOps::RVec<float>{{{vars_str}}}")
         df = df.Define(col_name, self.tmva_helper, [self.var_col])
         return df
-
