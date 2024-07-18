66ef204ca86eac25b887fd58a0067f8f1f0ddd53
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-07-18 11:06:17 +0200

    TMVAHelper: Fixing nightlies build

--- addons/TMVAHelper/src/TMVAHelper.cc
+++ addons/TMVAHelper/src/TMVAHelper.cc
@@ -1,23 +1,24 @@
 #include "TMVAHelper/TMVAHelper.h"
 
+tmva_helper_xgb::tmva_helper_xgb(const std::string &filename,
+                                 const std::string &name,
+                                 const unsigned int nslots) {
 
-tmva_helper_xgb::tmva_helper_xgb(const std::string &filename, const std::string &name, const unsigned &nvars, const unsigned int nslots) :
-    model_(name, filename), nvars_(nvars) {
-
-    const unsigned int nslots_actual = std::max(nslots, 1U);
-    interpreters_.reserve(nslots_actual);
-    for (unsigned int islot = 0; islot < nslots_actual; ++islot) {
-        interpreters_.emplace_back(model_);
-    }
+  const unsigned int nslots_actual = std::max(nslots, 1U);
+  m_interpreters.reserve(nslots_actual);
+  for (unsigned int islot = 0; islot < nslots_actual; ++islot) {
+    m_interpreters.emplace_back(TMVA::Experimental::RBDT<>(name, filename));
+  }
 }
 
-ROOT::VecOps::RVec<float> tmva_helper_xgb::operator()(const ROOT::VecOps::RVec<float> vars) {
-    auto const tbb_slot = std::max(tbb::this_task_arena::current_thread_index(), 0);
-    if (tbb_slot >= interpreters_.size()) {
-        throw std::runtime_error("Not enough interpreters allocated for number of tbb threads");
-    }
-    auto &interpreter_data = interpreters_[tbb_slot];
-    return interpreter_data.Compute(vars);
+ROOT::VecOps::RVec<float>
+tmva_helper_xgb::operator()(const ROOT::VecOps::RVec<float> vars) {
+  auto const tbb_slot =
+      std::max(tbb::this_task_arena::current_thread_index(), 0);
+  if (tbb_slot >= m_interpreters.size()) {
+    throw std::runtime_error(
+        "Not enough interpreters allocated for number of tbb threads");
+  }
+  auto &interpreter_data = m_interpreters[tbb_slot];
+  return interpreter_data.Compute(vars);
 }
-
-tmva_helper_xgb::~tmva_helper_xgb() {}
