0de3ad61272715309ed7cce10e3ab4bf9e11f08e
github-action-benchmark <github@users.noreply.github.com>
2024-07-18 14:31:38 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c47193b0e88c6b7c103c29e33cfc3a68d22e09a2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721313096504,
+  "lastUpdate": 1721313098464,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -41699,6 +41699,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c47193b0e88c6b7c103c29e33cfc3a68d22e09a2",
+          "message": "Switching to Alma9 in benchmarks and format workflows (#392)",
+          "timestamp": "2024-07-18T16:24:15+02:00",
+          "tree_id": "b6f5f1c6b5a187a50208bc8b4c4dc1319629739c",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/c47193b0e88c6b7c103c29e33cfc3a68d22e09a2"
+        },
+        "date": 1721313097737,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 2.331688437289646,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 6.8573050027379,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.780090736713018,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.962278015382219,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.897168626319198,
+            "unit": "Evt/s"
           }
         ]
       }
