ce2981facb268c854b05133f0646e27f03561708
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-09-18 16:47:52 +0200

    Adjusting stages source example

--- examples/data_source/stages_source.py
+++ examples/data_source/stages_source.py
@@ -1,34 +1,45 @@
-# list of processes (mandatory)
-processList = {
-    'p8_ee_WW_ecm240': {'output': 'p8_ee_WW_ecm240_out'}
-}
+'''
+Analysis example using PODIO ROOT DataSource for reading input files.
+'''
 
-# Production tag when running over EDM4Hep centrally produced events, this
-# points to the yaml files for getting sample statistics (mandatory)
-prodTag = "FCCee/winter2023/IDEA/"
 
-# Optional: output directory, default is local running directory
-outputDir = "."
+class Analysis():
+    '''
+    Mandatory class, with three mandatory methods:
+      * __init__
+      * analyzers
+      * output
+    '''
+    def __init__(self, _):
+        # list of processes (mandatory)
+        self.process_list = {
+            'p8_ee_WW_ecm240': {'output': 'p8_ee_WW_ecm240_out'}
+        }
 
-# Ncpus, default is 4, -1 uses all cores available
-# nCPUS = -1
+        # Production tag when running over EDM4Hep centrally produced events,
+        # this points to the yaml files for getting sample statistics
+        # (mandatory)
+        self.prod_tag = 'FCCee/winter2023/IDEA/'
 
-# How to read input files
-useDataSource = True
+        # Optional: output directory, default is local running directory
+        self.output_dir = "."
 
-testFile = 'https://fccsw.web.cern.ch/fccsw/testsamples/' \
-           'edm4hep1/p8_ee_WW_ecm240_edm4hep.root'
+        # Ncpus, default is 4, -1 uses all cores available
+        # self.n_threads = -1
 
-# RDFanalysis class where the use defines the operations on the TTree
-# (mandatory)
-class RDFanalysis():
+        # How to read input files
+        self.use_data_source = True
 
-    # analysis function to define the analyzers to process, please make sure
-    # you return the last dataframe, in this example it is df2
-    def analysers(df):
+        self.test_file = 'https://fccsw.web.cern.ch/fccsw/testsamples/' \
+                         'edm4hep1/p8_ee_WW_ecm240_edm4hep.root'
 
-        df2 = (
-            df
+    def analyzers(self, dframe):
+        '''
+        Analysis function to define the analyzers to process, please make sure
+        you return the last dataframe, in this example it is dframe2
+        '''
+        dframe2 = (
+            dframe
             .Define(
               "electron_truth",
               "recoParticle::selPDG(11)(MCRecoAssociations)")
@@ -38,12 +49,10 @@ class RDFanalysis():
               "recoParticle::getPt(electron_truth)")
         )
 
-        return df2
+        return dframe2
 
-    def output():
-        branchList = [
+    def output(self) -> list[str]:
+        return [
                 # "electron_truth",
                 "electron_truth_pt"
         ]
-
-        return branchList
