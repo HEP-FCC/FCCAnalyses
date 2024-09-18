741200b1bed2d6201e02e92eeefcb0bf7d1a107e
github-action-benchmark <github@users.noreply.github.com>
2024-09-18 13:41:17 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 7a1910f5ff32e90854dfb9d475e2b8bc7b863a03

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726666876049,
+  "lastUpdate": 1726666877476,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -42887,6 +42887,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7a1910f5ff32e90854dfb9d475e2b8bc7b863a03",
+          "message": "Synchronization of final and plots stages (#397)\n\n* Decoupling latex table printing from rdf running\r\n\r\n* Further minor adjustments\r\n\r\n* Providing default values in Hismaker Plot\r\n\r\n* Storing graph in working directory if path not specified\r\n\r\n* Before scaling the histograms in plots stage check final stage results\r\n\r\n* Now possible to have no signal or no background in the plot\r\n\r\n* Putting scaling  determination to separate function",
+          "timestamp": "2024-09-18T15:33:51+02:00",
+          "tree_id": "99b8b9aaad4b64a49a1a05362eac8a7b06e70c5b",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/7a1910f5ff32e90854dfb9d475e2b8bc7b863a03"
+        },
+        "date": 1726666877020,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 7.618227677136612,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.64975964010699,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.538185614459011,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.031023621465282,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.7421170942571957,
+            "unit": "Evt/s"
           }
         ]
       }
