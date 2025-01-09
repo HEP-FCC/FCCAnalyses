f3ed39c71dd3c4d55da98f9c926eda9d9a226547
Juraj Smiesko <juraj.smiesko@cern.ch>
2025-01-09 17:31:15 +0100

    Move handling of the HTCondor parameters to batch.py

--- python/submit.py
+++ python/submit.py
@@ -8,61 +8,10 @@ import logging
 import importlib
 import argparse
 import shutil
-from typing import Any
 from batch import send_to_batch
 
 
 LOGGER = logging.getLogger('FCCAnalyses.submit')
-
-
-# _____________________________________________________________________________
-def merge_config_analysis_class(config: dict[str, Any],
-                                args: argparse.Namespace,
-                                analysis_module: Any) -> dict[str, Any]:
-    '''
-    Merge configuration from the analysis script class and the command-line
-    arguments.
-    '''
-
-    analysis_class = analysis_module.Analysis(vars(args))
-    config['analysis-class'] = analysis_class
-
-    # Check if there are any processes defined
-    if not hasattr(analysis_class, 'process_list'):
-        LOGGER.error('Analysis does not define any processes!\n'
-                     'Aborting...')
-        sys.exit(3)
-    config['process-list'] = analysis_class.process_list
-
-    # Check if there is production tag or input directory defined
-    if not hasattr(analysis_class, 'prod_tag') and \
-       not hasattr(analysis_class, 'input_dir'):
-        LOGGER.error('Analysis does not define production tag or input '
-                     'directory!\nAborting...')
-        sys.exit(3)
-
-    if hasattr(analysis_class, 'prod_tag') and \
-       hasattr(analysis_class, 'input_dir'):
-        LOGGER.error('Analysis defines both production tag and input '
-                     'directory!\nAborting...')
-        sys.exit(3)
-
-    if hasattr(analysis_class, 'prod_tag'):
-        config['production-tag'] = analysis_class.prod_tag
-    else:
-        config['production-tag'] = None
-
-    if hasattr(analysis_class, 'input_dir'):
-        config['input-directory'] = analysis_class.input_dir
-    else:
-        config['input-directory'] = None
-
-    if hasattr(analysis_class, 'n_threads'):
-        config['n-threads'] = analysis_class.n_threads
-    else:
-        config['n-threads'] = 1
-
-    return config
 
 
 # _____________________________________________________________________________
@@ -90,9 +39,6 @@ def submit_analysis(parser: argparse.ArgumentParser) -> None:
                      'Aborting...')
         sys.exit(3)
 
-    config = {}
-    config['cli-arguments'] = vars(args)
-
     # Work with absolute path of the analysis script.
     anapath = os.path.abspath(args.anascript_path)
     LOGGER.info('Loading analysis script from:\n  %s', anapath)
@@ -101,7 +47,6 @@ def submit_analysis(parser: argparse.ArgumentParser) -> None:
     if not os.path.isfile(anapath):
         LOGGER.error('Analysis script not found!\nAborting...')
         sys.exit(3)
-    config['analysis-path'] = anapath
 
     # Load the analysis script as a module
     try:
@@ -114,10 +59,5 @@ def submit_analysis(parser: argparse.ArgumentParser) -> None:
                      err)
         sys.exit(3)
 
-    if hasattr(analysis_module, "Analysis"):
-        config = merge_config_analysis_class(config, args, analysis_module)
-
-    for process_name in config['process-list'].keys():
-        LOGGER.info('Submitting process "%s"', process_name)
-
-        send_to_batch(config, args, config['analysis-class'], process_name)
+    if args.where == 'ht-condor':
+        send_to_batch(args, analysis_module)
