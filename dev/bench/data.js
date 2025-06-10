4b892369a59e62158ba052f214b7cf3f72548464
github-action-benchmark <github@users.noreply.github.com>
2025-06-10 15:53:51 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 12b8698967b66b7d4a2d2547cf682115f20478ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749570829540,
+  "lastUpdate": 1749570831713,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -48901,6 +48901,65 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "12b8698967b66b7d4a2d2547cf682115f20478ce",
+          "message": "Test file path can be a template (#447)\n\n* Using newer test file\n\n* Test file path can be a template\n\n* Using domain to check for nightlies/release\n\n* Using domain to check for nightlies/release in histmaker too\n\n* Adding the k4h os/type string to standalone tests\n\n* Adding info to the man page",
+          "timestamp": "2025-06-10T17:43:17+02:00",
+          "tree_id": "d3c79d244ee598914309b68cc10a5032a4e71614",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/12b8698967b66b7d4a2d2547cf682115f20478ce"
+        },
+        "date": 1749570830895,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 5.80337006081004,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 9.23385983140511,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.075062398854066,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 9.67407957135632,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.692455101959396,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/data_source/stages_source.py",
+            "value": 5.462341661689294,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 5.276876673736297,
+            "unit": "Evt/s"
           }
         ]
       }
