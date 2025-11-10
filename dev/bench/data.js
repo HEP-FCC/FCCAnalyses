92ec159d5cca8646c366fd4d44136c65eb1c30ac
github-action-benchmark <github@users.noreply.github.com>
2025-11-10 16:10:15 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 720d360520996a497375ece9eb96ff9b62b7625e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762791014237,
+  "lastUpdate": 1762791015683,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -51409,6 +51409,65 @@ window.BENCHMARK_DATA = {
             "range": "10",
             "unit": "Seconds",
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "720d360520996a497375ece9eb96ff9b62b7625e",
+          "message": "Using Kebab style for keys of the dictionaries (#475)\n\n* Using Kebab style for keys of the dictionaries\n\n* Undo kebab style for get_attribute()",
+          "timestamp": "2025-11-10T16:59:34+01:00",
+          "tree_id": "55b7e48b03c9902dadc3a9365095ea1603b1e232",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/720d360520996a497375ece9eb96ff9b62b7625e"
+        },
+        "date": 1762791015247,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.820795766964233,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 11.105210884160659,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 6.889084757145417,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 11.614431266684843,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 1.499027721889969,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/data_source/stages_source.py",
+            "value": 6.93940098548235,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 6.599943150819265,
+            "unit": "Evt/s"
           }
         ]
       }
