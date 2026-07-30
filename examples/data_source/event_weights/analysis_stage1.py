edec79eb225191bf43a32baabe62ca594a93dcbb
Zeynep Eda Cabukoglu <cabukogluzeynepeda@gmail.com>
2026-07-30 12:12:54 +0200

    Use DataSource weight names in event weight analysis

--- examples/data_source/event_weights/analysis_stage1.py
+++ examples/data_source/event_weights/analysis_stage1.py
@@ -1,40 +1,20 @@
-'''
-Analysis example, measure Higgs mass in the Z(mumu)H recoil measurement.
-'''
-from argparse import ArgumentParser
+'''Example: retrieve event weights from EDM4hep EventHeader.'''
+
 
 import ROOT
 # Mandatory: Analysis class where the user defines the operations on the
 # dataframe.
 class Analysis():
-    '''
-    Higgs mass recoil analysis in Z(mumu)H.
-    '''
+    ''' Retrieve per-event weights from the EDM4hep EventHeader:  the full weights vector, and one weight selected by name.  '''   
     def __init__(self, cmdline_args):
-        # Parse additional arguments not known to the FCCAnalyses parsers.
-        # All command line arguments are provided in the `cmdline_arg`
-        # dictionary and arguments after "--" are stored under "remaining" key.
-        parser = ArgumentParser(
-            description='Additional analysis arguments',
-            usage='Provided after "--"')
-        parser.add_argument('--muon-pt', default='10.', type=float,
-                            help='Minimal pT of the mouns.')
-        self.ana_args, _ = parser.parse_known_args(cmdline_args['remaining'])
-
         # Mandatory: List of datasets used in the analysis
+        # Note: currently using the first pp-hhh file since couldn't find metadata in 10000 event file
         self.process_list = {
-            # Run over the full statistics and save it to one output file named
-            # <outputDir>/<process_name>.root
-            'p8_ee_ZZ_ecm240': {'fraction': 1.},
-            # Run over 50% of the statistics and save output into two files
-            # named <outputDir>/p8_ee_WW_ecm240/chunk<N>.root
-            # Number of input files needs to be larger that number of chunks
-            'p8_ee_WW_ecm240': {'fraction': 0.5, 'chunks': 2},
-            # Run over 20% of the statistics and save output into one file
-            # named <outputDir>/p8_ee_ZH_ecm240_out_f02.root
-            'p8_ee_ZH_ecm240': {'fraction': 0.2,
-                                'output': 'p8_ee_ZH_ecm240_out_f02'}
+            "/afs/cern.ch/user/z/zcabukog/event_weights_project/"
+            "pp_hhh_84TeV_weights_5evt.edm4hep.root": {}
         }
+   #'pp_hhh_84TeV_weights_5evt'
+
 
         # Mandatory: Production tag when running over the centrally produced
         # samples (this points to the yaml file for getting sample statistics)
@@ -44,9 +24,7 @@ class Analysis():
         self.input_dir = '/eos/experiment/fcc/hh/tutorials/' \
                          'edm4hep_tutorial_data/'
 
-        # Optional: output directory, default is local running directory
-        self.output_dir = 'outputs/FCCee/higgs/mH-recoil/mumu/' \
-                          f'stage1_{self.ana_args.muon_pt}'
+    
 
         #podio
         #line below tells FCCAnalyses to read the ROOT file using podio::DataSource instead of reading the raw ROOT branches directly
@@ -80,30 +58,37 @@ class Analysis():
         self.include_paths = ["Definitions.h"]
 
         # Optional: test file
-        self.test_file = '/afs/cern.ch/user/z/zcabukog/event_weights_project/pp_hhh_84TeV_weights_5evt.edm4hep.root'
-
+        self.test_file = (
+            "/afs/cern.ch/user/z/zcabukog/event_weights_project/"
+            "pp_hhh_84TeV_weights_5evt.edm4hep.root"
+        )
     # Mandatory: analyzers function to define the analysis graph, please make
-    # sure you return the dataframe, in this example it is dframe2
+    # sure you return the dataframe
     def analyzers(self, dframe):
         #define creates a new column in the dataframe, and the first argument is the name of the new column, and the second argument is the function that will be used to create the new column. 
+      
         dframe2 = dframe.Define(
             "event_weights",
             "GetAllWeights{}(EventHeader)"
         )
 
-        # Create one Functor 2 object and find the index of rwgt_4 once.
-        selected_weight_functor = ROOT.GetWeightByName(
-            "rwgt_4",
-            self.test_file
-        )
+        # Create one GetWeightByName object and store the requested label.
+        # The functor no longer receives or opens the input file.
+        selected_weight_functor = ROOT.GetWeightByName("rwgt_4")
 
-        # Functor 2: create a second column containing only rwgt_4.
+        # Create a new column containing the numerical value of rwgt_4
+        # for each event.
+        #
+        # RDataFrame passes two columns into the functor:
+        # EventHeader supplies the numerical event weights.
+        # _EventWeightNames supplies the corresponding weight labels.
         dframe3 = dframe2.Define(
             "selected_weight",
             selected_weight_functor,
-            ["EventHeader"]
+            ["EventHeader", "_EventWeightNames"]
         )
 
+       # Return the dataframe containing both event_weights and selected_weight.
         return dframe3
         # Pass EventHeader into the GetAllWeights functor and store its returned weights in a new column called event_weights.
 
@@ -112,4 +97,5 @@ class Analysis():
     # Mandatory: output function, please make sure you return the branch list
     # as a python list
     def output(self):
-        return ["event_weights", "selected_weight"]
+         return ["event_weights", "selected_weight"]
+        
