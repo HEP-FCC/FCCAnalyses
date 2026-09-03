cf774bff023676ffab6aadffdd2016ba5d6b52fd
github-action-benchmark <github@users.noreply.github.com>
2026-09-03 16:15:39 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fbc087058a57289d4947a3f9d6510fa0ee204364

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788452135909,
+  "lastUpdate": 1788452138825,
   "repoUrl": "https://github.com/HEP-FCC/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -60133,6 +60133,76 @@ window.BENCHMARK_DATA = {
             "value": 6.444339990615845,
             "range": "10",
             "unit": "Seconds",
+            "extra": "Analysis path: /Package/examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "22276694+jmcarcell@users.noreply.github.com",
+            "name": "Juan Miguel Carceller",
+            "username": "jmcarcell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fbc087058a57289d4947a3f9d6510fa0ee204364",
+          "message": "Fix compiler warnings (#539)\n\n* fix: resolve non-index compiler warnings\n\n* Fix a few other warnings\n\n* Fix more warnings\n\n* Fix more\n\n* Fix more\n\n* Do not check for tracks_begin being >= 0\n\n* Make a few more invasive changes\n\n* style: apply clang-format\n\n* fix: suppress FastJet deprecated-copy warnings\n\n---------\n\nCo-authored-by: Juan Miguel Carceller <jcarcell@lcgapp-alma9-physical.cern.ch>\nCo-authored-by: Juan Miguel Carceller <jmcarcell@users.noreply.github.com>",
+          "timestamp": "2026-09-03T18:03:54+02:00",
+          "tree_id": "90874cf85449dd125b64181fd6c4d42001df298b",
+          "url": "https://github.com/HEP-FCC/FCCAnalyses/commit/fbc087058a57289d4947a3f9d6510fa0ee204364"
+        },
+        "date": 1788452137956,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: /Package/examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 21.083356665431783,
+            "range": "1000",
+            "unit": "Evt/s",
+            "extra": "Analysis path: /Package/examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/ee/analysis_stage1.py",
+            "value": 10.044200618964323,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 7.979310694216109,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 10.965184168279169,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.4518164517630487,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/stage1.py",
+            "value": 3.8775870698112094,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: /Package/examples/data_source/stages_source.py",
+            "value": 14.602034451410448,
+            "range": "1000",
+            "unit": "Evt/s",
+            "extra": "Analysis path: /Package/examples/data_source/stages_source.py"
+          },
+          {
+            "name": "prod | Events processed per second: /Package/examples/data_source/analysis_stage1.py",
+            "value": 15.517492892308377,
+            "range": "1000",
+            "unit": "Evt/s",
             "extra": "Analysis path: /Package/examples/data_source/analysis_stage1.py"
           }
         ]
